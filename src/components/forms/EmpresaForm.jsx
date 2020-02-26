fed12e8cce7ab6ca356ed9b419df9e700905e43b
OmarAbaroa <alvaro1295@hotmail.com>
2020-02-26 12:09:02 -0600

    Form

--- src/components/forms/EmpresaForm.jsx
+++ src/components/forms/EmpresaForm.jsx
@@ -3,7 +3,8 @@ import Form from 'react-bootstrap/Form'
 import { Input, Button, Select } from '../form-components'
 import { Subtitle, P } from '../texts'
 import { URL_ASSETS } from '../../constants'
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle, faClipboard, faCameraRetro, faCamera } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 class EmpresaForm extends Component{
 
     constructor(props){
@@ -17,25 +18,42 @@ class EmpresaForm extends Component{
         const { form, onChange, title, img, removeFile} = this.props
         return(
             <Form { ... this.props}>
-                <Subtitle color="gold">
+                <Subtitle className="text-center" color="gold">
                     {title}
                 </Subtitle>
-                <Input onChange={ onChange } required name="name" type="text" value={ form.name } placeholder="Nombre"/>
-                {
-                    (img === '' ) && (form.file !== '' && form.file !== undefined && form.file !== null) &&
-                        <div className="p-3 position-relative">
-                            <img className='w-100' src={URL_ASSETS + form.file } />
+                <div className="row my-3 mx-auto">
+                    <div className="col-md-6">
+                        <Input onChange={ onChange } required name="name" type="text" value={ form.name } placeholder="Nombre"/>
+                    </div>
+                    <div className="col-md-6 ">
+                        <div className="image-upload">
+                            <Input
+                                onChange={ onChange }
+                                name="logo" 
+                                type="file" 
+                                value={ form.logo }
+                                id="logo"
+                                placeholder="Logo de la empresa"/>
+                            <label for="logo">
+                                <FontAwesomeIcon icon={faCamera} />
+                            </label>
                         </div>
-                }
-                {
-                    form.logo &&
-                        <div className="p-3 position-relative">
-                            <img value={img} className='w-100' src={ img } />
-                            <Button className="position-absolute delete-item " 
-                                onClick={removeFile} text='' icon={faTimesCircle}/>
-                        </div>
-                }
-                <Input onChange={ onChange } name="logo" type="file" value={ form.logo } placeholder="File"/>
+                        {
+                            (img === '' ) && (form.file !== '' && form.file !== undefined && form.file !== null) &&
+                                <div className="p-3 position-relative">
+                                    <img className='w-100' src={URL_ASSETS + form.file } />
+                                </div>
+                        }
+                        {
+                            form.logo &&
+                                <div className="p-3 position-relative">
+                                    <img value={img} className='w-100' src={ img } />
+                                    <Button className="position-absolute delete-item " 
+                                        onClick={removeFile} text='' icon={faTimesCircle}/>
+                                </div>
+                        }
+                    </div>
+                </div>
                 <div className="mt-3 text-center">
                     <Button className="mx-auto" type="submit" text="Enviar" />
                 </div>
