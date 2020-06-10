5fb851d0d588fb69e4b2948ca61033e807ad4cfb
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-10 11:04:34 -0500

    Cambio del diseño del SelectSearch

--- src/components/form-components/SelectSearch.jsx
+++ src/components/form-components/SelectSearch.jsx
@@ -2,23 +2,36 @@ import React, { Component } from 'react'
 import SelectSearch from 'react-select-search'
 import Input from './Input'
 
+import '../../styles/select_custom.css';
 class SelectSearchForm extends Component{
 
     render(){
         
-        const { placeholder, options } = this.props
+        const { name, onChange, options,  placeholder, value  } = this.props 
+        
         return(
             <div>
                 { 
                     options.length > 0 ?
                         <>
-                            <label className="mt-2 mb-1 label-form">
-                                {placeholder}
-                            </label>
-                            <SelectSearch 
-                                className="select-search"
-                                search
-                                { ... this.props} />
+                            <label className="col-form-label">{placeholder}</label>
+
+                            <div className="input-group">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text">
+                                    <i className="far fa-calendar-check kt-font-boldest text-success"></i>
+                                    </span>
+                                </div>
+
+                                <div className="dropdown bootstrap-select form-control">																	
+
+                                    <SelectSearch 
+                                        search
+                                        {...this.props}
+                                        />
+                                </div>    
+                            </div>
+                                
                         </>
                     :
                     <Input readOnly type="text" { ...this.props }/>
