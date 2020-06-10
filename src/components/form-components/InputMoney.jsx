098d7b3c71b21fea875d9ae402d1bcbe3ae00de4
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-10 11:03:37 -0500

    Cambio del diseño del input money

--- src/components/form-components/InputMoney.jsx
+++ src/components/form-components/InputMoney.jsx
@@ -9,19 +9,28 @@ class InputMoney extends Component{
     } */
 
     onChange = values => {
-        const { onChange, name } = this.props
+        const { onChange, name} = this.props
         onChange({target:{value:values.value, name:name}})
     }
 
     render(){
-        const { error, onChange, placeholder, value, prefix, thousandSeparator  } = this.props
+        const { error, onChange, placeholder, value, prefix, thousandSeparator,iconclass,spantext  } = this.props
         return(
             <div>
-                <Form.Label className="mt-2 mb-1 label-form">
+                <Form.Label className="col-form-label">
                     {placeholder}
                 </Form.Label><br />
-                <NumberFormat value = { value } displayType = { 'input' } thousandSeparator = { thousandSeparator ? thousandSeparator : false } prefix = { prefix } className="form-control w-100"
+                
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">
+                            <i className={iconclass+" kt-font-boldest text-success"}></i>
+                        </span>
+                    </div>
+                    <NumberFormat value = { value } displayType = { 'input' } thousandSeparator = { thousandSeparator ? thousandSeparator : false } prefix = { prefix } className="form-control w-100"
                     renderText = { value => <div> { value } </div> } onValueChange = { (values) => this.onChange(values) } placeholder = {placeholder} />
+                </div>
+                <span className="form-text text-muted">Por favor, ingrese su {spantext}</span>
             </div>
         )
     }
