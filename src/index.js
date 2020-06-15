37073db139cbbf7916ac928b85150ed3a3e3e27d
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-15 11:12:43 -0500

    Se eliminaron imports inecesarios y se corrigio error del import createBrowserHistory

--- src/index.js
+++ src/index.js
@@ -4,36 +4,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from "axios";
 import * as _redux from "./redux";
-
-/* import './index.css';
-import "./index.scss";    */
 import App from './App'; 
-/* import "./_metronic/_assets/plugins/keenthemes-icons/font/ki.css"; */
-/* import "socicon/css/socicon.css"; */
 import "@fortawesome/fontawesome-free/css/all.min.css";
-/* import "./_metronic/_assets/plugins/flaticon/flaticon.css";
-import "./_metronic/_assets/plugins/flaticon2/flaticon.css"; */ 
 import "react-datepicker/dist/react-datepicker.css";
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import reducer from './redux/reducers';
 import { createStore, applyMiddleware } from 'redux';
-import createHistory from 'history/createBrowserHistory'
+//import createHistory from 'history/createBrowserHistory'
+import { createBrowserHistory } from 'history'
 import { Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.scss'
 import thunk from 'redux-thunk'
-import { loadState, saveState } from './redux/store/store'
+import { loadState, saveState } from './redux/store/store' 
 
-/* import {
-  MetronicLayoutProvider,
-  MetronicSplashScreenProvider,
-  MetronicSubheaderProvider
-} from "./_metronic/layout";
-import {MetronicI18nProvider} from "./_metronic/i18n"; */
 const { PUBLIC_URL } = process.env;
-const history =  createHistory()
-
+//const history =  createHistory()
+const history = createBrowserHistory();
 const initialData = loadState()
 
 const store = createStore(reducer, initialData, applyMiddleware(thunk));
