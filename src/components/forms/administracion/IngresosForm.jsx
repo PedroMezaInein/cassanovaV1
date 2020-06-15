d29ada28c2636ed42160b4dd798ea062bd40a090
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-15 11:26:27 -0500

    Se modificaron los estilos del formulario.

--- src/components/forms/administracion/IngresosForm.jsx
+++ src/components/forms/administracion/IngresosForm.jsx
@@ -109,9 +109,8 @@ class EgresosForm extends Component {
                 <Subtitle className="text-center" color="gold">
                     {title}
                 </Subtitle>
-
-                <div className="row mx-0">
-                    <div className="col-md-6 px-2">
+                <div className="form-group row form-group-marginless mt-5">
+                    <div className="col-md-4">
                         <RadioGroup
                             name='factura'
                             onChange={this.updateFactura}
@@ -130,95 +129,187 @@ class EgresosForm extends Component {
                             placeholder={' Lleva factura '}
                             value={form.factura}
                         />
-                    </div>
+                    </div>                    
                     {
                         form.factura === 'Con factura' && title !== 'Editar ingreso' ?
-                            <div className="col-md-6 px-2">
-                                <FileInput
-                                    onChangeAdjunto={onChangeAdjunto}
-                                    placeholder={form['adjuntos']['factura']['placeholder']}
-                                    value={form['adjuntos']['factura']['value']}
-                                    name={'factura'}
-                                    id={'factura'}
-                                    accept="text/xml, application/pdf"
-                                    files={form['adjuntos']['factura']['files']}
-                                    deleteAdjunto={clearFiles} multiple />
+                        <div className="col-md-4">
+                            <FileInput
+                                onChangeAdjunto={onChangeAdjunto}
+                                placeholder={form['adjuntos']['factura']['placeholder']}
+                                value={form['adjuntos']['factura']['value']}
+                                name={'factura'}
+                                id={'factura'}
+                                accept="text/xml, application/pdf"
+                                files={form['adjuntos']['factura']['files']}
+                                deleteAdjunto={clearFiles} multiple 
+                                />
+                            <span className="form-text text-muted">Por favor, adjunta el documento. </span>
                             </div>
                             : ''
                     }
-
                     {
                         form.factura === 'Con factura' && title !== 'Editar venta' ?
-                            <div className="col-md-6 px-2">
-                                <Input placeholder="RFC" name="rfc" value={form.rfc} onChange={onChange} />
+                            <div className="col-md-4">
+                                <Input 
+                                    placeholder="RFC" 
+                                    name="rfc" 
+                                    value={form.rfc} 
+                                    onChange={onChange} 
+                                    iconclass={"far fa-file-alt"} 
+                                />
+                            <span className="form-text text-muted">Por favor, ingresa el RFC. </span>
                             </div>
                             : ''
                     }
-
-                    <div className="col-md-6 px-2">
+                </div>
+                <div class="separator separator-dashed mt-1 mb-2"></div>
+                <div className="form-group row form-group-marginless">
+                    <div className="col-md-4">
                         {
                             form.facturaObject ?
-                                <Input placeholder="Cliente" readOnly name="cliente" value={form.cliente} onChange={onChange} />
+                                <Input 
+                                    placeholder="Cliente" 
+                                    readOnly 
+                                    name="cliente" 
+                                    value={form.cliente} 
+                                    onChange={onChange}
+                                    iconclass={"far fa-user"}
+                                />                                
                                 :
-                                <SelectSearch options={options.clientes} placeholder="Selecciona el cliente"
-                                    name="cliente" value={form.cliente} onChange={this.updateCliente} />
+                                <SelectSearch 
+                                    options={options.clientes} 
+                                    placeholder="Selecciona el cliente"
+                                    name="cliente" 
+                                    value={form.cliente} 
+                                    onChange={this.updateCliente} 
+                                    iconclass={"far fa-user"}
+                                />                                
                         }
+                        <span className="form-text text-muted">Por favor, selecciona el cliente</span>
                     </div>
-
-                    <div className="col-md-6 px-2">
+                    <div className="col-md-4">
                         {
                             form.facturaObject ?
-                                <Input placeholder="Empresa" name="empresa" readOnly value={form.empresa} onChange={onChange} />
+                                <Input 
+                                    placeholder="Empresa" 
+                                    name="empresa" 
+                                    readOnly 
+                                    value={form.empresa} 
+                                    onChange={onChange} 
+                                />
                                 :
-                                <SelectSearch options={options.empresas} placeholder="Selecciona la empresa"
-                                    name="empresa" value={form.empresa} onChange={this.updateEmpresa} />
+                                <SelectSearch 
+                                    options={options.empresas} 
+                                    placeholder="Selecciona la empresa"
+                                    name="empresa" 
+                                    value={form.empresa} 
+                                    onChange={this.updateEmpresa} 
+                                    iconclass={"far fa-building"}/>
                         }
-                    </div>
-
-                    {
-                        form.empresa ?
-                            <div className="col-md-6 px-2">
-                                <SelectSearch options={options.cuentas} placeholder="Selecciona la cuenta"
-                                    name="cuenta" value={form.cuenta} onChange={this.updateCuenta} />
-                            </div>
-                            : ''
-                    }
-
-                    <div className="col-md-6 px-2">
-                        <SelectSearch options={options.areas} placeholder="Selecciona el área"
-                            name="area" value={form.area} onChange={this.updateArea} />
+                    <span className="form-text text-muted">Por favor, selecciona la empresa</span>
+                    </div>                    
+                        {
+                            form.empresa ?
+                            <div className="col-md-4">
+                                    <SelectSearch 
+                                        options={options.cuentas} 
+                                        placeholder="Selecciona la cuenta"
+                                        name="cuenta" 
+                                        value={form.cuenta} 
+                                        onChange={this.updateCuenta} 
+                                        iconclass={"far fa-credit-card"} 
+                                    />
+                                <span className="form-text text-muted">Por favor, selecciona la cuenta. </span>
+                                </div>
+                                : ''
+                        }
+                </div>
+                <div class="separator separator-dashed mt-1 mb-2"></div>
+                <div className="form-group row form-group-marginless">
+                    <div className="col-md-4">
+                        <SelectSearch 
+                            options={options.areas} 
+                            placeholder="Selecciona el área"
+                            name="area" 
+                            value={form.area} 
+                            onChange={this.updateArea} 
+                            iconclass={"far fa-window-maximize"}
+                        />
+                        <span className="form-text text-muted">Por favor, selecciona el área. </span>
                     </div>
                     {
                         form.area ?
-                            <div className="col-md-6 px-2">
-                                <SelectSearch options={options.subareas} placeholder="Selecciona el subárea"
-                                    name="subarea" value={form.subarea} onChange={this.updateSubarea} />
+                            <div className="col-md-4">
+                                <SelectSearch 
+                                    options={options.subareas} 
+                                    placeholder="Selecciona el subárea"
+                                    name="subarea" 
+                                    value={form.subarea} 
+                                    onChange={this.updateSubarea} 
+                                    iconclass={"far fa-window-restore"}
+                                />
+                                <span className="form-text text-muted">Por favor, selecciona la sub-área. </span>
                             </div>
                             : ''
-                    }
-
-                    <div className="col-md-6 px-2">
-                        <Select placeholder="Selecciona el tipo de pago" options={options.tiposPagos}
-                            name="tipoPago" value={form.tipoPago} onChange={this.updateTipoPago} />
+                    }                    
+                    <div className="col-md-4">
+                        <Select 
+                            placeholder="Selecciona el tipo de pago" 
+                            options={options.tiposPagos}
+                            name="tipoPago" 
+                            value={form.tipoPago} 
+                            onChange={this.updateTipoPago} 
+                        />
+                        <span className="form-text text-muted">Por favor, selecciona el tipo de pago. </span>
                     </div>
-                    <div className="col-md-6 px-2">
-                        <Select placeholder="Selecciona el impuesto" options={options.tiposImpuestos}
-                            name="tipoImpuesto" value={form.tipoImpuesto} onChange={onChange} />
+                </div>
+                <div class="separator separator-dashed mt-1 mb-2"></div>
+                <div className="form-group row form-group-marginless">
+                    <div className="col-md-4">
+                        <Select 
+                            placeholder="Selecciona el impuesto" 
+                            options={options.tiposImpuestos}
+                            name="tipoImpuesto" 
+                            value={form.tipoImpuesto} 
+                            onChange={onChange}
+                        />
+                        <span className="form-text text-muted">Por favor, selecciona el impuesto. </span>
                     </div>
-                    <div className="col-md-6 px-2">
-                        <Select placeholder="Selecciona el estatus de compra" options={options.estatusCompras}
-                            name="estatusCompra" value={form.estatusCompra} onChange={onChange} />
+                    <div className="col-md-4">
+                        <Select 
+                            placeholder="Selecciona el estatus de compra" 
+                            options={options.estatusCompras}
+                            name="estatusCompra" 
+                            value={form.estatusCompra} 
+                            onChange={onChange}
+                        />
+                        <span className="form-text text-muted">Por favor, selecciona el estatus de compra. </span>
                     </div>
-
-                    <div className="col-md-6 px-2">
-                        <InputMoney thousandSeparator={true} placeholder="Monto" value={form.total} name="total" onChange={onChange} />
+                    <div className="col-md-4">
+                        <InputMoney 
+                        thousandSeparator={true} 
+                        placeholder="Monto" 
+                        value={form.total} 
+                        name="total" 
+                        onChange={onChange} 
+                        iconclass={" fas fa-money-check-alt"}
+                        />
+                    <span className="form-text text-muted">Por favor, ingresa el monto </span>
                     </div>
-                    <div className="col-md-6 px-2">
+                </div>
+                
+                <div class="separator separator-dashed mt-1 mb-2"></div>
+                <div className="form-group row form-group-marginless">
+                    <div className="col-md-4">
                         <Calendar
-                            onChangeCalendar={this.handleChangeDate} placeholder="Fecha"
-                            name="fecha" value={form.fecha} />
+                            onChangeCalendar={this.handleChangeDate} 
+                            placeholder="Fecha"
+                            name="fecha" 
+                            value={form.fecha} 
+                        />
+                        <span className="form-text text-muted">Por favor, selecciona la fecha. </span>
                     </div>
-                    <div className="col-md-6 px-2 ">
+                    <div className="col-md-4">
                         <FileInput
                             onChangeAdjunto={onChangeAdjunto}
                             placeholder={form['adjuntos']['presupuesto']['placeholder']}
@@ -227,8 +318,9 @@ class EgresosForm extends Component {
                             accept="text/xml, application/pdf"
                             files={form['adjuntos']['presupuesto']['files']}
                             deleteAdjunto={clearFiles} />
+                        <span className="form-text text-muted">Por favor, adjunta el documento. </span>
                     </div>
-                    <div className="col-md-6 px-2 ">
+                    <div className="col-md-4">
                         <FileInput
                             onChangeAdjunto={onChangeAdjunto}
                             placeholder={form['adjuntos']['pago']['placeholder']}
@@ -236,14 +328,25 @@ class EgresosForm extends Component {
                             name={'pago'} id={'pago'}
                             accept="text/xml, application/pdf"
                             files={form['adjuntos']['pago']['files']}
-                            deleteAdjunto={clearFiles} />
-                    </div>
-                    <div className=" col-md-12 px-2">
-                        <Input as="textarea" placeholder="Descripción" rows="3" value={form.descripcion}
-                            name="descripcion" onChange={onChange} />
+                            deleteAdjunto={clearFiles} 
+                        />
+                        <span className="form-text text-muted">Por favor, adjunta el documento. </span>
                     </div>
                 </div>
-
+                <div class="separator separator-dashed mt-1 mb-2"></div>
+                <div className="form-group row form-group-marginless">
+                    <div className="col-md-12">
+                    <Input 
+                        as="textarea" 
+                        placeholder="Descripción" 
+                        rows="2" value={form.descripcion}
+                        name="descripcion" 
+                        onChange={onChange}
+                        iconclass={"far fa-file-alt"} 
+                        />
+                    <span className="form-text text-muted">Por favor, ingresa la descripción. </span>
+                    </div>
+                </div>
                 <div className="mt-3 text-center">
                     <Button icon='' className="mx-auto" type="submit" text="Enviar" />
                 </div>
