34a4136cc111129370bd2987d2f3818c51bff788
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-15 11:36:25 -0500

    Se importaron nuevas vistas (UrlLocation, MobileHeader y UserPanel), así como estilos

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -8,7 +8,21 @@ import NewAsideMenu from './newAsideMenu'
 import { logout, login } from '../../redux/reducers/auth_user'
 import axios from 'axios'
 import { URL_DEV } from '../../constants'
+import UrlLocation from './urlLocation'
+import ScrollBar from "react-perfect-scrollbar";
+import MobileHeader from './mobileHeader'
+import UserPanel from '../../../src/components/layout/UserPanel/userPanel'
 
+function openUserProfile (){  
+    if(document.getElementsByClassName("offcanvas")[0].classList.contains("offcanvas-on")){
+        // console.log(document.getElementsByClassName("offcanvas")[0].classList);
+        document.getElementsByClassName("offcanvas")[0].classList.remove("offcanvas-on");
+    }
+    else
+    { 
+        document.getElementsByClassName("offcanvas")[0].classList.add("offcanvas-on");
+    }
+}
 class Layout extends Component {
 
     state = {
@@ -45,25 +59,85 @@ class Layout extends Component {
             history.push('/login')
         })
     }
-
+    
     render() {
         const { children, } = this.props
-        const { menu } = this.state
+        const { menu } = this.state   
         return (
+            <> 
+            <MobileHeader/>
 
-            <div className="pl-5  position-relative nav-full-height"  >
-                <Navbar clickResponsiveMenu={this.clickResponsiveMenu} clickLogout={this.logoutUser} {... this.props} />
+            <div className="d-flex flex-column flex-root">
+                <div className="d-flex flex-row flex-column-fluid page">
+                    <MenuResponsive expanded={menu ? 'expanded' : ''} clickResponsiveMenu={this.clickResponsiveMenu} {... this.props} />
+                    
+                    <NewAsideMenu props =  {this.props} /> 
+                    <div className="d-flex flex-column flex-row-fluid wrapper"> 
+                        <div  className="header header-fixed">
+                            <div className="container-fluid d-flex align-items-stretch justify-content-between">
+                                <div className="header-menu-wrapper header-menu-wrapper-left" >
+                                    <div  className="header-menu header-menu-mobile header-menu-layout-default">
+                                        <ul className="menu-nav">
+                                            <li className="menu-item menu-item-submenu" data-menu-toggle="click" aria-haspopup="true">
+                                                <a href="/proyectos/proyectos" className="menu-link menu-toggle">
+                                                    <span className="menu-text">Proyectos</span>
+                                                    <i className="menu-arrow"></i>
+                                                </a>
+                                            </li>
+                                            <li className="menu-item menu-item-submenu" data-menu-toggle="click" aria-haspopup="true">
+                                                <a href="/administracion/proveedores" className="menu-link menu-toggle">
+                                                    <span className="menu-text">Proveedores</span>
+                                                    <i className="menu-arrow"></i>
+                                                </a>
+                                            </li>
+                                            <li className="menu-item menu-item-submenu menu-item-rel" data-menu-toggle="click" aria-haspopup="true">
+                                                <a href="/leads/leads" className="menu-link menu-toggle">
+                                                    <span className="menu-text">Leads</span>
+                                                    <i className="menu-arrow"></i>
+                                                </a>
+                                            </li>
+                                        </ul>                                        
+                                    </div>
+                                </div>                                
+                                <div className="topbar" onClick = { () => { openUserProfile() } }>
+                                    <div className="topbar-item">                                    
+                                        {/* <Navbar clickResponsiveMenu={this.clickResponsiveMenu} clickLogout={this.logoutUser} {... this.props} />*/}
+                                            <div className="btn btn-icon w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
+                                                <span className="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hola,</span>
+                                                <span className="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Omar</span>
+                                                <span className="symbol symbol-35 symbol-light-primary">
+                                                    <span className="symbol-label font-size-h5 font-weight-bold">O</span>
+                                                    
+                                                </span>
+                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                   
 
-                {/* <Sidebar {... this.props} /> */}
-                <NewAsideMenu props =  {this.props} />
-                
-                <MenuResponsive expanded={menu ? 'expanded' : ''} clickResponsiveMenu={this.clickResponsiveMenu} {... this.props} />
-                <div className="pl-5 container-fluid">
-                    {children}
+                        <div className="content d-flex flex-column flex-column-fluid">
+                            
+                            <UrlLocation/>
+                            
+                            <div className="d-flex flex-column-fluid">
+                                <div className="container-fluid">
+                                    <div className="row">
+                                        <div className="col-sm-12">
+                                            {children}
+                                        </div>
+                                    </div> 
+                                </div>
+                            </div>
+                        </div>                
+                    </div> 
+                    
                 </div>
             </div>
+            <UserPanel />
+            </>
         )
     }
+    
 }
 
 const mapStateToProps = state => {
