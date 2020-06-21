88dc4aad7e63764eba3ff5be7490ca496957a1c3
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-20 23:54:15 -0500

    Se agregaron las funciones para el mobileHeader

--- src/components/layout/mobileHeader.jsx
+++ src/components/layout/mobileHeader.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
+function openMobileAside(){    
+	document.body.classList.add('aside-on');		
+	document.getElementById("openbuerger").classList.add("mobile-toggle-active");
+	document.getElementById("aside").classList.add("aside-on");
+	document.getElementById('showaside').classList.add("aside-overlay"); 	
+}
+
+function openMobileHeader(){    
+	document.body.classList.add('header-menu-wrapper-on');		
+	document.getElementById("openbuerger").classList.add("mobile-toggle-active");
+	document.getElementById("showheadermenu").classList.add("header-menu-wrapper-on");
+	document.getElementById('showheader').classList.add("header-menu-wrapper-overlay"); 	
+}
+
+function openMobileTopbar(){    
+	if(!document.body.classList.contains('topbar-mobile-on') )
+    {
+		document.body.classList.add('topbar-mobile-on');
+		document.getElementById("opentopbar").classList.add("active");	
+	}else{
+		document.body.classList.remove('topbar-mobile-on');
+		document.getElementById("opentopbar").classList.remove("active");	
+	}
+}
 export default function MobileHeader() {
 
     return (
@@ -9,13 +33,13 @@ export default function MobileHeader() {
 					<img alt="Logo" src="/dashboard.png" />
 				</a>
 				<div className="d-flex align-items-center">
-					<button className="btn p-0 burger-icon burger-icon-left">
+					<button id="openbuerger"className="btn p-0 burger-icon burger-icon-left" onClick = { () => { openMobileAside() } }>
 						<span></span>
 					</button>
-					<button className="btn p-0 burger-icon ml-4">
+					<button className="btn p-0 burger-icon ml-4" onClick = { () => { openMobileHeader() } }>
 						<span></span>
 					</button>
-					<button className="btn btn-hover-text-primary p-0 ml-2">
+					<button id="opentopbar"className="btn btn-hover-text-primary p-0 ml-2" onClick = { () => { openMobileTopbar() } }>
 						<span className="svg-icon svg-icon-xl">
 							<svg xmlns="http://www.w3.org/2000/svg"width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
 								<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
