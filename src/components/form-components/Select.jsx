a2cabbeb15e53f333b49eeb3de41f374687febb3
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-22 14:08:37 -0500

    Validación para el select (con error)

--- src/components/form-components/Select.jsx
+++ src/components/form-components/Select.jsx
@@ -64,4 +64,70 @@ class Select extends Component{
     }
 }
 
+/* CÓDIGO QUE VALIDA EL SIGUIENTE SELECT
+import React, { Component } from 'react'
+import Form from 'react-bootstrap/Form'
+
+import '../../styles/select_custom.css';
+
+let selectValido =false;
+class Select extends Component{
+    
+
+    validarSelect(datosSelect)
+    {
+        console.log(datosSelect)
+        let value = datosSelect.props.value;
+        console.log(value);
+        if(value=="0")
+        {
+            console.log("Incorrecto")
+            selectValido=false;
+        }
+        else
+        {
+            console.log("Correto")
+            selectValido=true;
+        }
+    }
+  
+    render(){
+        const { options, placeholder, value, name, onChange, messageinc, ...props } = this.props
+        return(
+            <>
+                <Form.Label className="col-form-label">{ placeholder }</Form.Label>
+                
+                <Form.Control 
+                className={ selectValido? " form-control is-valid ": " form-control is-invalid " }
+                onChange={ this.validarSelect(this), onChange } 
+                name={ name } 
+                value={ value } 
+                as="select" {... props}>
+                    <option value={0} disabled>
+                        {placeholder}
+                    </option>
+                    {
+                        options.map((option, key) => {
+                            return(
+                                <option key={key} value={option.value}>
+                                    { option.text }
+                                </option>
+                            )
+                        })
+                    }
+                    <div className="valid-feedback msgValidation"> Valor correcto </div>
+                    <div className="invalid-feedback msgValidation">{messageinc}</div>
+                </Form.Control>
+                
+            </>
+        )
+    }
+}
+
 export default Select
+*/
+
+export default Select
+
+
+
