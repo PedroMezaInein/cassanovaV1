9e23e971c19acde8d027b91cee9ff6145694e236
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-22 14:05:08 -0500

    Validación de select, con error.

--- src/components/form-components/Select.jsx
+++ src/components/form-components/Select.jsx
@@ -2,19 +2,47 @@ import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form'
 
 import '../../styles/select_custom.css';
-
+ 
 class Select extends Component{
     /* constructor(props){
         super(props)
-    } */
+    } */ 
 
+    validarSelect(datosSelect)
+    {console.log("entre")
+        this.setState({
+            selectValido:false            
+        })
+       
+       /*         console.log(datosSelect)
+        let value = datosSelect.props.value;
+        console.log(value);
+        if(value=="0")
+        {
+            console.log("Incorrecto")
+            selectValido=false;
+        }
+        else
+        {
+            console.log("Correto")
+            selectValido=true;
+        }
+        */
+    }
+  
     render(){
-        const { options, placeholder, value, name, onChange, ...props } = this.props
+        const { options, placeholder, value, name, onChange, messageinc, ...props } = this.props
+       // console.log()
         return(
             <>
                 <Form.Label className="col-form-label">{ placeholder }</Form.Label>
                 
-                <Form.Control onChange={ onChange } name={ name } value={ value } as="select" {... props}>
+                <Form.Control 
+               // className={ selectValido? " form-control is-valid ": " form-control is-invalid " }
+                onChange={ this.validarSelect(this), onChange } 
+                name={ name } 
+                value={ value } 
+                as="select" {... props}>
                     <option value={0} disabled>
                         {placeholder}
                     </option>
@@ -28,6 +56,9 @@ class Select extends Component{
                         })
                     }
                 </Form.Control>
+                    <div className="valid-feedback msgValidation"> Valor correcto </div>
+                    <div className="invalid-feedback msgValidation">{messageinc}</div>
+                
             </>
         )
     }
