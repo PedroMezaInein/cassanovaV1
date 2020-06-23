b5e911b90ca4b8161bb476c5a4d85e3db79d752e
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-23 15:56:58 -0500

    Se agregó el input-group para los iconos

--- src/components/form-components/Select.jsx
+++ src/components/form-components/Select.jsx
@@ -21,108 +21,50 @@ class Select extends Component{
             })
         }else{
             this.setState({
-                selectValido: false
+                selectValido: false     
+                
             })
         }
     }
 
     render(){
-        const { options, placeholder, value, name, onChange, messageinc, ...props } = this.props
+        const { options, placeholder, value, name, onChange, iconclass, messageinc, ...props } = this.props
         const { selectValido } = this.state
         return(
             <>
                 <Form.Label className="col-form-label">{ placeholder }</Form.Label>
                 
-                <Form.Control 
-                    className={ selectValido ? " form-control is-valid " : " form-control is-invalid " }
-                    onChange={ (e) => { e.preventDefault(); this.validarSelect(e); onChange(e) }} 
-                    name={ name } 
-                    value={ value } 
-                    as="select" {... props}>
-                    <option value={0} disabled>
-                        {placeholder}
-                    </option>
-                    {
-                        options.map((option, key) => {
-                            return(
-                                <option key={key} value={option.value}>
-                                    { option.text }
-                                </option>
-                            )
-                        })
-                    }
-                </Form.Control>
-                    <div className="valid-feedback msgValidation"> Valor correcto </div>
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">
+                            <i className={iconclass+" kt-font-boldest text-primary"}></i>
+                        </span>
+                    </div>
+                    <Form.Control 
+                        className={ selectValido ? " form-control is-valid " : " form-control is-invalid" }
+                        onChange={ (e) => { e.preventDefault(); this.validarSelect(e); onChange(e) }} 
+                        name={ name } 
+                        value={ value } 
+                        as="select" {... props}>
+                        <option value={0} disabled>
+                            {placeholder}
+                        </option>
+                        {
+                            options.map((option, key) => {
+                                return(
+                                    <option key={key} value={option.value}>
+                                        { option.text }
+                                    </option>
+                                )
+                            })
+                        }
+                    </Form.Control>                
                     <div className="invalid-feedback msgValidation">{messageinc}</div>
-                
+                </div>
             </>
         )
     }
 }
-
-/* CÓDIGO QUE VALIDA EL SIGUIENTE SELECT
-import React, { Component } from 'react'
-import Form from 'react-bootstrap/Form'
-
-import '../../styles/select_custom.css';
-
-let selectValido =false;
-class Select extends Component{
-    
-
-    validarSelect(datosSelect)
-    {
-        console.log(datosSelect)
-        let value = datosSelect.props.value;
-        console.log(value);
-        if(value=="0")
-        {
-            console.log("Incorrecto")
-            selectValido=false;
-        }
-        else
-        {
-            console.log("Correto")
-            selectValido=true;
-        }
-    }
-  
-    render(){
-        const { options, placeholder, value, name, onChange, messageinc, ...props } = this.props
-        return(
-            <>
-                <Form.Label className="col-form-label">{ placeholder }</Form.Label>
-                
-                <Form.Control 
-                className={ selectValido? " form-control is-valid ": " form-control is-invalid " }
-                onChange={ this.validarSelect(this), onChange } 
-                name={ name } 
-                value={ value } 
-                as="select" {... props}>
-                    <option value={0} disabled>
-                        {placeholder}
-                    </option>
-                    {
-                        options.map((option, key) => {
-                            return(
-                                <option key={key} value={option.value}>
-                                    { option.text }
-                                </option>
-                            )
-                        })
-                    }
-                    <div className="valid-feedback msgValidation"> Valor correcto </div>
-                    <div className="invalid-feedback msgValidation">{messageinc}</div>
-                </Form.Control>
-                
-            </>
-        )
-    }
-}
-
-export default Select
-*/
-
 export default Select
 
 
