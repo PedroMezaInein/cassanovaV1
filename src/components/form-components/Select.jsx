809ad641e8e9439140e60572f58234ed09e02ea5
OmarAbaroa <alvaro1295@hotmail.com>
2020-06-23 09:58:44 -0500

    Validador select

--- src/components/form-components/Select.jsx
+++ src/components/form-components/Select.jsx
@@ -12,26 +12,33 @@ class Select extends Component{
         selectValido: false
     }
 
-    validarSelect(datosSelect){
-        console.log("entre")
-        this.setState({
-            selectValido:false            
-        })
+    validarSelect(e){
+        const { value } = e.target
+        
+        if(value > 0){
+            this.setState({
+                selectValido: true
+            })
+        }else{
+            this.setState({
+                selectValido: false
+            })
+        }
     }
 
     render(){
         const { options, placeholder, value, name, onChange, messageinc, ...props } = this.props
-       // console.log()
+        const { selectValido } = this.state
         return(
             <>
                 <Form.Label className="col-form-label">{ placeholder }</Form.Label>
                 
                 <Form.Control 
-               // className={ selectValido? " form-control is-valid ": " form-control is-invalid " }
-                onChange={ (e) => { e.preventDefault(); this.validarSelect(this); this.props.onChange(e) }} 
-                name={ name } 
-                value={ value } 
-                as="select" {... props}>
+                    className={ selectValido ? " form-control is-valid " : " form-control is-invalid " }
+                    onChange={ (e) => { e.preventDefault(); this.validarSelect(e); onChange(e) }} 
+                    name={ name } 
+                    value={ value } 
+                    as="select" {... props}>
                     <option value={0} disabled>
                         {placeholder}
                     </option>
