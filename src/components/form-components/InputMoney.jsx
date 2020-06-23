791965ed81ee07ca7a8b561b250ee8f692035b0e
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-22 23:05:28 -0500

    Validación para inputMoney

--- src/components/form-components/InputMoney.jsx
+++ src/components/form-components/InputMoney.jsx
@@ -2,17 +2,31 @@ import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form'
 import NumberFormat from 'react-number-format'
 
+let textoValido =false;
 class InputMoney extends Component{
 
     /* constructor(props){
         super(props)
     } */
+    
+    validarInput(textoEscrito)
+    {
+        console.log(textoEscrito) 
+        if(textoEscrito =="") // validar expresion regular
+        { 
+            textoValido=false;
+        }
+        else
+        { 
+            textoValido=true;
+        }
+    }
 
     onChange = values => {
+        this.validarInput(values.value)
         const { onChange, name} = this.props
         onChange({target:{value:values.value, name:name}})
     }
-
     render(){
         const { error, onChange, placeholder, value, prefix, thousandSeparator,iconclass } = this.props
         return(
@@ -24,15 +38,23 @@ class InputMoney extends Component{
                 <div className="input-group">
                     <div className="input-group-prepend">
                         <span className="input-group-text">
-                            <i className={iconclass+" kt-font-boldest text-success"}></i>
+                            <i className={iconclass+" kt-font-boldest text-primary"}></i>
                         </span>
                     </div>
-                    <NumberFormat value = { value } displayType = { 'input' } thousandSeparator = { thousandSeparator ? thousandSeparator : false } prefix = { prefix } className="form-control w-100"
-                    renderText = { value => <div> { value } </div> } onValueChange = { (values) => this.onChange(values) } placeholder = {placeholder} />
+                    <NumberFormat
+                        value = { value } 
+                        displayType = { 'input' } 
+                        thousandSeparator = { thousandSeparator ? thousandSeparator : false } 
+                        prefix = { prefix }
+                        //prefix={'$'} 
+                        className={textoValido?"w-100 form-control  is-valid":"w-100 form-control is-invalid"}
+                        renderText = { value => <div> { value } </div> } 
+                        onValueChange = { (values) => this.onChange(values)}
+                        placeholder = {placeholder} /> 
+                    <div className="invalid-feedback msgValidation">Incorrecto, introduce la cantidad.</div>
                 </div>
             </div>
         )
     }
 }
-
 export default InputMoney
