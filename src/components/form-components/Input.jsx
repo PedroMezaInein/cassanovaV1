98c6549289464b620cfaa48017334f050c6a5038
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-24 05:25:49 -0500

    Se modificó el input para validación y estilo

--- src/components/form-components/Input.jsx
+++ src/components/form-components/Input.jsx
@@ -3,33 +3,46 @@ import Form from 'react-bootstrap/Form'
 
 class Input extends Component {
 
-    /* constructor(props){
-        super(props)
-    } */
+    state = {
+        inputValido: false
+    }
+
+    validarInput(e){
+        const { value } = e.target
+        const {pattern}= this.props
+        var expRegular = new RegExp(pattern);         
+            if(expRegular.test(value)){
+                this.setState({
+                    inputValido: true
+                })
+            }else{
+                this.setState({
+                    inputValido: false     
+                    
+                })
+            } 
+    }
 
     render() {
-        const { error, onChange, placeholder, iconclass } = this.props 
+        const { error, onChange, placeholder, iconclass, messageinc, ...props } = this.props 
+        const { inputValido } = this.state
         return (
             <div >
                 <label className="col-form-label">{placeholder}</label>
-
-                <div className="input-group">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text">
-                            <i className={iconclass+" kt-font-boldest text-success"}></i>
-                        </span>
+                    <div className="input-icon">       
+                        <span className="input-icon input-icon-right">
+                            <i className={iconclass+" m-0 kt-font-boldest text-primary"}></i> 
+                        </span>                 
+                        <Form.Control 
+                            placeholder={placeholder} 
+                            className={ inputValido ? " form-control is-valid " : " form-control is-invalid" }
+                            onChange={(e) => { console.log("onChange");e.preventDefault(); this.validarInput(e); onChange(e) }} 
+                            {...props} 
+                        /> 
                     </div>
-                    <Form.Control onChange={(e) => { e.preventDefault(); onChange(e) }} className={`${error}`} {... this.props} />
-                    {
-                        error &&
-                        <label className="error-label">
-                            {error}
-                        </label>
-                    }
-                </div>                
+                    <span className={ inputValido ? "form-text text-danger hidden" : "form-text text-danger" }> {messageinc} </span>
             </div>
         )
     }
 }
-
 export default Input
