47d9c176ee72334b1acb9058ec0c79a721f9e06d
CariJG <64931832+CariJG@users.noreply.github.com>
2020-06-25 17:16:25 -0500

    Se agregó wizard

--- src/components/forms/proyectos/SolicitudCompraForm.jsx
+++ src/components/forms/proyectos/SolicitudCompraForm.jsx
@@ -3,6 +3,7 @@ import { Form } from 'react-bootstrap'
 import { Subtitle, Small } from '../../texts'
 import { Input, Select, SelectSearch, Button, Calendar, InputMoney, RadioGroup, FileInput } from '../../form-components'
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import {openWizard1, openWizard2, openWizard3 } from '../../../functions/wizard'
 
 
 class SolicitudCompraForm extends Component {
@@ -82,94 +83,207 @@ class SolicitudCompraForm extends Component {
     render() {
         const { title, options, form, onChange, children, onChangeAdjunto, clearFiles, ...props } = this.props
         return (
-            <Form {...props}>
-                <Subtitle className="text-center" color="gold">
-                    {
-                        title
-                    }
-                </Subtitle>
-
-                {
-                    children
-                }
-
-                <div className="row mx-0 my-3">
-                    <div className="col-md-6 px-2">
-                        <SelectSearch options={options.proyectos} placeholder="Selecciona el proyecto"
-                            name="proyecto" value={form.proyecto} onChange={this.updateProyecto} />
-                    </div>
-                    <div className="col-md-6 px-2">
-                        <SelectSearch options={options.proveedores} placeholder="Selecciona el proveedor"
-                            name="proveedor" value={form.proveedor} onChange={this.updateProveedor} />
-                    </div>
-                    <div className="col-md-6 px-2">
-                        <SelectSearch options={options.empresas} placeholder="Selecciona la empresa"
-                            name="empresa" value={form.empresa} onChange={this.updateEmpresa} />
-                    </div>
-                    <div className="col-md-6 px-2">
-                        <InputMoney thousandSeparator={true} placeholder="Monto" value={form.total} name="total" onChange={onChange} />
-                    </div>
-                    <div className="col-md-6 px-2">
-                        <SelectSearch options={options.areas} placeholder="Selecciona el área"
-                            name="areas" value={form.area} onChange={this.updateArea} />
-                    </div>
-                    {
-                        form.area ?
-                            <div className="col-md-6 px-2">
-                                <SelectSearch options={options.subareas} placeholder="Selecciona el subárea"
-                                    name="subarea" value={form.subarea} onChange={this.updateSubarea} />
+            <div className="wizard wizard-3" id="wizardP" data-wizard-state="step-first">
+                <div className="wizard-nav">
+                    <div className="wizard-steps px-8 py-8 px-lg-15 py-lg-3"> 
+                        <div id="wizard-1" className="wizard-step" data-wizard-state="current" data-wizard-type="step" onClick = { () => { openWizard1() } }>
+                            <div className="wizard-label">
+                                <h3 className="wizard-title">
+                                <span>1.</span>Soliciar compra</h3>
+                                <div className="wizard-bar"></div>
                             </div>
-                            : ''
-                    }
-                    <div className="col-md-6 px-2">
-                        <Select placeholder="Selecciona el tipo de pago" options={options.tiposPagos}
-                            name="tipoPago" value={form.tipoPago} onChange={onChange} />
+                        </div> 
+                        <div id="wizard-2" className="wizard-step" data-wizard-type="step" onClick = { () => { openWizard2() } }>
+                            <div className="wizard-label">
+                                <h3 className="wizard-title">
+                                <span>2.</span> Área y fecha</h3>
+                                <div className="wizard-bar"></div>
+                            </div>
+                        </div> 
+                        <div id="wizard-3" className="wizard-step" data-wizard-type="step" onClick = { () => { openWizard3() } }>
+                            <div className="wizard-label">
+                                <h3 className="wizard-title">
+                                <span>3.</span> Presupuesto</h3>
+                                <div className="wizard-bar"></div>
+                            </div>
+                        </div>   
                     </div>
-                    <div className="col-md-6 px-2">
-                        <Calendar onChangeCalendar={this.handleChangeDate}
-                            placeholder="Fecha" name="fecha" value={form.fecha} />
-                    </div>
-                    <div className="col-md-6 px-2">
-                        <RadioGroup
-                            name='factura'
-                            onChange={onChange}
-                            options={
-                                [
+                </div>
+                <div className="row justify-content-center py-10 px-8 py-lg-12 px-lg-10">
+                    <div className="col-xl-12 col-xxl-7">
+                        <Form 
+                            {...props}
+                            >
+                            {/*<Subtitle className="text-center" color="gold">{title}</Subtitle>*/}
+                            {children}
+                            <div id="wizard-1-content" className="pb-3" data-wizard-type="step-content" data-wizard-state="current">
+                                <h5 className="mb-4 font-weight-bold text-dark">Ingresa los datos</h5>
+                                <div className="form-group row form-group-marginless">
+                                    <div className="col-md-6">
+                                        <SelectSearch 
+                                            options={options.proyectos}
+                                            placeholder="Selecciona el proyecto"
+                                            name="proyecto"
+                                            value={form.proyecto}
+                                            onChange={this.updateProyecto}
+                                        />
+                                    </div>
+                                    <div className="col-md-6">
+                                        <SelectSearch
+                                            options={options.proveedores}
+                                            placeholder="Selecciona el proveedor"
+                                            name="proveedor"
+                                            value={form.proveedor}
+                                            onChange={this.updateProveedor}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="separator separator-dashed mt-1 mb-2"></div>
+                                <div className="form-group row form-group-marginless">
+                                    <div className="col-md-4">
+                                        <SelectSearch
+                                            options={options.empresas}
+                                            placeholder="Selecciona la empresa"
+                                            name="empresa"
+                                            value={form.empresa}
+                                            onChange={this.updateEmpresa} 
+                                        />
+                                    </div>
+                                    <div className="col-md-4">
+                                        <InputMoney 
+                                            thousandSeparator={true}
+                                            placeholder="Monto"
+                                            value={form.total}
+                                            name="total" onChange={onChange}
+                                            iconclass={" fas fa-money-check-alt"}
+                                        />
+                                    </div>
+                                    <div className="col-md-4">
+                                        <Select
+                                            placeholder="Selecciona el tipo de pago"
+                                            options={options.tiposPagos}
+                                            name="tipoPago"
+                                            value={form.tipoPago}
+                                            onChange={onChange}
+                                            messageinc="Incorrecto. Selecciona el tipo de pago."
+                                            iconclass={"fas fa-coins"}
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                                <div className="d-flex justify-content-between border-top mt-3 pt-3">
+                                    <div className="mr-2"></div>
+                                    <div>
+                                        <button type="button" className="btn btn-primary font-weight-bold text-uppercase" onClick = { () => { openWizard2() }} data-wizard-type="action-next">Siguiente</button>
+                                    </div>
+                                </div>
+                            </div>   
+                            <div id="wizard-2-content" className="pb-3" data-wizard-type="step-content">
+                                <h5 className="mb-4 font-weight-bold text-dark">Selecciona el área y fecha</h5>
+								<div className="form-group row form-group-marginless">
+                                    <div className="col-md-4">
+                                        <SelectSearch 
+                                            options={options.areas}
+                                            placeholder="Selecciona el área"
+                                            name="areas"
+                                            value={form.area}
+                                            onChange={this.updateArea}
+                                        />
+                                    </div>
                                     {
-                                        label: 'Si',
-                                        value: 'Con factura'
-                                    },
-                                    {
-                                        label: 'No',
-                                        value: 'Sin factura'
+                                        form.area ?
+                                            <div className="col-md-4">
+                                                <SelectSearch 
+                                                    options={options.subareas}
+                                                    placeholder="Selecciona el subárea"
+                                                    name="subarea"
+                                                    value={form.subarea}
+                                                    onChange={this.updateSubarea}
+                                                />
+                                            </div>
+                                        : ''
                                     }
-                                ]
-                            }
-                            placeholder={' Lleva factura '}
-                            value={form.factura}
-                        />
-                    </div>
-                    <div className="col-md-6 px-2">
-                        <FileInput
-                            onChangeAdjunto={onChangeAdjunto}
-                            placeholder={form.adjuntos.adjunto.placeholder}
-                            value={form.adjuntos.adjunto.value}
-                            name='adjunto' id='adjunto'
-                            accept="image/*, application/pdf"
-                            files={form.adjuntos.adjunto.files}
-                            deleteAdjunto={clearFiles} multiple />
-                    </div>
-                    <div className=" col-md-12 px-2">
-                        <Input as="textarea" placeholder="Descripción" rows="3" value={form.descripcion}
-                            name="descripcion" onChange={onChange} />
+                                    <div className="col-md-4">
+                                        <Calendar
+                                            onChangeCalendar={this.handleChangeDate}
+                                            placeholder="Fecha"
+                                            name="fecha"
+                                            value={form.fecha}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="separator separator-dashed mt-1 mb-2"></div>
+                                <div className="form-group row form-group-marginless">
+                                    <div className="col-md-12">
+                                        <Input 
+                                            as="textarea"
+                                            placeholder="Descripción"
+                                            rows="3"
+                                            value={form.descripcion}
+                                            name="descripcion"
+                                            onChange={onChange}
+                                            style={{paddingLeft:"10px"}} 
+                                            messageinc="Incorrecto. Ingresa la descripción."
+                                        />
+                                    </div>
+                                </div>
+                                <div className="d-flex justify-content-between border-top mt-3 pt-3">
+                                    <div className="mr-2">
+                                        <button type="button" className="btn btn-light-primary font-weight-bold text-uppercase" onClick = { () => { openWizard1() }} data-wizard-type="action-prev">Anterior</button>
+                                    </div>
+                                    <div>
+                                        <button type="button" className="btn btn-primary font-weight-bold text-uppercase" onClick = { () => { openWizard3() }} data-wizard-type="action-next">Siguiente</button>
+                                    </div>
+                                </div> 
+                            </div>
+                            <div id="wizard-3-content" className="pb-3" data-wizard-type="step-content">
+                                <h5 className="mb-4 font-weight-bold text-dark">Selecciona el presupuesto</h5>        
+                                <div className="form-group row form-group-marginless">
+                                    <div className="col-md-4">
+                                        <RadioGroup
+                                            name='factura'
+                                            onChange={onChange}
+                                            options={
+                                                [
+                                                    {
+                                                        label: 'Si',
+                                                        value: 'Con factura'
+                                                    },
+                                                    {
+                                                        label: 'No',
+                                                        value: 'Sin factura'
+                                                    }
+                                                ]
+                                            }
+                                            placeholder={' Lleva factura '}
+                                            value={form.factura}
+                                        />
+                                    </div>
+                                    <div className="col-md-4">
+                                        <FileInput
+                                            onChangeAdjunto={onChangeAdjunto}
+                                            placeholder={form.adjuntos.adjunto.placeholder}
+                                            value={form.adjuntos.adjunto.value}
+                                            name='adjunto' id='adjunto'
+                                            accept="image/*, application/pdf"
+                                            files={form.adjuntos.adjunto.files}
+                                            deleteAdjunto={clearFiles} multiple 
+                                        />
+                                    </div>
+                                </div>
+                                <div className="d-flex justify-content-between border-top mt-3 pt-3">
+                                    <div className="mr-2">
+                                        <button type="button" className="btn btn-light-primary font-weight-bold text-uppercase"  onClick = { () => { openWizard2() }} data-wizard-type="action-prev">Anterior</button>
+                                    </div>
+                                    <div>
+                                        <Button icon='' className="btn btn-primary font-weight-bold text-uppercase" type="submit" data-wizard-type="action-submit" text="Enviar" />
+                                    </div>
+                                </div>
+                            </div>
+                        </Form>
                     </div>
                 </div>
-
-                <div className="d-flex justify-content-center my-3">
-                    <Button icon='' type="submit" className="text-center mx-auto" text='Enviar' />
-                </div>
-
-            </Form>
+            </div>
         )
     }
 }
