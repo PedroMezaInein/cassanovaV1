6eb6e98e3c45c61e7465fc82642db64196282338
CariJG <64931832+CariJG@users.noreply.github.com>
2020-06-25 14:37:10 -0500

    Se modificó el formulario

--- src/components/forms/presupuesto/ConceptoForm.jsx
+++ src/components/forms/presupuesto/ConceptoForm.jsx
@@ -34,38 +34,71 @@ class ConceptoForm extends Component {
         const { title, options, form, onChange, ...props } = this.props
         return (
             <Form {...props}>
-                    <div className="form-group row form-group-marginless">
-                        <div className="col-md-4">
-                            <SelectSearch options={options.partidas} placeholder="Selecciona la partida"
-                                name="partida" value={form.partida} onChange={this.updatePartida} iconclass={" fas fa-book"} fas fa-weight-hanging/>
-                            <span className="form-text text-muted">Por favor, selecciona la partida</span>
+                    <div className="form-group row form-group-marginless pt-4">
+                        <div className="col-md-6">
+                            <SelectSearch 
+                                options={options.partidas}
+                                placeholder="Selecciona la partida"
+                                name="partida"
+                                value={form.partida}
+                                onChange={this.updatePartida} 
+                                iconclass={" fas fa-book"} 
+                            />
+                            {/*<span className="form-text text-muted">Por favor, selecciona la partida</span>*/}
                         </div>
-                        <div className="col-md-4">
-                            <SelectSearch options={options.subpartidas} placeholder="Selecciona la subpartida"
-                                name="subpartida" value={form.subpartida} onChange={this.updateSubpartida} iconclass={" fas fa-book"} fas fa-weight-hanging/>
-                            <span className="form-text text-muted">Por favor, selecciona la subpartida</span>
-                        </div>
-                        <div className="col-md-4">
-                            <SelectSearch options={options.unidades} placeholder="Selecciona la unidad"
-                                name="unidad" value={form.unidad} onChange={this.updateUnidades} iconclass={" fas fa-weight-hanging"}/>
-                            <span className="form-text text-muted">Por favor, selecciona la unidad</span>
-                        </div>
-                    </div>
-                    <div className="separator separator-dashed mt-1 mb-2"></div>
-                    <div className="form-group row form-group-marginless">
-                        <div className="col-md-4">
-                            <InputMoney thousandSeparator={true} placeholder="Costo" value={form.costo} name="costo" onChange={onChange} iconclass={"fas fa-dollar-sign"}/>
-                            <span className="form-text text-muted">Por favor, ingrese su costo. </span>
-                        </div>
-                        <div className="col-md-8">
-                            <Input as="textarea" placeholder="Descripción" rows="1" value={form.descripcion}
-                                name="descripcion" onChange={onChange} iconclass={"far fa-file-alt"} />
-                            <span className="form-text text-muted">Por favor, ingresa la descripción. </span>
+                        <div className="col-md-6" hidden={options.subpartidas.length <= 0 ? true : false}>
+                            <SelectSearch 
+                                options={options.subpartidas}
+                                placeholder="Selecciona la subpartida"
+                                name="subpartida"
+                                value={form.subpartida}
+                                onChange={this.updateSubpartida}
+                                iconclass={" fas fa-book"}
+                            />
+                            {/*<span className="form-text text-muted">Por favor, selecciona la subpartida</span>*/}
                         </div>
                     </div>
                     <div className="separator separator-dashed mt-1 mb-2"></div>
-                    
-                
+                    <div className="form-group row form-group-marginless">
+                        <div className="col-md-6">
+                            <SelectSearch
+                                options={options.unidades}
+                                placeholder="Selecciona la unidad"
+                                name="unidad"
+                                value={form.unidad}
+                                onChange={this.updateUnidades}
+                                iconclass={" fas fa-weight-hanging"}
+                            />
+                            {/*<span className="form-text text-muted">Por favor, selecciona la unidad</span>*/}
+                        </div>
+                        <div className="col-md-6">
+                            <InputMoney 
+                                thousandSeparator={true}
+                                placeholder="Costo"
+                                value={form.costo}
+                                name="costo"
+                                onChange={onChange}
+                                iconclass={"fas fa-dollar-sign"}
+                            />
+                            {/*<span className="form-text text-muted">Por favor, ingrese su costo. </span>*/}
+                        </div>
+                    </div>
+                        <div className="separator separator-dashed mt-1 mb-2"></div>
+                        <div className="form-group row form-group-marginless">
+                            <div className="col-md-12">
+                                <Input 
+                                    as="textarea" 
+                                    placeholder="Descripción"
+                                    rows="3"
+                                    value={form.descripcion}
+                                    name="descripcion"
+                                    onChange={onChange}
+                                    messageinc="Incorrecto. Ingresa una descripción."
+                                    style={{paddingLeft:"10px"}} 
+                                />
+                                {/*<span className="form-text text-muted">Por favor, ingresa la descripción. </span>*/}
+                            </div>
+                        </div>
                     <div className="d-flex justify-content-center my-3">
                         <Button icon='' type="submit" className="text-center mx-auto" text='Enviar' />
                     </div>
