f764a60b00103f27ce5eb8aef2ad75106f9ec5bf
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-28 19:44:32 -0500

    Evita que pasemos de step si no se han llenado los datos

--- src/functions/wizard.jsx
+++ src/functions/wizard.jsx
@@ -1,4 +1,3 @@
-
 export function openWizard1(){  
     document.getElementById('wizardP').setAttribute("data-wizard-state","first");
     document.getElementById('wizard-1').setAttribute("data-wizard-state","current");
@@ -9,21 +8,33 @@ export function openWizard1(){
     document.getElementById("wizard-3-content").removeAttribute("data-wizard-state");
 }
 export function openWizard2(){  
-    document.getElementById('wizardP').setAttribute("data-wizard-state","between");
-    document.getElementById('wizard-1').setAttribute("data-wizard-state","done");
-    document.getElementById('wizard-2').setAttribute("data-wizard-state","current");
-    document.getElementById('wizard-3').setAttribute("data-wizard-state","pending");
-    document.getElementById('wizard-2-content').setAttribute("data-wizard-state","current");
-    document.getElementById("wizard-1-content").removeAttribute("data-wizard-state");
-    document.getElementById("wizard-3-content").removeAttribute("data-wizard-state");
+    var elementsInvalid = document.getElementById("wizard-1-content").getElementsByClassName("is-invalid");
+
+    if(elementsInvalid.length===0){
+        document.getElementById('wizardP').setAttribute("data-wizard-state","between");
+        document.getElementById('wizard-1').setAttribute("data-wizard-state","done");
+        document.getElementById('wizard-2').setAttribute("data-wizard-state","current");
+        document.getElementById('wizard-3').setAttribute("data-wizard-state","pending");
+        document.getElementById('wizard-2-content').setAttribute("data-wizard-state","current");
+        document.getElementById("wizard-1-content").removeAttribute("data-wizard-state");
+        document.getElementById("wizard-3-content").removeAttribute("data-wizard-state");
+    }else{
+        alert("Rellena todos los campos")
+    }    
 }
 
-export function openWizard3(){  
-    document.getElementById('wizardP').setAttribute("data-wizard-state","last");
-    document.getElementById('wizard-1').setAttribute("data-wizard-state","done");
-    document.getElementById('wizard-2').setAttribute("data-wizard-state","done");
-    document.getElementById('wizard-3').setAttribute("data-wizard-state","current");
-    document.getElementById('wizard-3-content').setAttribute("data-wizard-state","current"); 
-    document.getElementById("wizard-1-content").removeAttribute("data-wizard-state");
-    document.getElementById("wizard-2-content").removeAttribute("data-wizard-state");   
+export function openWizard3(){      
+    var elementsInvalid = document.getElementById("wizard-2-content").getElementsByClassName("is-invalid");
+
+    if(elementsInvalid.length===0){
+        document.getElementById('wizardP').setAttribute("data-wizard-state","last");
+        document.getElementById('wizard-1').setAttribute("data-wizard-state","done");
+        document.getElementById('wizard-2').setAttribute("data-wizard-state","done");
+        document.getElementById('wizard-3').setAttribute("data-wizard-state","current");
+        document.getElementById('wizard-3-content').setAttribute("data-wizard-state","current"); 
+        document.getElementById("wizard-1-content").removeAttribute("data-wizard-state");
+        document.getElementById("wizard-2-content").removeAttribute("data-wizard-state");       
+    }else{
+        alert("Rellena todos los campos")
+    }
 }
