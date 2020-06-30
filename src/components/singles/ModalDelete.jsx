7cfa18dff07d947dd724ca82b16df733abaf085e
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-30 14:28:09 -0500

    Se modificó color de los botones, se agregó el botón de cerrar y se agregó el titulo del modal

--- src/components/singles/ModalDelete.jsx
+++ src/components/singles/ModalDelete.jsx
@@ -1,20 +1,37 @@
 import React, {Component} from 'react'
-import { Modal } from '../singles'
+//import { Modal } from '../singles'
 import { Button } from '../form-components'
+import Modal from 'react-bootstrap/Modal'
 
 export default class ModalDelete extends Component{
     render(){
-        const { show, handleClose, children, onClick } = this.props
+        const { show, handleClose, children,title, onClick } = this.props
         return(
-            <Modal show = { show } handleClose={ handleClose } >
-                {
-                    children
-                }
-                <div className="d-flex justify-content-center mt-3">
-                    <Button icon='' onClick = { handleClose } text="Cancelar" className="mr-3" color="green"/>
-                    <Button icon='' onClick = { onClick } text="Continuar" color="red"/>
-                </div>
-            </Modal>
+            <>
+                <Modal
+                    show={show}
+                    onHide={handleClose}
+                    keyboard={true}
+                    centered
+                    //size="xl"
+                >
+                    <Modal.Header>
+                        <h5 className="modal-title" id="exampleModalLabel">{title}</h5>
+						<button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick = { handleClose }>
+						    <i aria-hidden="true" className="ki ki-close"></i>
+						</button>
+                    </Modal.Header>
+                    <Modal.Body style={{paddingTop:"0px"}}>
+                        {
+                            children
+                        }
+                        <div className="d-flex justify-content-center mt-3">
+                            <Button icon='' onClick = { handleClose } text="Cancelar" className={"btn btn-light-primary font-weight-bolder mr-3"}/>
+                            <Button icon='' onClick = { onClick } text="Continuar" className={"btn btn-danger font-weight-bold mr-2"}/>
+                        </div>
+                    </Modal.Body>               
+                </Modal>
+            </>
         )
     }
 }
