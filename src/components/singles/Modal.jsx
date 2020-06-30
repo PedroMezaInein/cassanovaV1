c4cd144e30c3d3e3809c6b656243db84a8f0e163
CariJG <48261019+CariJG@users.noreply.github.com>
2020-06-30 14:28:36 -0500

    Se agregó el botón de cerrar y se agregó el titulo del modal

--- src/components/singles/Modal.jsx
+++ src/components/singles/Modal.jsx
@@ -22,8 +22,11 @@ class modal extends Component{
                 keyboard={true}
                 centered
             >
-                <Modal.Header closeButton>
-                    <Modal.Title>{title}</Modal.Title>
+                <Modal.Header>
+                    <h5 className="modal-title" id="exampleModalLabel">{title}</h5>
+					<button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick = { handleClose }>
+					    <i aria-hidden="true" className="ki ki-close"></i>
+					</button>
                 </Modal.Header>
                 <Modal.Body style={{paddingTop:"0px"}}>
                     { children }
