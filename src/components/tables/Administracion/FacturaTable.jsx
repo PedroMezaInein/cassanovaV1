4e57500f6fb9873164ea4aaf0cc164f16bf14b07
CariJG <64931832+CariJG@users.noreply.github.com>
2020-07-01 17:39:59 -0500

    Se modificó la tabla facturas

--- src/components/tables/Administracion/FacturaTable.jsx
+++ src/components/tables/Administracion/FacturaTable.jsx
@@ -1,58 +1,68 @@
 import React, { Component } from 'react'
-import DataTable from '../Data'
+import { renderToString } from 'react-dom/server'
+import TableForModals from '../../../components/tables/TableForModals'
 import { FACTURAS_COLUMNS_2 } from '../../../constants'
 import { setTextTable, setDateTable, setMoneyTable, setArrayTable, setAdjuntosList } from '../../../functions/setters'
 import { deleteAlert } from '../../../functions/alert'
 import { Button } from '../../form-components'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { DataTable } from '../../../components/tables'
+import swal from 'sweetalert'
 
 export default class FacturaTable extends Component{
 
     state = {
-        data: []
+        data: [],
+        dataArray: {
+            facturas: []
+        },
     }
     componentDidMount(){
         const { facturas } = this.props
+        const { dataArray } = this.state
+        dataArray.facturas = facturas
         this.setState({
             ... this.state,
-            data: this.setFactura(facturas)
+            data: this.setFactura(facturas),
+            dataArray
         })
     }
 
     //Setter
     setFactura = facturas => {
         let aux = []
+        const { data } = this.state
         facturas.map( (factura) => {
-            console.log(factura, 'FACTURA')
             aux.push(
                 {
                     actions: this.setActions(factura),
-                    folio: setTextTable(factura.folio),
-                    serie: setTextTable(factura.serie),
-                    noCertificado: setTextTable(factura.numero_certificado),
-                    emisor: setArrayTable(
+                    folio: renderToString(setTextTable(factura.folio)),
+                    serie: renderToString(setTextTable(factura.serie)),
+                    noCertificado: renderToString(setTextTable(factura.numero_certificado)),
+                    emisor: renderToString(setArrayTable(
                         [
                             {name:'RFC', text:factura.rfc_emisor},
                             {name:'Nombre', text:factura.nombre_emisor}
-                        ]),
-                    receptor: setArrayTable(
+                        ])),
+                    receptor: renderToString(setArrayTable(
                         [
                             {name:'RFC', text:factura.rfc_receptor},
                             {name:'Nombre', text:factura.nombre_receptor}
-                        ]),
-                    usoCFDI: setTextTable(factura.uso_cfdi),
-                    expedicion: setArrayTable(
+                        ])),
+                    usoCFDI: renderToString(setTextTable(factura.uso_cfdi)),
+                    expedicion: renderToString(setArrayTable(
                         [
                             {name:'Lugar', text:factura.lugar_expedicion},
                             {name:'Fecha', text:factura.fecha}
-                        ]),
-                    subtotal: setMoneyTable(factura.subtotal),
-                    total: setMoneyTable(factura.total),
-                    adjuntos: setAdjuntosList([
+                        ])),
+                    subtotal: renderToString(setMoneyTable(factura.subtotal)),
+                    total: renderToString(setMoneyTable(factura.total)),
+                    adjuntos: renderToString(setAdjuntosList([
                         factura.pdf ? {name: 'factura.pdf', url: factura.pdf.url} : '',
                         factura.xml ? {name: 'factura.xml', url: factura.xml.url} : '',
-                    ]),
-                    fecha: setDateTable(factura.created_at)
+                    ])),
+                    fecha: renderToString(setDateTable(factura.created_at)),
+                    id: factura.id
                 }
             )
         })
@@ -60,15 +70,18 @@ export default class FacturaTable extends Component{
     }
 
     componentDidUpdate(prevProps){
+        const { dataArray } = this.state
         if (prevProps.facturas !== this.props.facturas) {
+            dataArray.facturas = this.props.facturas
             this.setState({
                 ... this.state,
-                data: this.setFactura(this.props.facturas)
+                data: this.setFactura(this.props.facturas),
+                dataArray
             })
         }
     }
 
-    setActions = factura => {
+    /*setActions = factura => {
         const { deleteFactura } = this.props
         return(
             <>
@@ -86,16 +99,69 @@ export default class FacturaTable extends Component{
                 </div>
             </>
         )
+    }*/
+    setActions = factura => {
+        let aux = []
+        aux.push(
+            {
+                text: 'Eliminar',
+                btnclass: 'danger',
+                iconclass: 'flaticon2-rubbish-bin',
+                action: 'delete',
+                tooltip: { id: 'delete', text: 'Eliminar', type: 'error' }
+            }            
+        )        
+        return aux
     }
+    openModalDeleteFactura = (factura) => {
+        const { deleteFactura } = this.props
+        console.log("entre")
+        swal({
+            title: '¿Seguro deseas borrar la factura?',
+            icon: 'warning',
+            buttons: {
+                cancel: {
+                    text: "Cancelar",
+                    value: null,
+                    visible: true,
+                    className: "button__green btn-primary cancel",
+                    closeModal: true,
+                },
+                confirm: {
+                    text: "Aceptar",
+                    value: true,
+                    visible: true,
+                    className: "button__red btn-primary",
+                    closeModal: true
+                }
+            }
+        }).then((result) => {
+            if (result) {
+                deleteFactura(factura.id)             
+            }            
+        })
+    } 
 
     render(){
-        const { data } = this.state
+        const { data, dataArray } = this.state
+        console.log(this.state); 
         return(
             <>
                 {
                     data.length ? 
-                        <DataTable columns = { FACTURAS_COLUMNS_2 } data = { data } hideSelector = { true } />
-                    : ''
+                     //   <DataTable columns = { FACTURAS_COLUMNS_2 } data = { data } hideSelector = { true } />
+                    <TableForModals 
+                            columns = { FACTURAS_COLUMNS_2 } 
+                            data = { data } 
+                            hideSelector = { true } 
+                            mostrar_acciones={true}
+                            actions={{
+                                'delete': { function: this.openModalDeleteFactura}
+                            }}
+                            elements={dataArray.facturas}
+
+                            idTable = 'kt_datatable_estado'/>
+                        : ''
                 }
                 
             </>
