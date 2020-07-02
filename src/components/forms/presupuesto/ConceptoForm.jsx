67afb951508642d654c7ee2397c8fab86fc84ac1
CariJG <48261019+CariJG@users.noreply.github.com>
2020-07-02 15:15:57 -0500

    Se agregó validateAlert y formeditado.

--- src/components/forms/presupuesto/ConceptoForm.jsx
+++ src/components/forms/presupuesto/ConceptoForm.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Form } from 'react-bootstrap'
 import { Subtitle, Small } from '../../texts'
 import { Input, Select, SelectSearch, Button, Calendar, InputMoney, RadioGroup } from '../../form-components'
+import { validateAlert } from '../../../functions/alert'
 
 class ConceptoForm extends Component {
 
@@ -31,50 +32,21 @@ class ConceptoForm extends Component {
     }
 
     render() {
-        const { title, options, form, onChange, onSubmit, ...props } = this.props
+        const { title, options, form, onChange, onSubmit, formeditado, ...props } = this.props
         return (
             <Form id="form-concepto"
                 onSubmit = { 
                     (e) => {
                         e.preventDefault(); 
-                        var elementsInvalid = document.getElementById("form-concepto").getElementsByClassName("is-invalid"); 
-                        if(elementsInvalid.length===0){   
-                            onSubmit(e)
-                        }else{ 
-                            alert("Rellena todos los campos")
-                        } 
+                        validateAlert(onSubmit, e, 'form-concepto')
                     }
                 }
                 {...props}
                 >
                     <div className="form-group row form-group-marginless pt-4">
-                        <div className="col-md-6">
-                            <SelectSearch 
-                                options={options.partidas}
-                                placeholder="Selecciona la partida"
-                                name="partida"
-                                value={form.partida}
-                                onChange={this.updatePartida} 
-                                iconclass={" fas fa-book"} 
-                            />
-                            {/*<span className="form-text text-muted">Por favor, selecciona la partida</span>*/}
-                        </div>
-                        <div className="col-md-6" hidden={options.subpartidas.length <= 0 ? true : false}>
-                            <SelectSearch 
-                                options={options.subpartidas}
-                                placeholder="Selecciona la subpartida"
-                                name="subpartida"
-                                value={form.subpartida}
-                                onChange={this.updateSubpartida}
-                                iconclass={" fas fa-book"}
-                            />
-                            {/*<span className="form-text text-muted">Por favor, selecciona la subpartida</span>*/}
-                        </div>
-                    </div>
-                    <div className="separator separator-dashed mt-1 mb-2"></div>
-                    <div className="form-group row form-group-marginless">
-                        <div className="col-md-6">
+                        <div className="col-md-4">
                             <SelectSearch
+                                formeditado={formeditado}
                                 options={options.unidades}
                                 placeholder="Selecciona la unidad"
                                 name="unidad"
@@ -84,9 +56,10 @@ class ConceptoForm extends Component {
                             />
                             {/*<span className="form-text text-muted">Por favor, selecciona la unidad</span>*/}
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-md-4">
                             <InputMoney 
                                 requirevalidation={1}
+                                formeditado={formeditado}
                                 thousandSeparator={true}
                                 placeholder="Costo"
                                 value={form.costo}
@@ -96,15 +69,40 @@ class ConceptoForm extends Component {
                             />
                             {/*<span className="form-text text-muted">Por favor, ingrese su costo. </span>*/}
                         </div>
+                        <div className="col-md-4">
+                            <SelectSearch 
+                                formeditado={formeditado}
+                                options={options.partidas}
+                                placeholder="Selecciona la partida"
+                                name="partida"
+                                value={form.partida}
+                                onChange={this.updatePartida} 
+                                iconclass={" fas fa-book"} 
+                            />
+                            {/*<span className="form-text text-muted">Por favor, selecciona la partida</span>*/}
+                        </div>
                     </div>
-                        <div className="separator separator-dashed mt-1 mb-2"></div>
-                        <div className="form-group row form-group-marginless">
-                            <div className="col-md-12">
+                    <div className="separator separator-dashed mt-1 mb-2"></div>
+                    <div className="form-group row form-group-marginless">
+                        <div className="col-md-4">
+                            <SelectSearch 
+                                formeditado={formeditado}
+                                options={options.subpartidas}
+                                placeholder="Selecciona la subpartida"
+                                name="subpartida"
+                                value={form.subpartida}
+                                onChange={this.updateSubpartida}
+                                iconclass={" fas fa-book"}
+                            />
+                            {/*<span className="form-text text-muted">Por favor, selecciona la subpartida</span>*/}
+                        </div>
+                        <div className="col-md-8">
                                 <Input 
                                     requirevalidation={1}
+                                    formeditado={formeditado}
                                     as="textarea" 
                                     placeholder="Descripción"
-                                    rows="3"
+                                    rows="1"
                                     value={form.descripcion}
                                     name="descripcion"
                                     onChange={onChange}
@@ -113,11 +111,10 @@ class ConceptoForm extends Component {
                                 />
                                 {/*<span className="form-text text-muted">Por favor, ingresa la descripción. </span>*/}
                             </div>
-                        </div>
+                    </div>
                     <div className="d-flex justify-content-center my-3">
                         <Button icon='' type="submit" className="text-center mx-auto" text='Enviar' />
                     </div>
-
             </Form>
         )
     }
