6191f34981ba55c566955cbfa932096b91dc3f50
CariJG <48261019+CariJG@users.noreply.github.com>
2020-07-02 13:38:19 -0500

    Se agregó el nuevo formulario, se agregaron validaciones

--- src/components/forms/catalogos/PartidaForm.jsx
+++ src/components/forms/catalogos/PartidaForm.jsx
@@ -2,46 +2,69 @@ import React, { Component } from 'react'
 import { Form } from 'react-bootstrap'
 import {Subtitle} from '../../texts'
 import {Input, Select, SelectSearch, Button} from '../../form-components'
-import { faAngleRight, faTimes, faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Badge } from 'react-bootstrap'
+import { validateAlert } from '../../../functions/alert'
 
 class PartidaForm extends Component{
 
     render(){
-        const { title, form, onChange, addSubpartida, deleteSubpartida, ... props } = this.props
+        const { title, form, onChange, addSubpartida, deleteSubpartida, onSubmit, formeditado, requirevalidation, ... props } = this.props
         return(
-            <Form { ... props}>
-                <Subtitle className="text-center" color="gold">
-                    {
-                        title
+            <Form id="form-partida"
+                onSubmit = { 
+                    (e) => {
+                        e.preventDefault(); 
+                        validateAlert(onSubmit, e, 'form-partida')
                     }
-                </Subtitle>
-                <div className="row mx-0">
-                    <div className="col-md-6 mx-center px-2">
-                        <Input required name="partida" value={form.partida} placeholder="Nombre de la partida" onChange={onChange} iconclass={"far fa-window-maximize"}/>
-                        <span className="form-text text-muted">Por favor, ingresa el nombre de la partida </span>
+                }
+                { ... props}
+                >
+                <div className="form-group row form-group-marginless pt-4">
+                    <div className="col-md-5">
+                        <Input 
+                            requirevalidation={1} 
+                            formeditado={formeditado} 
+                            name="partida" 
+                            value={form.partida} 
+                            placeholder="Nombre de la partida" 
+                            onChange={onChange} 
+                            iconclass={"far fa-window-maximize"}
+                            messageinc="Incorrecto. Ingresa el nombre de la partida."
+                        />
                     </div>
-                    <div className="col-md-6 mx-center d-flex px-2 align-items-end">
-                        <div className="w-100">
-                            <Input name="subpartida" value={form.subpartida} placeholder="Subpartida " onChange={onChange} iconclass={"far fa-window-restore"}/>
-                            <span className="form-text text-muted">Por favor, ingresa la subpartida </span>
-                        </div>
-                        <Button icon = {faCaretRight} text ="" color="transparent" className="pb-3 ml-2" onClick = { addSubpartida } />
+                    <div className="col-md-5">
+                        <Input 
+                            requirevalidation={0} 
+                            formeditado={formeditado}
+                            name="subpartida" 
+                            value={form.subpartida} 
+                            placeholder="Subpartida " 
+                            onChange={onChange} 
+                            iconclass={"far fa-window-restore"}
+                        />
                     </div>
+                    <div className="col-md-2 mt-3 d-flex justify-content-center align-items-center"> 
+                        <Button icon = {faPlus} pulse={"pulse-ring"} className={"btn btn-icon btn-light-primary pulse pulse-primary mr-5"} onClick = { addSubpartida } />
+                    </div>
+                </div>
+                <div className="col-md-12 row mx-0">   
                     {
                         form.subpartidas.map((element, key) => {
                             return(
-                                <div className="col-md-3 px-2" key = { key } >
-                                    <Badge variant = "light" className="image-upload d-flex px-3 align-items-center" pill>
-                                        <FontAwesomeIcon
-                                            icon = { faTimes }
+                                <div className="tagify form-control p-1 col-md-3 px-2 d-flex justify-content-center align-items-center" tabIndex="-1" style={{borderWidth:"0px"}} key = { key }>
+                                    <div className=" image-upload d-flex px-3 align-items-center tagify__tag tagify__tag--primary tagify--noAnim"  >
+                                        <div 
+                                            title="Borrar archivo" 
+                                            className="tagify__tag__removeBtn" 
+                                            role="button" 
+                                            aria-label="remove tag" 
                                             onClick = { (e) => { e.preventDefault(); deleteSubpartida(element) }}
-                                            className = "small-button mr-2" />
-                                            {
-                                                element
-                                            }
-                                    </Badge>
+                                        >
+                                        </div>                                                            
+                                        <div><span className="tagify__tag-text p-1 ">{element}</span></div>
+                                        </div>
                                 </div>
                             )
                         })
