92b472e26b5e5cd9905c4133856cfbf34eabdd39
CariJG <48261019+CariJG@users.noreply.github.com>
2020-07-02 13:38:37 -0500

    Se agregó el nuevo formulario y se agregaron validaciones

--- src/components/forms/catalogos/TipoForm.jsx
+++ src/components/forms/catalogos/TipoForm.jsx
@@ -5,18 +5,33 @@ import {Input, Select, SelectSearch, Button} from '../../form-components'
 import { faAngleRight, faTimes, faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Badge } from 'react-bootstrap'
+import { validateAlert } from '../../../functions/alert'
 
 class UnidadForm extends Component{
 
     render(){
-        const { title, form, onChange, ... props } = this.props
+        const { title, form, onChange, onSubmit, formeditado, ... props } = this.props
         return(
-            <Form { ... props}>
-                <div className="row mx-0">
-                    <div className="col-md-12 mx-center px-2">
-                        <Input required name="tipo" value={form.tipo} placeholder="Tipo" onChange={onChange} 
-                            iconclass={"far fa-window-maximize"}/>
-                        <span className="form-text text-muted">Por favor, ingresa el tipo </span>
+            <Form id="form-tipo"
+                onSubmit = { 
+                    (e) => {
+                        e.preventDefault(); 
+                        validateAlert(onSubmit, e, 'form-tipo')
+                    }
+                }
+                { ... props}>
+                <div className="form-group row form-group-marginless pt-4">
+                    <div className="col-md-12">
+                        <Input                             
+                            requirevalidation={1}
+                            formeditado={formeditado} 
+                            name="tipo" 
+                            value={form.tipo} 
+                            placeholder="Tipo" 
+                            onChange={onChange} 
+                            iconclass={"far fa-window-maximize"}
+                            messageinc="Incorrecto. Ingresa el tipo de contrato."
+                        />
                     </div>
                 </div>
                 <div className="mt-3 text-center">
