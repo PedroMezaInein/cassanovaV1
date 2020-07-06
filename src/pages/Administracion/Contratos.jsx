d3703f39f9d109835ee68d1109ebf757627b12ef
OmarAbaroa <alvaro1295@hotmail.com>
2020-07-06 18:03:08 -0500

    Contrato modal

--- src/pages/Administracion/Contratos.jsx
+++ src/pages/Administracion/Contratos.jsx
@@ -212,6 +212,19 @@ class Contratos extends Component {
         })
     }
 
+    handleCloseModal = () => {
+        const { modal } = this.state
+        modal.adjuntos = false
+        this.setState({
+            ... this.state,
+            form: this.clearForm(),
+            modal,
+            tipo: 'Cliente',
+            adjuntos: [],
+            contrato: ''
+        })
+    }
+
     handleCloseModalDelete = () => {
         const { modal } = this.state
         modal.delete = false
@@ -226,51 +239,43 @@ class Contratos extends Component {
 
     openModalAdjuntos = contrato => {
         const { modal, data, adjuntos, form} = this.state
-        modal.adjuntos = true
-        if(contrato.empresa){
-            form.empresa = contrato.empresa.id.toString()
-        }
-        if(contrato.cliente){
-            form.cliente = contrato.cliente.id.toString()
-        }
-        form.fechaInicio = new Date(contrato.fecha_inicio)
-        form.fechaFin = new Date(contrato.fecha_fin)
-        form.descripcion = contrato.descripcion
-        if(contrato.tipo_contrato)
-            form.tipoContrato = contrato.tipo_contrato.id.toString()
-        form.monto = contrato.monto
-        form.nombre = contrato.nombre
-        let aux = []
-        if(contrato.adjuntos)
-            contrato.adjuntos.map( (adj) => {
-                aux.push(
-                    {
-                        name: adj.name, url: adj.url
-                    }
-                )
-            })
+        modal.adjuntos = true   
         data.adjuntos = contrato.adjuntos
-        let textoTabla = []
-        contrato.adjuntos.map((documento) => {
-            textoTabla.push({
-                actions: this.setActionsAdjuntos(documento),
-                adjunto: renderToString(setTextTable(documento.name)),
-                id: documento.id
-            }) 
-        }) 
-
         this.setState({
             ... this.state,
-            modal, 
-            adjuntos:textoTabla,
-            data
+            modal,
+            data,
+            adjuntos: this.setAdjuntos(contrato.adjuntos),
+            contrato: contrato
         })
     }
 
-    onSubmitAdjuntos = contrato => {
-        this.updateContratoAxios()
+    handleCloseAdjuntos = () =>{
+        const { modal } = this.state
+        modal.adjuntos = false
+        this.setState({
+            ... this.state,
+            modal,
+            adjuntos: []
+        })
+    }
 
+    setAdjuntos = adjuntos =>{
+        let aux = []
+        adjuntos.map((documento) => {
+            aux.push({
+                actions: this.setActionsAdjuntos(documento),
+                adjunto: renderToString(setArrayTable([{text: documento.name, url: documento.url}])),
+                id: documento.id
+            }) 
+        })
+        return aux
+    }
 
+    onSubmitAdjuntos = e => {
+        e.preventDefault()
+        waitAlert()
+        this.addAdjuntoContratoAxios()
     }
 
     onChange = e => {
@@ -389,7 +394,7 @@ class Contratos extends Component {
         return aux
     } 
     
-    openModalDeleteAdjunto = (contrato) => {
+    openModalDeleteAdjunto = (adjunto) => {
         swal({
             title: '¿Estás seguro?',
             icon: 'warning',
@@ -411,7 +416,7 @@ class Contratos extends Component {
             }
         }).then((result) => {
             if (result) {
-                this.deleteContratoAxios(contrato.id)                
+                this.deleteAdjuntoContratoAxios(adjunto.id)                
             }            
         })
     } 
@@ -582,6 +587,65 @@ class Contratos extends Component {
         })
     }
 
+    async addAdjuntoContratoAxios(){
+        const { access_token } = this.props.authUser
+        const { form, contrato } = this.state
+        const data = new FormData();
+        
+        let aux = Object.keys(form.adjuntos)
+        aux.map( (element) => {
+            if(form.adjuntos[element].value !== ''){
+                for (var i = 0; i < form.adjuntos[element].files.length; i++) {
+                    data.append(`files_name_${element}[]`, form.adjuntos[element].files[i].name)
+                    data.append(`files_${element}[]`, form.adjuntos[element].files[i].file)
+                }
+                data.append('adjuntos[]', element)
+            }
+        })
+
+        await axios.post(URL_DEV + 'contratos/' + contrato.id +'/adjunto/', data, { headers: {Accept: '*/*', 'Content-Type': 'multipart/form-data', Authorization:`Bearer ${access_token}`}}).then(
+            (response) => {
+                const { contratosClientes, contratosProveedores, contrato } = response.data
+                const { data, contratos, modal } = this.state
+
+                swal({
+                    title: '¡Felicidades 🥳!',
+                    text: response.data.message !== undefined ? response.data.message : 'El contrato fue registrado con éxito.',
+                    icon: 'success',
+                    timer: 1500,
+                    buttons: false
+                })
+
+                data.contratos.proveedores = contratosProveedores
+                contratos.proveedores = this.setContratos(contratosProveedores, 'Proveedor')
+                data.contratos.clientes = contratosClientes
+                contratos.clientes = this.setContratos(contratosClientes, 'Cliente')
+                data.adjuntos = contrato.adjuntos
+
+                modal.form = false
+                
+                this.setState({
+                    ... this.state,
+                    data,
+                    contratos,
+                    modal,
+                    adjuntos: this.setAdjuntos(contrato.adjuntos)
+                })
+            },
+            (error) => {
+                console.log(error, 'error')
+                if(error.response.status === 401){
+                    forbiddenAccessAlert()
+                }else{
+                    errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
+                }
+            }
+        ).catch((error) => {
+            errorAlert('Ocurrió un error desconocido catch, intenta de nuevo.')
+            console.log(error, 'error')
+        })
+    }
+
     async updateContratoAxios(){
         const { access_token } = this.props.authUser
         const { form, contrato } = this.state
@@ -672,6 +736,52 @@ class Contratos extends Component {
         })
     }
 
+    async deleteAdjuntoContratoAxios(adjunto){
+        const { access_token } = this.props.authUser
+        const { form, contrato } = this.state
+        await axios.delete(URL_DEV + 'contratos/' + contrato.id +'/adjunto/' + adjunto, { headers: {Authorization:`Bearer ${access_token}`}}).then(
+            (response) => {
+                const { contratosClientes, contratosProveedores, contrato } = response.data
+                const { data, contratos, modal } = this.state
+
+                swal({
+                    title: '¡Felicidades 🥳!',
+                    text: response.data.message !== undefined ? response.data.message : 'El contrato fue registrado con éxito.',
+                    icon: 'success',
+                    timer: 1500,
+                    buttons: false
+                })
+
+                swal.close()
+
+                data.contratos.proveedores = contratosProveedores
+                contratos.proveedores = this.setContratos(contratosProveedores, 'Proveedor')
+                data.contratos.clientes = contratosClientes
+                contratos.clientes = this.setContratos(contratosClientes, 'Cliente')
+                
+                this.setState({
+                    ... this.state,
+                    data,
+                    contratos,
+                    contrato: '',
+                    modal,
+                    adjuntos: this.setAdjuntos(contrato.adjuntos)
+                })
+            },
+            (error) => {
+                console.log(error, 'error')
+                if(error.response.status === 401){
+                    forbiddenAccessAlert()
+                }else{
+                    errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
+                }
+            }
+        ).catch((error) => {
+            errorAlert('Ocurrió un error desconocido catch, intenta de nuevo.')
+            console.log(error, 'error')
+        })
+    }
+
     render() {
         const { data, contratos, title, options, form, modal, tipo, formeditado, adjuntos} = this.state
         return (
@@ -732,7 +842,7 @@ class Contratos extends Component {
                 </Modal>
                 <ModalDelete title={tipo === 'Cliente' ? '¿Quieres eliminar el contrato de cliente?' : '¿Quieres eliminar el contrato de proveedor?'} show = { modal.delete } handleClose = { this.handleCloseModalDelete } onClick=  { (e) => { e.preventDefault(); waitAlert(); this.deleteContratoAxios() }}>
                 </ModalDelete>
-                <Modal title = { 'Adjuntos del contrato' } show = { modal.adjuntos } handleClose = { this.handleCloseModal }>
+                <Modal title = 'Adjuntos del contrato' show = { modal.adjuntos } handleClose = { this.handleCloseModal }>
                     <Form id="form-adjuntos"
                         onSubmit = { 
                             (e) => {
