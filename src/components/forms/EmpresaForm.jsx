479a823ef6d80e0aaff88dfa186339597e6c5cdb
CariJG <64931832+CariJG@users.noreply.github.com>
2020-07-09 11:55:14 -0500

    Se agregó validaciones

--- src/components/forms/EmpresaForm.jsx
+++ src/components/forms/EmpresaForm.jsx
@@ -4,6 +4,9 @@ import { Input,InputImage, Button, Select} from '../form-components'
 import { Subtitle, P } from '../texts'
 import { faTimesCircle, faClipboard, faCameraRetro, faCamera } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { validateAlert } from '../../functions/alert'
+import { RFC} from '../../constants'
+
 class EmpresaForm extends Component{
 
     constructor(props){
@@ -14,51 +17,95 @@ class EmpresaForm extends Component{
     }
     
     render(){
-        const { form, onChange, title, img, removeFile} = this.props
+        const { form, onChange, title, img, removeFile,onSubmit, formeditado} = this.props
         return(
-            <Form { ... this.props}>
+            <Form id="form-empresa"
+                onSubmit = { 
+                    (e) => {
+                        e.preventDefault(); 
+                        validateAlert(onSubmit, e, 'form-empresa')
+                    }
+                }
+                { ... this.props}
+                >
                     <div className="form-group row form-group-marginless">
                         <div className="col-md-4">
-                            <Input onChange={ onChange } required name="name" type="text" value={ form.name } placeholder="Nombre" iconclass={"far fa-user"}/>
-                            <span className="form-text text-muted">Por favor, ingrese su nombre. </span>
+                            <Input 
+                                requirevalidation={1}
+                                formeditado={formeditado}
+                                onChange={ onChange } 
+                                name="name" 
+                                type="text" 
+                                value={ form.name } 
+                                placeholder="Nombre" 
+                                iconclass={"far fa-user"}
+                                messageinc="Incorrecto. Ingresa el nombre de la empresa"
+                                />
+                            {/* <span className="form-text text-muted">Por favor, ingrese su nombre. </span> */}
                         </div>
                         <div className="col-md-4">
-                            <Input onChange={ onChange } required name="razonSocial" type="text" value={ form.razonSocial } placeholder="Razón social" iconclass={"far fa-building"}/>
-                            <span className="form-text text-muted">Por favor, ingrese su razón social. </span>
-                        </div>
-                        <div className="col-md-4">
-                            <Input onChange={ onChange } required name="rfc" type="text" value={ form.rfc } placeholder="RFC" iconclass={"fas fa-users-cog"}/>
-                            <span className="form-text text-muted">Por favor, ingrese su RFC. </span>
-                        </div>
-                        <div className="col-md-4">
-                        <div className="image-upload">
-                            <Input
+                            <Input 
+                                requirevalidation={1}
+                                formeditado={formeditado}
                                 onChange={ onChange }
-                                name="logo" 
-                                type="file" 
-                                value={ form.logo }
-                                id="logo"
-                                placeholder="Logo de la empresa"
+                                name="razonSocial" 
+                                type="text" 
+                                value={ form.razonSocial } 
+                                placeholder="Razón social" 
+                                iconclass={"far fa-building"}
+                                messageinc="Incorrecto. Ingresa la razón social"
                             />
-                                <span className="form-text text-muted">Por favor, adjunte su logo. </span>
-                                <label htmlFor="logo">
-                                <FontAwesomeIcon icon={faCamera} />
-                                </label>
+                            {/* <span className="form-text text-muted">Por favor, ingrese su razón social. </span> */}
                         </div>
-                        {
-                            (img === '' ) && (form.file !== '' && form.file !== undefined && form.file !== null) &&
-                                <div className="p-3 position-relative">
-                                    <img className='max-h-70px' src={form.file } />
+                        <div className="col-md-4">
+                            <Input 
+                                requirevalidation={1}
+                                formeditado={formeditado}
+                                onChange={ onChange } 
+                                name="rfc" 
+                                type="text" 
+                                value={ form.rfc } 
+                                placeholder="RFC" 
+                                iconclass={"far fa-file-alt"}
+                                patterns={RFC}
+                                messageinc="Incorrecto. Ej. ABCD001122ABC"
+                                maxLength="13"
+                                />
+                            {/* <span className="form-text text-muted">Por favor, ingrese su RFC. </span> */}
+                        </div>
+                        <div className="separator separator-dashed mt-1 mb-2"></div>
+                        <div className="form-group row form-group-marginless mt-4 pl-3">
+                            <div className="col-md-4">
+                                <div className="image-upload">
+                                    <input
+                                        onChange={ onChange }
+                                        formeditado={formeditado}
+                                        name="logo" 
+                                        type="file" 
+                                        value={ form.logo }
+                                        id="logo"
+                                        placeholder="Logo de la empresa"
+                                    />
+                                        <span className="form-text text-muted">Por favor, adjunte su logo. </span>
+                                        <label htmlFor="logo">
+                                        <FontAwesomeIcon icon={faCamera} />
+                                        </label>
                                 </div>
-                        }
-                        {
-                            form.logo &&
-                                <div className="p-3 position-relative">
-                                    <img value={img} className='max-h-70px' src={ img } />
-                                    <Button className="position-absolute delete-item " 
-                                        onClick={removeFile} text='' icon={faTimesCircle}/>
-                                </div>
-                        }
+                            {
+                                (img === '' ) && (form.file !== '' && form.file !== undefined && form.file !== null) &&
+                                    <div className="p-3 position-relative">
+                                        <img className='max-h-70px' src={form.file } />
+                                    </div>
+                            }
+                            {
+                                form.logo &&
+                                    <div className="p-3 position-relative">
+                                        <img value={img} className='max-h-70px' src={ img } />
+                                        <Button className="position-absolute delete-item " 
+                                            onClick={removeFile} text='' icon={faTimesCircle}/>
+                                    </div>
+                            }
+                            </div>
                         </div>
                     </div>
                 
