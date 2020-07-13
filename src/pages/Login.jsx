7f4181823af5ff6bd8ba10803e1e402da550588a
CariJG <64931832+CariJG@users.noreply.github.com>
2020-07-13 16:27:36 -0500

    Se agregaron links

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,10 +27,14 @@ class Login extends Component{
                         <div className="d-flex flex-center bgi-size-cover bgi-no-repeat flex-row-fluid" style ={ { backgroundImage: "url('/1.jpg')" } }>
                             <div className="row d-flex flex-center ">
                                 <div className="d-flex flex-center mb-3">
-                                    <a href="#" >
-                                        <img src="/inein.png" className="img-empresa  mr-5" alt="" />
-                                        <img src="/im.png" className="img-empresa ml-5 mr-5" alt="" />
-                                        <img src="/Rocco.png" className="img-empresa ml-5" alt="" style={{height:"3rem"}} />
+                                    <a href="https://inein.mx/" target="_blank" >
+                                        <img src="/inein.png" className="img-empresa  mr-5"/>
+                                    </a>
+                                    <a href="https://infraestructuramedica.mx/" target="_blank">
+                                        <img src="/im.png" className="img-empresa ml-5 mr-5"/>
+                                    </a>
+                                    <a href="https://www.rocco.mx/" target="_blank">
+                                        <img src="/Rocco.png" className="img-empresa ml-5"/>
                                     </a>
                                 </div>
                                 <div className="login-form text-center text-white p-7 position-relative overflow-hidden pt-4">
