0be55a55f663326e5976f67b0fd2753ba3b0853f
OmarAbaroa <alvaro1295@hotmail.com>
2020-07-15 17:30:38 -0500

    Tareas iterar tableros

--- src/pages/Usuarios/Tareas.jsx
+++ src/pages/Usuarios/Tareas.jsx
@@ -28,6 +28,7 @@ class Tareas extends Component{
 
     state = {
         columns:[],
+        tableros: [],
         user : '',
         users: '',
         activeKey: '',
@@ -315,12 +316,15 @@ class Tareas extends Component{
                 const { data : { tareas : columns } } = response
                 const { data : { user : user } } = response
                 const { data : { users : users } } = response
+                const { tableros } = response.data
+                console.log(tableros, 'tableros')
                 this.setState({
                     ... this.state,
                     user: user,
-                    users: users
+                    users: users,
+                    tableros: tableros
                 })
-                this.setTareas(columns)
+                this.setTareas(tableros[0].tareas)
             },
             (error) => {
                 console.log(error, 'error')
@@ -731,7 +735,7 @@ class Tareas extends Component{
                     <Card className="card-custom card-stretch"> 
                         <Card.Body className="px-3">
                         <Nav className="navi navi-hover navi-active navi-link-rounded navi-bold navi-icon-center navi-light-icon">
-							<Nav.Item className="navi-item my-2">
+							{/* <Nav.Item className="navi-item my-2">
 								<Nav.Link className="navi-link active pl-2">
 									<span className="navi-icon mr-2">
                                         <span className="svg-icon svg-icon-lg">
@@ -810,7 +814,31 @@ class Tareas extends Component{
 									</span>
 								<span className="navi-text font-weight-bolder font-size-lg" id="tablero">MERCADOTECNIA</span>
 								</Nav.Link>
-							</Nav.Item>
+							</Nav.Item> */}
+                            {
+                                this.state.tableros.map( (tablero, key) => {
+                                    return(
+                                        <div key={key}>
+                                            <Nav.Item className="navi-item my-2">
+                                                                            <Nav.Link className="navi-link pl-2">
+                                                                                <span className="navi-icon mr-2">
+                                                                                    <span className="svg-icon svg-icon-lg">
+                                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                                                            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                                                                                <rect x="0" y="0" width="24" height="24"></rect>
+                                                                                                <path d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z" fill="#000000" fillRule="nonzero"></path>
+                                                                                                <path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3"></path>
+                                                                                            </g>
+                                                                                        </svg>
+                                                                                    </span>
+                                                                                </span>
+                                                                            <span className="navi-text font-weight-bolder font-size-lg" id="tablero">{tablero.nombre}</span>
+                                                                            </Nav.Link>
+                                                                        </Nav.Item> 
+                                        </div>
+                                    )
+                                })
+                            }
 						</Nav>
                         </Card.Body>
                     </Card> 
