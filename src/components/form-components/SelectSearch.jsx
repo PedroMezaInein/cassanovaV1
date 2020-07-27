052b9123725dac8fadc6feb9ec05cb8de6266b06
CariJG <64931832+CariJG@users.noreply.github.com>
2020-07-27 15:55:02 -0500

    Se dio formato al documento

--- src/components/form-components/SelectSearch.jsx
+++ src/components/form-components/SelectSearch.jsx
@@ -3,73 +3,73 @@ import SelectSearch from 'react-select-search'
 import Input from './Input'
 
 import '../../styles/select_custom.css';
-class SelectSearchForm extends Component{
+class SelectSearchForm extends Component {
 
     state = {
         requirevalidation: true
     }
 
-    renderFontValue = (valueProps,onChange) => {  
+    renderFontValue = (valueProps, onChange) => {
         const { requirevalidation } = this.state
-        const {customstyle} = this.props
+        const { customstyle } = this.props
 
         let validado = false;
-        if(requirevalidation){
-            if( onChange === null || onChange.value === null ){
-                validado=false;
-            }else{
-                validado=true;
+        if (requirevalidation) {
+            if (onChange === null || onChange.value === null) {
+                validado = false;
+            } else {
+                validado = true;
             }
-        }else{
+        } else {
             validado = true
         }
 
-        return (           
-            <> 
-                <div className="input-icon">       
+        return (
+            <>
+                <div className="input-icon">
                     <span className="input-icon input-icon-right">
-                        <i className={"flaticon2-search-1 m-0 kt-font-boldest text-primary"}></i> 
-                    </span>  
-                    <input   
-                        className={ validado ? " form-control is-valid text-uppercase" : " form-control is-invalid text-uppercase" }   
+                        <i className={"flaticon2-search-1 m-0 kt-font-boldest text-primary"}></i>
+                    </span>
+                    <input
+                        className={validado ? " form-control is-valid text-uppercase" : " form-control is-invalid text-uppercase"}
                         {...valueProps}
                         style={customstyle}
-                        />
+                    />
                 </div>
-                <span className={ validado ? "form-text text-danger hidden" : "form-text text-danger" }> Selecciona una opción </span>
-            </>               
+                <span className={validado ? "form-text text-danger hidden" : "form-text text-danger"}> Selecciona una opción </span>
+            </>
         );
     }
 
-    componentDidMount(){
-        
+    componentDidMount() {
+
     }
 
-    render(){
-        
-        const { options,  placeholder, iconclass} = this.props 
-        return(
+    render() {
+
+        const { options, placeholder, iconclass } = this.props
+        return (
             <>
-                { 
+                {
                     options.length > 0 ?
-                        <>  
-                                <label className="col-form-label">{placeholder}</label> 
-                                        <SelectSearch 
-                                        renderValue={this.renderFontValue}   
-                                        search 
-                                        {...this.props}
-                                        />   
+                        <>
+                            <label className="col-form-label">{placeholder}</label>
+                            <SelectSearch
+                                renderValue={this.renderFontValue}
+                                search
+                                {...this.props}
+                            />
                         </>
-                    : 
-                    <Input 
-                    iconclass={iconclass}
-                    readOnly 
-                    type="text"
-                    { ...this.props }/>
+                        :
+                        <Input
+                            iconclass={iconclass}
+                            readOnly
+                            type="text"
+                            {...this.props} />
                 }
-                
+
             </>
-            
+
         )
     }
 
