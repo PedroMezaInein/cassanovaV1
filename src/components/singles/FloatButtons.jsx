8fd1a2106e7861e728c895ca208052903302f512
CariJG <64931832+CariJG@users.noreply.github.com>
2020-07-31 14:13:26 -0500

    Se cambio diseño

--- src/components/singles/FloatButtons.jsx
+++ src/components/singles/FloatButtons.jsx
@@ -1,33 +1,36 @@
 import React, { Component } from 'react'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
 
-class FloatButtons extends Component{
-    render(){
+class FloatButtons extends Component {
+    render() {
         const { save, recover, formulario, url } = this.props
 
-        return(
-            <div className="position-fixed float-button d-flex align-items-center">
-                <div>
-                    <button onClick={ save } className="ml-2 my-2 btn btn-actions-table btn-md btn-icon btn-bg-white btn-text-success btn-hover-success" 
-                        title='Guardar'>
-                        <i className='fas fa-save'></i>
-                    </button>
-                    <br />
-                    {
-                        url === formulario.page ?
-                            Object.keys(formulario.form).length > 0 ?
-                                <button onClick={ recover } className="ml-2 my-2 btn btn-actions-table btn-md btn-icon btn-bg-white btn-text-primary btn-hover-primary" 
-                                    title='Recuperar formulario'>
-                                    <i className='fas fa-clipboard-list'></i>
-                                </button>  
+        return (
+            <ul className="sticky-toolbar nav flex-column pl-2 pr-2 pt-3 pb-2 mt-4">
+                <OverlayTrigger overlay={<Tooltip>Guardar</Tooltip>}>
+                    <li className="nav-item mb-2" data-placement="right">
+                        <a className="btn btn-sm btn-icon btn-bg-light btn-text-success btn-hover-success" onClick={save}>
+                            <i className="far fa-save"></i>
+                        </a>
+                    </li>
+                </OverlayTrigger>
+                {
+                    url === formulario.page ?
+                        Object.keys(formulario.form).length > 0 ?
+                            <OverlayTrigger overlay={<Tooltip>Recuperar formulario</Tooltip>}>
+                                <li className="nav-item mb-2" title="" data-placement="left">
+                                    <a className="btn btn-sm btn-icon btn-bg-light btn-text-primary btn-hover-primary" onClick={recover}>
+                                        <i className="flaticon2-list-3"></i>
+                                    </a>
+                                </li>
+                            </OverlayTrigger>
                             : ''
 
                         : ''
-                    }
-                      
-                </div>
-            </div>
+                }
+            </ul>
         )
     }
 }
-
 export default FloatButtons
