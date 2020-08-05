2bb407084086ab12c5e6e1f960bacf9f65995262
CariJG <64931832+CariJG@users.noreply.github.com>
2020-08-05 16:55:44 -0500

    Se cambio onChange

--- src/pages/Presupuesto/ActualizarPresupuesto.jsx
+++ src/pages/Presupuesto/ActualizarPresupuesto.jsx
@@ -11,51 +11,21 @@ import { ActualizarPresupuestoForm } from "../../components/forms";
 class ActualizarPresupuesto extends Component {
     state = {
         formeditado: 0,
-        modal: {
-            form: false,
-            delete: false,
-            adjuntos: false,
-        },
-        title: "Nuevo presupuesto",
         form: {
-            proyecto: "",
-            area: "",
-            empresa: "",
-            fecha: new Date(),
-            tiempo_ejecucion: "",
-            partida: "",
-            subpartida: "",
-            conceptos: {}
-        },
-        options: {
-            proyectos: [],
-            empresas: [],
-            areas: [],
-            partidas: [],
-            subpartidas: [],
-        },
-        data: {
-            partidas: [],
-            subpartidas: [],
-            conceptos: []
-        },
+            conceptos: [{
+                descipcion: '',
+                costo: '',
+                cantidad_preliminar: '',
+                desperdicio: '',
+                active:true
+            }]
+        }
     };
 
     componentDidMount() {
-        const {
-            authUser: {
-                user: { permisos: permisos },
-            },
-        } = this.props;
-        const {
-            history: {
-                location: { pathname: pathname },
-            },
-        } = this.props;
-        const {
-            history,
-            location: { state: state },
-        } = this.props;
+        const { authUser: { user: { permisos: permisos } } } = this.props;
+        const { history: { location: { pathname: pathname } } } = this.props;
+        const { history, location: { state: state } } = this.props;
 
         const presupuesto = permisos.find(function (element, index) {
             const {
@@ -65,145 +35,28 @@ class ActualizarPresupuesto extends Component {
         });
 
         if (!presupuesto) history.push("/");
-        this.getOptionsAxios();
     }
 
-    setOptions = (name, array) => {
-        const { options } = this.state;
-        options[name] = setOptions(array, "nombre", "id");
+    onChange = (key, e, name) => {
+        const { value } = e.target
+        const { form} = this.state
+        form['conceptos'][key][name] = value
         this.setState({
             ...this.state,
-            options,
-        });
-    };
-
-    async getOptionsAxios() {
-        waitAlert();
-        const { access_token } = this.props.authUser;
-        await axios
-            .get(URL_DEV + "presupuestos/options", {
-                responseType: "json",
-                headers: {
-                    Accept: "*/*",
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json;",
-                    Authorization: `Bearer ${access_token}`,
-                },
-            })
-            .then(
-                (response) => {
-                    swal.close();
-                    const { empresas, proyectos, areas, partidas, conceptos } = response.data;
-                    const { options, data, form } = this.state;
-                    data.partidas = partidas
-                    let aux = {}
-                    conceptos.map((concepto) => {
-                        aux[concepto.clave] = false
-                    })
-                    form.conceptos = aux;
-                    options["proyectos"] = setOptions(proyectos, "nombre", "id");
-                    options["empresas"] = setOptions(empresas, "name", "id");
-                    options["areas"] = setOptions(areas, "nombre", "id");
-                    options["partidas"] = setOptions(partidas, "nombre", "id");
-
-                    this.setState({
-                        ...this.state,
-                        options,
-                    });
-                },
-                (error) => {
-                    console.log(error, "error");
-                    if (error.response.status === 401) {
-                        forbiddenAccessAlert();
-                    } else {
-                        errorAlert(
-                            error.response.data.message !== undefined
-                                ? error.response.data.message
-                                : "Ocurrió un error desconocido, intenta de nuevo."
-                        );
-                    }
-                }
-            )
-            .catch((error) => {
-                errorAlert("Ocurrió un error desconocido catch, intenta de nuevo.");
-                console.log(error, "error");
-            });
-    }
-
-    async addPresupuestosAxios() {
-        const { access_token } = this.props.authUser
-        const { form } = this.state
-        const data = new FormData();
-
-        await axios.post(URL_DEV + 'presupuesto', data, { headers: { Accept: '*/*', 'Content-Type': 'multipart/form-data', Authorization: `Bearer ${access_token}` } }).then(
-            (response) => {
-                this.setState({
-                    ... this.state,
-                    modal: false,
-                    form: this.clearForm()
-                })
-                swal({
-                    title: '¡Felicidades 🥳!',
-                    text: response.data.message !== undefined ? response.data.message : 'El egreso fue registrado con éxito.',
-                    icon: 'success',
-                    timer: 1500,
-                    buttons: false,
-                })
-                const { history } = this.props
-                history.push({
-                    pathname: '/presupuesto/presupuesto'
-                });
-            },
-            (error) => {
-                console.log(error, 'error')
-                if (error.response.status === 401) {
-                    forbiddenAccessAlert()
-                } else {
-                    errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
-                }
-            }
-        ).catch((error) => {
-            errorAlert('Ocurrió un error desconocido catch, intenta de nuevo.')
-            console.log(error, 'error')
+            form
         })
     }
 
-    onChange = (e) => {
-        const { name, value } = e.target;
-        const { data } = this.state
-        switch (name) {
-            case 'partida':
-                data.partidas.map((partida) => {
-                    data.conceptos = []
-                    if (partida.id.toString() === value) {
-                        data.subpartidas = partida.subpartidas
-                    }
-                })
-                break;
-            case 'subpartida':
-                data.subpartidas.map((subpartida) => {
-                    if (subpartida.id.toString() === value) {
-                        data.conceptos = subpartida.conceptos
-                    }
-                })
-                break;
-            default:
-                break;
-        }
-
-        const { form } = this.state;
-        form[name] = value;
-        this.setState({
-            ...this.state,
-            form,
-            data
-        });
-    };
-
-    checkButton = e => {
+    checkButton = (key, e) => {
         const { name, value, checked } = e.target
         const { form } = this.state
-        form.conceptos[name] = checked
+
+        console.log(form.conceptos)
+        console.log(name, 'name')
+        console.log(checked, 'checked')
+        console.log(key, 'key')
+
+        form.conceptos[key][name]= !checked
         this.setState({
             ... this.state,
             form
@@ -223,15 +76,11 @@ class ActualizarPresupuesto extends Component {
             <Layout active={"presupuesto"} {...this.props}>
                 <ActualizarPresupuestoForm
                     formeditado={formeditado}
-                    title={title}
                     form={form}
                     onChange={this.onChange}
                     checkButton={this.checkButton}
-                    options={options}
-                    setOptions={this.setOptions}
                     onSubmit={this.onSubmit}
-                    data={data}
-                /* {... this.props} */
+                    {...this.props}
                 />
             </Layout>
         );
