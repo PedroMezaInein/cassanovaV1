336e6a72a79cdabdf7b0798f8436f65ad6993b1a
OmarAbaroa <alvaro1295@hotmail.com>
2020-08-26 16:30:59 -0500

    Calendario

--- src/pages/Perfil/Calendario.jsx
+++ src/pages/Perfil/Calendario.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Layout from '../../components/layout/layout';
+import axios from 'axios';
 
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -11,6 +12,9 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Tooltip from 'react-bootstrap/Tooltip'
 import { Modal } from '../../components/singles'
 import { SolicitarVacacionesForm } from "../../components/forms";
+import { errorAlert, forbiddenAccessAlert, waitAlert, doneAlert } from '../../functions/alert';
+import { URL_DEV } from '../../constants';
+
 class Calendario extends Component {
 
     state = {
@@ -22,6 +26,18 @@ class Calendario extends Component {
             fechaFin: new Date(),
         }
     };
+
+    componentDidMount(){
+        const { authUser: { user : { permisos : permisos } } } = this.props
+        const { history : { location: { pathname: pathname } } } = this.props
+        const { match : { params: { action: action } } } = this.props
+        const { history, location: { state: state} } = this.props
+        const remisiones = permisos.find(function(element, index) {
+            const { modulo: { url: url } } = element
+            return pathname === url
+        });
+        this.getVacaciones()
+    }
 
     handleDateClick = (arg) => { // bind with an arrow function
         console.log(arg)
@@ -75,6 +91,80 @@ class Calendario extends Component {
         })
     }
 
+    async askVacationAxios(){
+        const { access_token } = this.props.authUser
+        const { form } = this.state
+        await axios.post(URL_DEV + 'vacaciones', form, { headers: { Authorization: `Bearer ${access_token}` } }).then(
+            (response) => {
+                doneAlert(response.data.message !== undefined ? response.data.message : 'Vacaciones solicitadas con éxito.')
+            },
+            (error) => {
+                console.log(error, 'error')
+                if(error.response.status === 401){
+                    forbiddenAccessAlert()
+                }else{
+                    errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
+                }
+            }
+        ).catch((error) => {
+            errorAlert('Ocurrió un error desconocido catch, intenta de nuevo.')
+            console.log(error, 'error')
+        })
+    }
+
+    async getVacaciones() {
+        const { access_token } = this.props.authUser
+        await axios.get(URL_DEV + 'vacaciones', { headers: { Authorization: `Bearer ${access_token}` } }).then(
+            (response) => {
+                const { empleados, vacaciones } = response.data
+                let aux = []
+                let mes = ''
+                let dia = ''
+                let año = new Date().getFullYear();
+                empleados.map( (empleado, key) => {
+                    mes = empleado.rfc.substr(6,2);
+                    dia = empleado.rfc.substr(8,2);
+                    for(let x = -5; x <= 5; x++){
+                        aux.push({
+                            title: empleado.nombre,
+                            start: Number(Number(año) + Number(x))+'-'+mes+'-'+dia,
+                            end: Number(Number(año) + Number(x))+'-'+mes+'-'+dia,
+                            iconClass: 'fas fa-birthday-cake',
+                            containerClass: 'cumpleaños'
+                        })
+                    }
+                })
+                vacaciones.map( (vacacion) => {
+                    if(vacacion.estatus === 'Aceptado')
+                        aux.push({
+                            title: vacacion.empleado.nombre,
+                            start: vacacion.fecha_inicio,
+                            end: vacacion.fecha_fin,
+                            iconClass: 'fas fa-umbrella-beach',
+                            containerClass: 'vacaciones'
+                        })
+                })
+
+                this.setState({
+                    ... this.state,
+                    events: aux
+                })
+
+            },
+            (error) => {
+                console.log(error, 'error')
+                if(error.response.status === 401){
+                    forbiddenAccessAlert()
+                }else{
+                    errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
+                }
+            }
+        ).catch((error) => {
+            errorAlert('Ocurrió un error desconocido catch, intenta de nuevo.')
+            console.log(error, 'error')
+        })
+    }
+
     render() {
         const { events, form, title, formeditado, modal, key} = this.state
         return (
@@ -97,6 +187,7 @@ class Calendario extends Component {
                             formeditado={formeditado}
                             form={form}
                             onChange={this.onChange}
+                            onSubmit = { (e) => { e.preventDefault(); waitAlert(); this.askVacationAxios()}}
                         />
                     </Modal>
                     <Card.Body>
@@ -120,8 +211,8 @@ class Calendario extends Component {
 function renderEventContent(eventInfo) {
     console.log(eventInfo)
     return (
-        <div className="event">
-            <i className={eventInfo.event._def.extendedProps.iconClass+" kt-font-boldest"}></i> 
+        <div className={eventInfo.event._def.extendedProps.containerClass + ' evento'}>
+            <i className={eventInfo.event._def.extendedProps.iconClass+" kt-font-boldest mr-3"}></i> 
             <span>{eventInfo.event.title}</span>
         </div>
     )
