66efee572f7594f6b82a7c1743986c3b3bc3f556
CariJG <64931832+CariJG@users.noreply.github.com>
2020-08-28 17:16:23 -0500

    Se dio formato

--- src/components/forms/perfil/EstatusForm.jsx
+++ src/components/forms/perfil/EstatusForm.jsx
@@ -5,7 +5,7 @@ import { setDateTableLG } from '../../../functions/setters';
 class EstatusForm extends Component {
 
     render() {
-        const { onSubmit, estatus} = this.props
+        const { onSubmit, estatus } = this.props
         console.log(estatus)
         return (
             <Form id="form-estatus"
@@ -16,51 +16,49 @@ class EstatusForm extends Component {
                     }
                 }
             >
-                <div className="tab-content mt-4" >
-                    <div className="table-responsive">
-                        <table className="table table-head-custom table-head-bg table-borderless table-vertical-center">
-                            <thead>
-                                <tr className="text-center">
-                                    <th style={{ minWidth: "100px" }}>
-                                        <span className="text-dark-75">Fecha de inicio</span>
-                                    </th>
-                                    <th style={{ minWidth: "100px" }}>
-                                        <span className="text-dark-75">Fecha final</span>
-                                    </th>
-                                    <th style={{ minWidth: "100px" }}>
-                                        <span className="text-dark-75">Estatus</span>
-                                    </th>
-                                </tr>
-                            </thead>
-                            {
-                                estatus.map((estatus, key) => {
-                                    return (
-                                        <tbody key={key}>
-                                            <tr className="text-center">
-                                                <td>
-                                                    <span className="font-size-lg">{setDateTableLG(estatus.fecha_inicio)}</span>
-                                                </td>
-                                                <td>
-                                                    <span className="font-size-lg">{setDateTableLG(estatus.fecha_fin)}</span>
-                                                </td>
-                                                <td className="pr-0">
-                                                    {
-                                                        (estatus.estatus === "Aceptadas") ?
-                                                            <span className="label label-lg label-light-primary label-inline font-weight-bold">Aceptadas</span>
+                <div className="table-responsive mt-4">
+                    <table className="table table-head-custom table-head-bg table-borderless table-vertical-center">
+                        <thead>
+                            <tr className="text-center">
+                                <th style={{ minWidth: "100px" }}>
+                                    <span className="text-dark-75">Fecha de inicio</span>
+                                </th>
+                                <th style={{ minWidth: "100px" }}>
+                                    <span className="text-dark-75">Fecha final</span>
+                                </th>
+                                <th style={{ minWidth: "100px" }}>
+                                    <span className="text-dark-75">Estatus</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        {
+                            estatus.map((estatus, key) => {
+                                return (
+                                    <tbody key={key}>
+                                        <tr className="text-center">
+                                            <td>
+                                                <span className="font-size-lg">{setDateTableLG(estatus.fecha_inicio)}</span>
+                                            </td>
+                                            <td>
+                                                <span className="font-size-lg">{setDateTableLG(estatus.fecha_fin)}</span>
+                                            </td>
+                                            <td className="pr-0">
+                                                {
+                                                    (estatus.estatus === "Aceptadas") ?
+                                                        <span className="label label-lg label-light-primary label-inline font-weight-bold">Aceptadas</span>
+                                                        :
+                                                        (estatus.estatus === "Rechazadas") ?
+                                                            <span className="label label-lg label-light-danger label-inline font-weight-bold">Rechazadas</span>
                                                             :
-                                                            (estatus.estatus === "Rechazadas") ?
-                                                                <span className="label label-lg label-light-danger label-inline font-weight-bold">Rechazadas</span>
-                                                                :
-                                                                <span className="label label-lg label-light-warning label-inline font-weight-bold">En espera</span>
-                                                    }
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    )
-                                })
-                            }
-                        </table>
-                    </div>
+                                                            <span className="label label-lg label-light-warning label-inline font-weight-bold">En espera</span>
+                                                }
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                )
+                            })
+                        }
+                    </table>
                 </div>
             </Form>
         )
