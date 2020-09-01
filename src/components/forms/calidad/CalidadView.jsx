4fb6c00407944fa67d836a9b97e55905e409baa8
CariJG <64931832+CariJG@users.noreply.github.com>
2020-09-01 14:13:34 -0500

    Se agregaron las partidas de data

--- src/components/forms/calidad/CalidadView.jsx
+++ src/components/forms/calidad/CalidadView.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Tooltip from 'react-bootstrap/Tooltip'
-
+import Moment from 'react-moment'
 
 class CalidadView extends Component {
 
     getIniciales = nombre => {
         let aux = nombre.split(' ');
         let iniciales = ''
-        aux.map((element)=>{
-            if(element !== '-')
+        aux.map((element) => {
+            if (element !== '-')
                 iniciales = iniciales + element.charAt(0) + ' '
         })
         return iniciales
@@ -25,7 +25,7 @@ class CalidadView extends Component {
                         <div className="d-flex">
                             <div className="mr-4" id="symbol_calidad">
                                 <div className="symbol symbol-50 symbol-lg-120 symbol-light-primary">
-                                    <span className="font-size-h3 symbol-label font-weight-boldest">
+                                    <span className="font-size-h6 symbol-label font-weight-boldest ">
                                         {
                                             data ?
                                                 data.proyecto ?
@@ -40,47 +40,60 @@ class CalidadView extends Component {
                             <div className="flex-grow-1">
                                 <div className="d-flex align-items-start justify-content-between flex-wrap mt-2">
                                     <div className="mr-3">
-                                        <div className="d-flex align-items-center text-dark font-size-h5 font-weight-bold mr-3"> 
+                                        <div className="d-flex align-items-center text-dark font-size-h5 font-weight-bold mr-3">
                                             {
                                                 data ?
-                                                    data.proyecto?
+                                                    data.proyecto ?
                                                         data.proyecto.nombre
                                                     : ''
                                                 : ''
                                             }
                                         </div>
-                                        <div className="d-flex flex-wrap my-2">
+                                        <div className="d-flex flex-wrap mt-2">
                                             <div className="text-muted font-weight-bold my-2">
                                                 <i className="far fa-user-circle icon-md mr-2"></i>
-                                                    {
-                                                        data ?
-                                                            data.usuario ?
-                                                                data.usuario.name
-                                                            : ''
+                                                {
+                                                    data ?
+                                                        data.usuario ?
+                                                            data.usuario.name
                                                         : ''
-                                                    }
-                                                </div>
+                                                    : ''
+                                                }
+                                            </div>
                                         </div>
                                     </div>
 
                                     <div className="mb-2">
-                                        <span className="label label-lg bg- label-inline font-weight-bold py-2">En espera</span>
+                                    {
+                                        data ?
+                                            data.estatus_ticket ?
+                                                <span className="label label-lg bg- label-inline font-weight-bold py-2" 
+                                                    style={{
+                                                        color: `${data.estatus_ticket.letra}`,
+                                                        backgroundColor: `${data.estatus_ticket.fondo}`,
+                                                        fontSize:"11.7px"
+                                                    }} >
+                                                    {data.estatus_ticket.estatus}
+                                                </span>
+                                            :''
+                                        :''
+                                    }
                                     </div>
                                 </div>
 
                                 <div className="d-flex align-items-start flex-wrap justify-content-between">
                                     <div className="font-weight-bold text-dark-50 py-lg-2 col-md-10 text-justify pl-0">
-                                            {
-                                                data ?
-                                                    data.descripcion
-                                                : ''
-                                            }
+                                        {
+                                            data ?
+                                                data.descripcion
+                                            : ''
+                                        }
                                     </div>
                                     <OverlayTrigger overlay={<Tooltip>Aceptar</Tooltip>}>
-                                        <a onClick = { () => { changeEstatus('Aceptado') } } className="btn btn-icon btn-light-success success2 btn-sm mr-2 ml-auto"><i className="flaticon2-check-mark icon-sm"></i></a>
+                                        <a onClick={() => { changeEstatus('Aceptado') }} className="btn btn-icon btn-light-success success2 btn-sm mr-2 ml-auto"><i className="flaticon2-check-mark icon-sm"></i></a>
                                     </OverlayTrigger>
                                     <OverlayTrigger overlay={<Tooltip>Rechazar</Tooltip>}>
-                                        <a onClick = { () => { changeEstatus('Rechazado') } } className="btn btn-icon  btn-light-danger btn-sm pulse pulse-danger"><i className="flaticon2-cross icon-sm"></i></a>
+                                        <a onClick={() => { changeEstatus('Rechazado') }} className="btn btn-icon  btn-light-danger btn-sm pulse pulse-danger"><i className="flaticon2-cross icon-sm"></i></a>
                                     </OverlayTrigger>
                                 </div>
                             </div>
@@ -105,7 +118,15 @@ class CalidadView extends Component {
                                         </div>
                                     </div>
                                     <div>
-                                        <div className="font-size-h6 text-dark-75 font-weight-bolder">PARTIDA</div>
+                                        <div className="font-size-h6 text-dark-75 font-weight-bolder">
+                                            {
+                                                data ?
+                                                    data.partida ?
+                                                        data.partida.nombre
+                                                    : ''
+                                                : ''
+                                            }
+                                        </div>
                                         <div className="font-size-sm text-muted font-weight-bold mt-1">PARTIDA</div>
                                     </div>
                                 </div>
@@ -126,7 +147,15 @@ class CalidadView extends Component {
                                         </div>
                                     </div>
                                     <div>
-                                        <div className="font-size-h6 text-dark-75 font-weight-bolder">MANTENIMIENTO</div>
+                                        <div className="font-size-h6 text-dark-75 font-weight-bolder">
+                                            {
+                                                data ?
+                                                    data.tipo_trabajo ?
+                                                        data.tipo_trabajo.tipo
+                                                    : ''
+                                                : ''
+                                            }
+                                        </div>
                                         <div className="font-size-sm text-muted font-weight-bold mt-1">TIPO DE TRABAJO</div>
                                     </div>
                                 </div>
@@ -147,7 +176,17 @@ class CalidadView extends Component {
                                         </div>
                                     </div>
                                     <div>
-                                        <div className="font-size-h6 text-dark-75 font-weight-bolder">09/07/2020</div>
+                                        <div className="font-size-h6 text-dark-75 font-weight-bolder">
+                                            {
+                                                data ?
+                                                    data.updated_at ?
+                                                        <Moment format="DD/MM/YYYY">
+                                                            {data.created_at}
+                                                        </Moment>
+                                                    : ''
+                                                : ''
+                                            }
+                                        </div>
                                         <div className="font-size-sm text-muted font-weight-bold mt-1">FECHA</div>
                                     </div>
                                 </div>
@@ -174,11 +213,19 @@ class CalidadView extends Component {
                                     </span>
                                 </div>
                                 <div className="d-flex flex-column font-weight-bold">
-                                    <div className="font-size-h6 text-dark-75 font-weight-bolder">INSTALACIONES ESPECIALES DE PROTECCIÓN CIVIL</div>
+                                    <div className="font-size-h6 text-dark-75 font-weight-bolder">
+                                        {
+                                            data ?
+                                                data.partida ?
+                                                    data.partida.nombre
+                                                : ''
+                                            : ''
+                                        }
+                                    </div>
                                     <span className="text-muted">PARTIDA</span>
                                 </div>
                             </div>
-                            <div className="d-flex align-items-center mb-4">
+                            <div className="d-flex align-items-center mb-3">
                                 <div className="symbol symbol-40 symbol-light-primary mr-4">
                                     <span className="symbol-label">
                                         <span className="svg-icon svg-icon-lg svg-icon-primary">
@@ -193,7 +240,15 @@ class CalidadView extends Component {
                                     </span>
                                 </div>
                                 <div className="d-flex flex-column font-weight-bold">
-                                    <div className="font-size-h6 text-dark-75 font-weight-bolder">VICIOS OCULTOS</div>
+                                    <div className="font-size-h6 text-dark-75 font-weight-bolder">
+                                        {
+                                            data ?
+                                                data.tipo_trabajo ?
+                                                    data.tipo_trabajo.tipo
+                                                : ''
+                                            : ''
+                                        }
+                                    </div>
                                     <span className="text-muted">TIPO DE TRABAJO</span>
                                 </div>
                             </div>
@@ -212,7 +267,17 @@ class CalidadView extends Component {
                                     </span>
                                 </div>
                                 <div className="d-flex flex-column font-weight-bold">
-                                    <div className="font-size-h6 text-dark-75 font-weight-bolder">09/07/2020</div>
+                                    <div className="font-size-h6 text-dark-75 font-weight-bolder">
+                                        {
+                                            data ?
+                                                data.updated_at ?
+                                                    <Moment format="DD/MM/YYYY">
+                                                        {data.created_at}
+                                                    </Moment>
+                                                : ''
+                                            : ''
+                                        }
+                                    </div>
                                     <span className="text-muted">FECHA</span>
                                 </div>
                             </div>
