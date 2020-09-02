4125bec50a708a44890c0a6aba4a735df8dd9dac
CariJG <64931832+CariJG@users.noreply.github.com>
2020-09-02 18:24:54 -0500

    Se agregó ruta de svg

--- src/components/layout/UserPanel/navUser.jsx
+++ src/components/layout/UserPanel/navUser.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { toAbsoluteUrl } from "../../../functions/routers"
+import SVG from "react-inlinesvg";
 export default function NavUser() {
 
     return (
@@ -10,13 +11,7 @@ export default function NavUser() {
 						<div className="symbol symbol-40 bg-light mr-3">
 							<div className="symbol-label">
 								<span className="svg-icon svg-icon-md svg-icon-success">
-									<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-										<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-											<rect x="0" y="0" width="24" height="24" />
-											<path d="M13.2070325,4 C13.0721672,4.47683179 13,4.97998812 13,5.5 C13,8.53756612 15.4624339,11 18.5,11 C19.0200119,11 19.5231682,10.9278328 20,10.7929675 L20,17 C20,18.6568542 18.6568542,20 17,20 L7,20 C5.34314575,20 4,18.6568542 4,17 L4,7 C4,5.34314575 5.34314575,4 7,4 L13.2070325,4 Z" fill="#000000" />
-											<circle fill="#000000" opacity="0.3" cx="18.5" cy="5.5" r="2.5" />
-										</g>
-									</svg>
+									<SVG src={toAbsoluteUrl('/images/svg/Notification2.svg')} />
 								</span>
 							</div>
 						</div>
@@ -31,13 +26,7 @@ export default function NavUser() {
 						<div className="symbol symbol-40 bg-light mr-3">
 							<div className="symbol-label">
 								<span className="svg-icon svg-icon-md svg-icon-info">
-									<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-										<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-											<rect x="0" y="0" width="24" height="24"/>
-											<path d="M4.875,20.75 C4.63541667,20.75 4.39583333,20.6541667 4.20416667,20.4625 L2.2875,18.5458333 C1.90416667,18.1625 1.90416667,17.5875 2.2875,17.2041667 C2.67083333,16.8208333 3.29375,16.8208333 3.62916667,17.2041667 L4.875,18.45 L8.0375,15.2875 C8.42083333,14.9041667 8.99583333,14.9041667 9.37916667,15.2875 C9.7625,15.6708333 9.7625,16.2458333 9.37916667,16.6291667 L5.54583333,20.4625 C5.35416667,20.6541667 5.11458333,20.75 4.875,20.75 Z" fill="#000000" fillRule="nonzero" opacity="0.3"/>
-											<path d="M2,11.8650466 L2,6 C2,4.34314575 3.34314575,3 5,3 L19,3 C20.6568542,3 22,4.34314575 22,6 L22,15 C22,15.0032706 21.9999948,15.0065399 21.9999843,15.009808 L22.0249378,15 L22.0249378,19.5857864 C22.0249378,20.1380712 21.5772226,20.5857864 21.0249378,20.5857864 C20.7597213,20.5857864 20.5053674,20.4804296 20.317831,20.2928932 L18.0249378,18 L12.9835977,18 C12.7263047,14.0909841 9.47412135,11 5.5,11 C4.23590829,11 3.04485894,11.3127315 2,11.8650466 Z M6,7 C5.44771525,7 5,7.44771525 5,8 C5,8.55228475 5.44771525,9 6,9 L15,9 C15.5522847,9 16,8.55228475 16,8 C16,7.44771525 15.5522847,7 15,7 L6,7 Z" fill="#000000"/>
-										</g>
-									</svg>
+									<SVG src={toAbsoluteUrl('/images/svg/Chat-check.svg')} />
 								</span>
 							</div>
 						</div>
