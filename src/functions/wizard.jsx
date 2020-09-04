07d52f5ab17ccc69540718dbed9d6f6064e541a2
CariJG <64931832+CariJG@users.noreply.github.com>
2020-09-04 18:15:47 -0500

    Se agregaron wizard para dos tabs

--- src/functions/wizard.jsx
+++ src/functions/wizard.jsx
@@ -53,3 +53,34 @@ export function openWizard3() {
         })
     }
 }
+
+
+
+
+export function openWizard1_for2_wizard() {
+    document.getElementById('wizardP').setAttribute("data-wizard-state", "first");
+    document.getElementById('wizard-1').setAttribute("data-wizard-state", "current");
+    document.getElementById('wizard-2').setAttribute("data-wizard-state", "pending");
+    document.getElementById('wizard-1-content').setAttribute("data-wizard-state", "current");
+    document.getElementById("wizard-2-content").removeAttribute("data-wizard-state");
+}
+
+export function openWizard2_for2_wizard() {
+    var elementsInvalid = document.getElementById("wizard-1-content").getElementsByClassName("is-invalid");
+
+    if (elementsInvalid.length === 0) {
+        document.getElementById('wizardP').setAttribute("data-wizard-state", "last");
+        document.getElementById('wizard-1').setAttribute("data-wizard-state", "done");
+        document.getElementById('wizard-2').setAttribute("data-wizard-state", "current");
+        document.getElementById('wizard-2-content').setAttribute("data-wizard-state", "current");
+        document.getElementById("wizard-1-content").removeAttribute("data-wizard-state");
+    } else {
+        swal({
+            title: '¡Lo sentimos!',
+            text: 'Llena todos los campos requeridos',
+            icon: 'warning',
+            timer: 1500,
+            buttons: false
+        })
+    }
+}
