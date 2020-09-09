25b8f4622ffbe9d9046da7f40f03ae66210b404b
CariJG <64931832+CariJG@users.noreply.github.com>
2020-09-09 14:16:33 -0500

    Se agregó tabla

--- src/components/forms/reportes/TablaReportes.jsx
+++ src/components/forms/reportes/TablaReportes.jsx
@@ -5,38 +5,42 @@ class TablaReportes extends Component {
     render() {
         const { } = this.props
         return (
-            <>
-                <div className="table-responsive">
-                    <table className="table table-head-custom table-head-bg table-borderless table-vertical-center">
-                        <thead>
-                            <tr className="text-left text-uppercase">
-                                <th style={{ minWidth: "250px" }} className="pl-7">
-                                    <span className="text-dark-75">Proyecto</span>
-                                </th>
-                                <th style={{ minWidth: "100px" }}>Ventas</th>
-                                <th style={{ minWidth: "100px" }}>Compras</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td className="pl-0 py-8">
-                                    <div className="d-flex align-items-center">
-                                        <div>
-                                            <div className="text-dark-75 font-weight-bolder text-hover-primary mb-1 font-size-lg pl-2">Proyecto 1</div>
-                                        </div>
+            <div className="table-responsive">
+                <table className="table table-head-custom table-head-bg table-borderless table-vertical-center">
+                    <thead>
+                        <tr className="text-left text-uppercase">
+                            <th style={{ minWidth: "250px" }} className="pl-7">
+                                <span className="text-dark-75 font-size-lg">Proyecto</span>
+                            </th>
+                            <th className="text-center" style={{ minWidth: "100px" }}>
+                                <span className="text-muted font-weight-bold">Ventas</span>
+                                <span className="text-dark-75 font-weight-bolder d-block font-size-sm">$8,000,000</span>
+                            </th>
+                            <th className="text-center" style={{ minWidth: "100px" }}>
+                                <span className="text-muted font-weight-bold">Compras</span>
+                                <span className="text-dark-75 font-weight-bolder d-block font-size-sm">$8,000,000</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="text-center">
+                            <td className="p-2">
+                                <div className="d-flex align-items-start">
+                                    <div>
+                                        <div className="text-dark-75 font-weight-bolder text-hover-primary mb-1 font-size-lg pl-2">Proyecto 1</div>
                                     </div>
-                                </td>
-                                <td>
-                                    <span className="text-dark-75 font-weight-bolder d-block font-size-lg">$8,000,000</span>
-                                </td>
-                                <td>
-                                    <span className="text-dark-75 font-weight-bolder d-block font-size-lg">$520</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </>
+                                </div>
+                            </td>
+                            <td>
+                                <span className="text-dark-75 font-weight-bolder d-block font-size-lg">$8,000,000</span>
+                            </td>
+                            <td>
+                                <span className="text-dark-75 font-weight-bolder d-block font-size-lg">$520</span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
