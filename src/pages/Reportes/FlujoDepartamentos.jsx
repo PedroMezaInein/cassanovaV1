fc35f16d7cf671b4dc5345ebbd0ac82488421202
CariJG <64931832+CariJG@users.noreply.github.com>
2020-09-09 14:16:02 -0500

    Se agregó onChange

--- src/pages/Reportes/FlujoDepartamentos.jsx
+++ src/pages/Reportes/FlujoDepartamentos.jsx
@@ -2,43 +2,66 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Layout from '../../components/layout/layout';
 import { Card } from 'react-bootstrap';
-import RangeCalendar from '../../components/form-components/RangeCalendar';
-
+import { FlujoDepartamentosForm, TablaReportes } from '../../components/forms'
 
 class FlujoDepartamentos extends Component {
 
     state = {
-        form:{
+        form: {
             fechaInicio: new Date(),
             fechaFin: new Date,
-        }
+            empresas: [],
+            empresa: 0,
+        },
+        options: {
+            empresas: [],
+        },
     }
 
-    onChangeRange = range => {
-        const { startDate, endDate } = range
-        const { form } = this.state
-        form.fechaInicio = startDate
-        form.fechaFin = endDate
+    onChangeEmpresa = e => {
+        const { name, value } = e.target
+        const { options, form } = this.state
+        let auxEmpresa = form.empresas
+        let aux = []
+        options.empresas.find(function (_aux) {
+            if (_aux.value.toString() === value.toString()) {
+                auxEmpresa.push(_aux)
+            } else {
+                aux.push(_aux)
+            }
+        })
+
+        options.empresas = aux
+        form['empresas'] = auxEmpresa
         this.setState({
             ... this.state,
-            form
+            form,
+            options
         })
     }
 
     render() {
-        const { form } = this.state
+        const { form, options} = this.state
         return (
-            <Layout active = 'reportes'  {...this.props}>
+            <Layout active='reportes'  {...this.props}>
                 <Card className="card-custom">
                     <Card.Header>
                         <div className="card-title">
-                            <h3 className="card-label">Flujo de departamentos</h3>
+                            <h3 className="card-label">FLUJO DE DEPARTAMENTOS</h3>
                         </div>
                     </Card.Header>
                     <Card.Body>
-                        <div className="d-flex justify-content-center">
-                            <RangeCalendar start = { form.fechaInicio } end = { form.fechaFin } 
-                                onChange = { this.onChangeRange } />
+                        <div id="id-row" className="row">
+                            <div id="col-calendar" className="col-lg-5">
+                                <FlujoDepartamentosForm
+                                    onChangeEmpresa = { this.onChangeEmpresa } 
+                                    form={form}
+                                    options = { options } 
+                                />
+                            </div>
+                            <div id="col-table"  className="col-lg-7">
+                                <TablaReportes />
+                            </div>
                         </div>
                     </Card.Body>
                 </Card>
