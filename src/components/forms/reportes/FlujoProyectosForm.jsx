f03845c1cf4069d801b9d916991c4d57571193b0
CariJG <64931832+CariJG@users.noreply.github.com>
2020-09-09 14:14:44 -0500

    Se agregó onChange

--- src/components/forms/reportes/FlujoProyectosForm.jsx
+++ src/components/forms/reportes/FlujoProyectosForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form'
-import { Button, Select } from '../../form-components'
+import { Select} from '../../form-components'
 import { validateAlert } from '../../../functions/alert'
 import RangeCalendar from '../../../components/form-components/RangeCalendar';
 
@@ -28,47 +28,41 @@ class FlujoProyectosForm extends Component {
     }
 
     render() {
-        const { form, onChange, options, formeditado, onSubmit, ...props } = this.props
+        const { form, onChange, onChangeEmpresa,  options, formeditado, onSubmit, ...props } = this.props
         return (
             <>
-            <Form id="form-flujo-proyectos"
-                onSubmit={
-                    (e) => {
-                        e.preventDefault();
-                        validateAlert(onSubmit, e, 'form-flujo-proyectos')
+                <Form id="form-flujo-proyectos"
+                    onSubmit={
+                        (e) => {
+                            e.preventDefault();
+                            validateAlert(onSubmit, e, 'form-flujo-proyectos')
+                        }
                     }
-                }
-                {...props}
-            >           
-                <RangeCalendar   
-                    onChange={this.onChangeRange}
-                    start = { form.fechaInicio } 
-                    end = { form.fechaFin }
-                />
-                        
-                        {/* <Select
-                            requirevalidation={1}
-                            formeditado={formeditado}
-                            name='empresa'
-                            options={options.empresas}
-                            placeholder='SELECCIONA LA(S) EMPRESA(S)'
-                            value={form.empresa}
-                            onChange={onChangeEmpresa}
-                            iconclass={"far fa-building"}
-                            messageinc="Incorrecto. Selecciona la(s) empresa(s)."
-                        /> */}
-
-                {/* <div className="separator separator-dashed mt-1 mb-2"></div>
-                Cuentas por cobrar*/}
-
-                <div className="card-footer py-3 pr-1">
-                    <div className="row">
-                        <div className="col-lg-12 text-right pr-0 pb-0">
-                            <Button text='ENVIAR' type='submit' className="btn btn-primary mr-2" icon=''/>
-                        </div>
+                    {...props}
+                >
+                    <Select
+                        requirevalidation={1}
+                        formeditado={formeditado}
+                        name='empresa'
+                        options={options.empresas}
+                        placeholder='SELECCIONA LA(S) EMPRESA(S)'
+                        value={form.empresa}
+                        onChange={onChangeEmpresa}
+                        iconclass={"far fa-building"}
+                        messageinc="Incorrecto. Selecciona la(s) empresa(s)."
+                    />
+                    <div className="text-center mt-4">
+                        <RangeCalendar
+                            onChange={this.onChangeRange}
+                            start={form.fechaInicio}
+                            end={form.fechaFin}
+                        />
                     </div>
-                </div>
-            </Form>
+                    <div className="text-center">
+                        <div className="mt-4 font-size-h6 label label-light-primary label-inline p-3"><strong>Cuentas por cobrar:</strong><span className="font-weight-normal font-size-lg">&nbsp;Suma</span></div>
+                    </div>
+                    {/* <div className="card-title font-weight-bolder mt-4 font-size-h5 text-center ">Cuentas por cobrar: <span className="font-weight-normal font-size-h6">Suma</span></div> */}
+                </Form>
             </>
         )
     }
