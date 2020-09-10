53382ae86ef0f0de4be393ac50dee18ee31ac002
CariJG <64931832+CariJG@users.noreply.github.com>
2020-09-10 12:14:15 -0500

    Se agregaron totales

--- src/components/forms/reportes/AccordionEstadosResultados.jsx
+++ src/components/forms/reportes/AccordionEstadosResultados.jsx
@@ -10,25 +10,37 @@ class AccordionEstadosResultados extends Component {
                 nombre:'VENTAS',
                 icono:'flaticon2-chart',
                 tipo:1,
-                isActive: false
+                isActive: false,
+                total_header:'Total ventas',
+                total_cantidad:300.00
             },
             {
                 nombre:'Ingresos',
                 icono:'flaticon2-chart',
                 tipo:2,
-                isActive: false
+                isActive: false,
+                total_header:'Total ingresos',
+                total_cantidad:800.00
             },
             {
                 nombre:'COSTOS DE SERVICIO',
                 icono:'flaticon-coins',
                 tipo:3,
-                isActive: false
+                isActive: false,
+                total_header:'Total costos de servicio',
+                utilidad:'Utilidad bruta',
+                total_cantidad:1000.00,
+                total_cantidad_utilidad:3000.00
             },
             {
                 nombre:'GASTOS OPERATIVOS',
                 icono:'flaticon2-chart',
                 tipo:4,
-                isActive: false
+                isActive: false,
+                total_header:'Total de gastos operativos',
+                resultado: 'Resultado operativo',
+                total_cantidad:3100.00,
+                total_cantidad_resultado:4000.00
             }
         ]
     };
@@ -72,6 +84,7 @@ class AccordionEstadosResultados extends Component {
                         accordion.map( (element,key) =>
                             {
                                 return(
+                                    <>
                                     <div className="card" key={key}>
                                         <div className="card-header" >
                                             <div className={(element.isActive) ? 'card-title text-primary collapsed' : 'card-title text-dark-50'} onClick = { () => { this.openAccordion(key) } }>
@@ -90,6 +103,45 @@ class AccordionEstadosResultados extends Component {
                                             </div>
                                         </div>
                                     </div>
+                                    <div className="d-flex justify-content-end">
+                                        <div className="d-flex flex-column mt-2">
+                                            <div className="d-flex align-items-center justify-content-between flex-grow-1">
+                                                <div className="mr-2 mt-2">
+                                                    <h6 className="font-weight-bolder">{element.total_header}</h6>
+                                                </div>
+                                                <div className="font-weight-boldest font-size-h4 text-primary ml-3">$ {element.total_cantidad}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {
+                                        element.utilidad? 
+                                        <div className="d-flex justify-content-end">
+                                            <div className="d-flex flex-column mt-2">
+                                                <div className="d-flex align-items-center justify-content-between flex-grow-1">
+                                                    <div className="mr-2 mt-2">
+                                                        <h6 className="font-weight-bolder">{element.utilidad}</h6>
+                                                    </div>
+                                                    <div className="font-weight-boldest font-size-h4 text-info ml-3">$ {element.total_cantidad_utilidad}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        :''
+                                    }
+                                    {
+                                        element.resultado? 
+                                        <div className="d-flex justify-content-end">
+                                            <div className="d-flex flex-column mt-2">
+                                                <div className="d-flex align-items-center justify-content-between flex-grow-1">
+                                                    <div className="mr-2 mt-2">
+                                                        <h6 className="font-weight-bolder">{element.resultado}</h6>
+                                                    </div>
+                                                    <div className="font-weight-boldest font-size-h4 text-info ml-3">$ {element.total_cantidad_resultado}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        :''
+                                    }
+                                    </>
                                 )                           
                             }
                         )
