fa7712cd717efd625c621e6a6bd78eeda8acb52a
OmarAbaroa <alvaro1295@hotmail.com>
2020-09-11 11:15:55 -0500

    Flujo proyectos

--- src/pages/Reportes/FlujoProyectos.jsx
+++ src/pages/Reportes/FlujoProyectos.jsx
@@ -104,13 +104,13 @@ class FlujoProyectos extends Component {
         waitAlert()
         await axios.post(URL_DEV + 'reportes/flujo-proyectos', form, { headers: { Authorization: `Bearer ${access_token}` } }).then(
             (response) => {
-                const { empresa, suma} = response.data
-                const { proyectos } = this.state
+                const { proyectos, suma} = response.data
+                /* const { proyectos } = this.state */
 
 
                 this.setState({
                     ... this.state,
-                    proyectos: empresa.proyectos ? empresa.proyectos : [],
+                    proyectos: proyectos,
                     suma:suma
                 })
             },
