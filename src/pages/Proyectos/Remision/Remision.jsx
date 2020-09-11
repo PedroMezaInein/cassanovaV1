2ad1fb4cc184c731fd62fa73180cfd35d2291a7a
CariJG <64931832+CariJG@users.noreply.github.com>
2020-09-11 16:46:24 -0500

    Se modificó los botones

--- src/pages/Proyectos/Remision/Remision.jsx
+++ src/pages/Proyectos/Remision/Remision.jsx
@@ -261,7 +261,7 @@ class Remisiones extends Component{
                         {
                             remision.convertido ? '' :
                                 <div className="col-md-12 mb-3 d-flex justify-content-end">
-                                    <Button className="mx-2 my-2 my-md-0 small-button" onClick={(e) => {e.preventDefault(); this.changePageConvert(remision)} } text='' icon={faSync} color="transparent" 
+                                    <Button pulse="pulse-ring" className="btn btn-icon btn-light-info pulse pulse-info" onClick={(e) => {e.preventDefault(); this.changePageConvert(remision)} } icon={faSync}
                                         tooltip={{text:'COMPRAR'}} />
                                 </div>
                         }
