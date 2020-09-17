d5162d2de7f9ff1a41d78d823416d41d395c18a4
OmarAbaroa <alvaro1295@hotmail.com>
2020-09-17 09:58:14 -0500

    Reporte deptos

--- src/components/forms/reportes/TablaReportesDepartamento.jsx
+++ src/components/forms/reportes/TablaReportesDepartamento.jsx
@@ -3,12 +3,24 @@ import { setMoneyTableSinSmall } from '../../../functions/setters'
 
 class TablaReportesDepartamento extends Component {
 
+    state = {
+        active: ''
+    }
+    
     sumaEgresosDepartamentos(subareas) {
         let suma = 0
         subareas.map((subarea)=>{
             subarea.egresos.map((egresos) => {
                 suma = suma + egresos.total
             })
+        })
+        return suma
+    }
+
+    sumaEgresosSubarea = egresos => {
+        let suma = 0
+        egresos.map((egreso)=>{
+            suma = suma + egreso.total
         })
         return suma
     }
@@ -26,9 +38,23 @@ class TablaReportesDepartamento extends Component {
         return suma
     }
 
+    setDeptos = depto =>{
+        let { active } = this.state
+        if(active === depto.id){
+            active = ''
+        }else{
+            active = depto.id
+        }
+        this.setState({
+            ... this.state,
+            active
+        })
+    }
+
 
     render() {
         const { departamentos } = this.props
+        const { active } = this.state
         return (
             <div className="table-responsive w-75">
                 <table className="table table-head-custom table-head-bg table-vertical-center">
@@ -47,22 +73,43 @@ class TablaReportesDepartamento extends Component {
                     <tbody>
                         {
                             departamentos.map((departamento,key)=>{
-                                    return(
-                                        <tr key={key} className="border-bottom">
-                                                <td className="p-2">
-                                                    <div className="d-flex align-items-start">
-                                                        <div>
-                                                            <div className="text-dark-75 mb-1 font-size-lg pl-2">{departamento.nombre}</div>
-                                                        </div>
+                                return(
+                                    <>
+                                        <tr key={key} className="border-bottom" onClick = { (e) => { e.preventDefault(); this.setDeptos(departamento) } }>
+                                            <td className="btn btn-hover  p-2">
+                                                <div className="d-flex align-items-start">
+                                                    <div>
+                                                        <div className="text-dark-75 mb-1 font-size-lg pl-2">{departamento.nombre}</div>
                                                     </div>
-                                                </td>
-                                                <td>
-                                                    <span className="text-right text-dark-75 d-block font-size-lg">{setMoneyTableSinSmall(this.sumaEgresosDepartamentos(departamento.subareas))}</span>
-                                                </td>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <span className="text-right text-dark-75 d-block font-size-lg">{setMoneyTableSinSmall(this.sumaEgresosDepartamentos(departamento.subareas))}</span>
+                                            </td>
                                         </tr>
-                                    )  
-                                }                                                         
-                            )
+                                        {
+                                            active === departamento.id ?
+                                                departamento.subareas.map((subarea, index)=>{
+                                                    return(
+                                                        <tr key={index} className="border-bottom" >
+                                                            <td className="p-2">
+                                                                <div className="d-flex align-items-start">
+                                                                    <div className="pl-4">
+                                                                        <div className="text-dark-75 mb-1 font-size-md pl-2">{subarea.nombre}</div>
+                                                                    </div>
+                                                                </div>
+                                                            </td>
+                                                            <td className="pr-4">
+                                                                <span className="text-right text-dark-75 d-block font-size-md">{setMoneyTableSinSmall(this.sumaEgresosSubarea(subarea.egresos))}</span>
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                                })
+                                            : <tr></tr>
+                                        }
+                                    </>
+                                )  
+                            })
                         }
                     </tbody>
                 </table>
