63586166341d80fc4a9904ec83a040b0264fd56d
CariJG <64931832+CariJG@users.noreply.github.com>
2020-09-28 12:36:16 -0500

    Se quitó pt-4 y se agregó card-footer py-3 pr-1

--- src/components/forms/bancos/TraspasoForm.jsx
+++ src/components/forms/bancos/TraspasoForm.jsx
@@ -4,26 +4,20 @@ import { Form } from 'react-bootstrap'
 import { DATE } from '../../../constants'
 import { validateAlert } from '../../../functions/alert'
 import ItemSlider from '../../singles/ItemSlider'
-
 class TareaForm extends Component {
-
     updateOrigen = value => {
         const { onChange } = this.props
         onChange({ target: { name: 'origen', value: value } })
     }
-
     updateDestino = value => {
         const { onChange } = this.props
         onChange({ target: { name: 'destino', value: value } })
     }
-
     changeDate = date => {
         const { onChange } = this.props
         onChange({ target: { value: date, name: 'fecha' } })
     }
-
     render() {
-
         const { title, options, form, onChange, onChangeAdjunto, deleteAdjunto, requirevalidation, onSubmit, formeditado, handleChange, deleteFile, ...props } = this.props
         return (
             <Form id="form-transpasos"
@@ -35,7 +29,7 @@ class TareaForm extends Component {
                 }
                 {...props}
             >
-                <div className="form-group row form-group-marginless pt-4">
+                <div className="form-group row form-group-marginless">
                     <div className="col-md-3">
                         <SelectSearch
                             formeditado={formeditado}
@@ -99,24 +93,27 @@ class TareaForm extends Component {
                 <div className="separator separator-dashed mt-1 mb-2"></div>
                 <div className="form-group row form-group-marginless justify-content-center">
                     <div className="col-md-6">
-                        <ItemSlider items = { form.adjuntos.adjuntos.files }
-                                item = 'adjuntos' handleChange = { handleChange }
-                                deleteFile = { deleteFile } 
-                                multiple = { false }/>
+                        <ItemSlider items={form.adjuntos.adjuntos.files}
+                            item='adjuntos' handleChange={handleChange}
+                            deleteFile={deleteFile}
+                            multiple={false} />
                     </div>
                 </div>
-
-                <div className="text-center mt-3 mb-2">
-                    <Button icon='' className="mx-auto" 
-                        onClick={
-                            (e) => {
-                                e.preventDefault();
-                                validateAlert(onSubmit, e, 'form-transpasos')
-                            }
-                        }
-                        text="ENVIAR" />
+                <div className="card-footer py-3 pr-1">
+                    <div className="row">
+                        <div className="col-lg-12 text-right pr-0 pb-0">
+                            <Button icon='' className="mx-auto"
+                                onClick={
+                                    (e) => {
+                                        e.preventDefault();
+                                        validateAlert(onSubmit, e, 'form-transpasos')
+                                    }
+                                }
+                                text="ENVIAR"
+                            />
+                        </div>
+                    </div>
                 </div>
-
             </Form>
         )
     }
