92c235035312c86aefce321704a8705099671c03
OmarAbaroa <alvaro1295@hotmail.com>
2020-09-29 12:18:53 -0500

    Icons en notificaciones

--- src/components/singles/Notificacion.jsx
+++ src/components/singles/Notificacion.jsx
@@ -3,12 +3,25 @@ import SVG from "react-inlinesvg";
 import { toAbsoluteUrl } from "../../functions/routers"
 
 class Notificacion extends Component {
+
+    setIcon = tipo => {
+        switch(tipo){
+            case 'lead':
+                return toAbsoluteUrl('/images/svg/notificaciones/lead.svg');
+            case 'tarea':
+                return toAbsoluteUrl('/images/svg/notificaciones/tarea.svg');
+            case 'ticket':
+                return toAbsoluteUrl('/images/svg/notificaciones/ticket.svg');
+            default:
+                return toAbsoluteUrl('/images/svg/portapapeles.svg');
+        }
+    }
     render() {
         const { data } = this.props
         return (
             <div>
-                <span className="svg-icon svg-icon-lg svg-icon-success mr-3">
-                    <SVG src={toAbsoluteUrl('/images/svg/portapapeles.svg')} />
+                <span className="svg-icon svg-icon-lg svg-icon-success mx-3">
+                    <SVG src = { this.setIcon(data.tipo) } />
                 </span>
                 <span>
                     {
