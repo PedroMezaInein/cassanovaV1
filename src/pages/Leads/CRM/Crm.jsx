2f775fbbf0f0ceae5b006207b74f143b130763e7
OmarAbaroa <alvaro1295@hotmail.com>
2020-10-07 11:48:14 -0500

    Buttons

--- src/pages/Leads/CRM/Crm.jsx
+++ src/pages/Leads/CRM/Crm.jsx
@@ -4,27 +4,36 @@ import Layout from '../../../components/layout/layout';
 import { Button } from '../../../components/form-components';
 
 class Crm extends Component {
+
+    changePageAdd = tipo => {
+        const { history } = this.props
+        history.push({
+            pathname: '/leads/crm/add/'+tipo
+        });
+    }
+
     render() {
         return (
             <Layout active = 'leads' { ... this.props } >
-                <div className = "d-flex justify-content-end">
+                <div className = "d-flex justify-content-end flex-wrap">
                     <Button
-                        /* onClick={() => { changeEstatus('Aceptado') }}  */
-                        className = "btn btn-icon btn-light-success success2 btn-md mr-2"
-                        only_icon = "fas fa-phone icon-md"
+                        onClick={() => { this.changePageAdd('telefono') }} 
+                        className = "btn btn-light-success success2 mr-2"
+                        only_icon = "fas fa-phone icon-md mr-2"
                         tooltip = { { text:'Nuevo lead por teléfono' } }
+                        text = 'Nuevo lead por teléfono'
                         />
                     <Button
-                        /* onClick={() => { changeEstatus('Aceptado') }}  */
-                        className = "btn btn-icon btn-light-warning btn-md mr-2"
-                        only_icon = "flaticon2-black-back-closed-envelope-shape icon-md"
+                        className = "btn btn-light-warning mr-2"
+                        only_icon = "flaticon2-black-back-closed-envelope-shape icon-md mr-2"
                         tooltip = { { text:'Nuevo lead por correo' } }
+                        text = 'Nuevo lead por correo'
                         />
                     <Button
-                        /* onClick={() => { changeEstatus('Aceptado') }}  */
-                        className = "btn btn-icon btn-light-info btn-md mr-2"
-                        only_icon = "fas fa-dove icon-md"
+                        className = "btn btn-light-info mr-2"
+                        only_icon = "fas fa-dove icon-md mr-2"
                         tooltip = { { text:'Nuevo lead por Tawk to' } }
+                        text = 'Nuevo lead por Tawk to'
                         />
                 </div>
             </Layout>
