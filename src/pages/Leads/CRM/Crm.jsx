43945ce7091f66f1aca6e3760ca89895851297c0
CariJG <64931832+CariJG@users.noreply.github.com>
2020-10-09 17:35:23 -0500

    Nuevos cambios

--- src/pages/Leads/CRM/Crm.jsx
+++ src/pages/Leads/CRM/Crm.jsx
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import { URL_DEV } from '../../../constants'
 import Layout from '../../../components/layout/layout';
-import { Col, Row, OverlayTrigger, Tooltip, Card, Dropdown, DropdownButton } from 'react-bootstrap'
-import SVG from "react-inlinesvg";
-import { toAbsoluteUrl } from "../../../functions/routers"
+import { Col, Row, Card } from 'react-bootstrap'
 import { UltimosContactosCard, SinContacto, UltimosIngresosCard } from '../../../components/cards'
 import { forbiddenAccessAlert, errorAlert } from '../../../functions/alert'
+import LeadNuevo from '../../../components/tables/Lead/LeadNuevo'
+import LeadContacto from '../../../components/tables/Lead/LeadContacto'
+import LeadNegociacion from '../../../components/tables/Lead/LeadNegociacion'
+import LeadContrato from '../../../components/tables/Lead/LeadContrato'
 class Crm extends Component {
     state = {
         ultimos_contactados:{
@@ -201,7 +203,16 @@ class Crm extends Component {
             errorAlert('Ocurrió un error desconocido catch, intenta de nuevo.')
             console.log(error, 'error')
         })
-    }  
+    }
+    onChange = e => {
+        const { name, value } = e.target
+        const { form } = this.state
+        form[name] = value
+        this.setState({
+            ... this.state,
+            form
+        })
+    }
 
     changePageAdd = tipo => {
         const { history } = this.props
@@ -245,94 +256,37 @@ class Crm extends Component {
                             </h3>
                         </Card.Header>
                         <Card.Body className="py-2">
-                            <div className="tab-content">
-                                <div className="table-responsive-lg">
-                                    <table className="table table-head-custom table-head-bg table-borderless table-vertical-center">
-                                        <thead>
-                                            <tr className="text-left text-uppercase">
-                                                <th style={{ minWidth: "250px" }} className="pl-7">
-                                                    <span className="text-dark-75">Proyecto/Nombre</span>
-                                                </th>
-                                                <th style={{ minWidth: "100px" }} className="text-center">Vendedor</th>
-                                                <th style={{ minWidth: "100px" }} className="text-center">Origen</th>
-                                                <th style={{ minWidth: "100px" }} className="text-center">Estatus</th>
-                                                <th style={{ minWidth: "80px" }}></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td className="pl-0 py-8">
-                                                    <div className="d-flex align-items-center">
-                                                        <div className="symbol symbol-45 symbol-light-primary mr-3">
-                                                            <span className="symbol-label font-size-h5">P</span>
-                                                        </div>
-                                                        <div>
-                                                            <a href="#" className="text-dark-75 font-weight-bolder text-hover-primary mb-1 font-size-lg">Nombre cliente X</a>
-                                                            <span className="text-muted font-weight-bold d-block">Proyecto X</span>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td className="d-flex justify-content-center">
-                                                    <div className="symbol-group symbol-hover">
-                                                        <OverlayTrigger overlay={<Tooltip>OMAR ABAROA</Tooltip>}>
-                                                            <div className="symbol symbol-35 symbol-circle">
-                                                                <img alt="Pic" src="/100_1.jpg" />
-                                                            </div>
-                                                        </OverlayTrigger>
-                                                        <OverlayTrigger overlay={<Tooltip>CARINA JIMÉNEZ</Tooltip>}>
-                                                            <div className="symbol symbol-35 symbol-circle">
-                                                                <img alt="Pic" src="/100_2.jpg" />
-                                                            </div>
-                                                        </OverlayTrigger>
-                                                        <OverlayTrigger overlay={<Tooltip>FERNANDO MÁRQUEZ</Tooltip>}>
-                                                            <div className="symbol symbol-35 symbol-circle">
-                                                                <img alt="Pic" src="/100_3.jpg" />
-                                                            </div>
-                                                        </OverlayTrigger>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <span className="text-dark-75 font-weight-bolder d-block font-size-lg text-center">WEB</span>
-                                                </td>
-                                                <td className="text-center">
-                                                    <DropdownButton
-                                                        variant={"secondary"}
-                                                        title={"Estatus"}
-                                                    >
-                                                        <Dropdown.Header>
-                                                            <span className="font-size-sm">Elige una opción</span>
-                                                        </Dropdown.Header>
-                                                        <Dropdown.Divider />
-                                                        <Dropdown.Item eventKey="1">
-                                                            <a href="#" className="navi-link">
-                                                                <span className="navi-text">
-                                                                    <span className="label label-xl label-inline label-light-success rounded-0">CONTRATADO</span>
-                                                                </span>
-                                                            </a>
-                                                        </Dropdown.Item>
-                                                        <Dropdown.Item eventKey="2">
-                                                            <a href="#" className="navi-link">
-                                                                <span className="navi-text">
-                                                                    <span className="label label-xl label-inline label-light-danger rounded-0">DETENIDO</span>
-                                                                </span>
-                                                            </a>
-                                                        </Dropdown.Item>
-                                                    </DropdownButton>
-                                                </td>
-                                                <td className="pr-0 text-right">
-                                                    <OverlayTrigger overlay={<Tooltip>Ver más</Tooltip>}>
-                                                        <a className="btn btn-default btn-icon btn-sm mr-2">
-                                                            <span className="svg-icon svg-icon-md">
-                                                                <SVG src={toAbsoluteUrl('/images/svg/Arrow-right.svg')} />
-                                                            </span>
-                                                        </a>
-                                                    </OverlayTrigger>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
+                            <LeadNuevo/>
+                        </Card.Body>
+                    </Card>
+                    <Card className="card-custom card-stretch gutter-b py-2">
+                        <Card.Header className="align-items-center border-0">
+                            <h3 className="card-title align-items-start flex-column">
+                                <span className="font-weight-bolder text-dark">Leads en contacto</span>
+                            </h3>
+                        </Card.Header>
+                        <Card.Body className="py-2">
+                            <LeadContacto/>
+                        </Card.Body>
+                    </Card>
+                    <Card className="card-custom card-stretch gutter-b py-2">
+                        <Card.Header className="align-items-center border-0">
+                            <h3 className="card-title align-items-start flex-column">
+                                <span className="font-weight-bolder text-dark">Leads en negociación</span>
+                            </h3>
+                        </Card.Header>
+                        <Card.Body className="py-2">
+                            <LeadNegociacion/>
+                        </Card.Body>
+                    </Card>
+                    <Card className="card-custom card-stretch gutter-b py-2">
+                        <Card.Header className="align-items-center border-0">
+                            <h3 className="card-title align-items-start flex-column">
+                                <span className="font-weight-bolder text-dark">Leads contratados</span>
+                            </h3>
+                        </Card.Header>
+                        <Card.Body className="py-2">
+                            <LeadContrato/>
                         </Card.Body>
                     </Card>
                 </Col>
@@ -351,42 +305,3 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Crm)
-
-
-
-// onpag2=(e)=>{
-    //     e.preventDefault()
-    //     const {numPage}=this.state 
-        
-    //     this.setState({
-    //         numPage:numPage+1
-    //     })
-    //     this.getUltimosContactos()
-    // }
-    // async getUltimosContactos() {
-    //     const { access_token } = this.props.authUser
-    //     const{numPage}=this.state
-
-    //     await axios.get(URL_DEV + 'crm/timeline/ultimos-contactos/'+numPage, { headers: { Authorization: `Bearer ${access_token}` } }).then(
-    //         (response) => {
-    //             const { contactos } = response.data
-    //             const { ultimos_contactados } = this.state
-    //             ultimos_contactados.data = contactos
-    //             this.setState({
-    //                 ... this.state,
-    //                 ultimos_contactados
-    //             })
-    //         },
-    //         (error) => {
-    //             console.log(error, 'error')
-    //             if (error.response.status === 401) {
-    //                 forbiddenAccessAlert()
-    //             } else {
-    //                 errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
-    //             }
-    //         }
-    //     ).catch((error) => {
-    //         errorAlert('Ocurrió un error desconocido catch, intenta de nuevo.')
-    //         console.log(error, 'error')
-    //     })
-    // }
