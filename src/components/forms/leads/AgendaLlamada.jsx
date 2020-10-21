3f1e8fff919b579f1f23eef167a53612783f364b
CariJG <64931832+CariJG@users.noreply.github.com>
2020-10-21 15:31:49 -0500

    Se agregó modal

--- src/components/forms/leads/AgendaLlamada.jsx
+++ src/components/forms/leads/AgendaLlamada.jsx
@@ -1,37 +1,157 @@
 import React, { Component } from 'react'
-// import { CalendarDay } from '../../form-components'
+import { CalendarDay, InputGray, Button } from '../../form-components'
+import { TEL, EMAIL } from '../../../constants'
+import { Col } from 'react-bootstrap'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import TimeRangePicker from '@wojtekmaj/react-timerange-picker';
+import { messageAlert } from '../../../functions/alert'
 // import { TimePicker } from 'antd';
 // import 'antd/dist/antd.css';
 class AgendaLlamada extends Component {
+    addCorreo = () => {
+        const { onChange, form } = this.props
+        let aux = false
+        let array = []
+        if (/^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i.test(form.correo)) {
+            if (form.correo) {
+                form.correos.map((correo) => {
+                    if (correo === form.correo) {
+                        aux = true
+                    }
+                    return false
+                })
+                if (!aux) {
+                    array = form.correos
+                    array.push(form.correo)
+                    onChange({ target: { name: 'correos', value: array } })
+                    onChange({ target: { name: 'correo', value: '' } })
+                }
+            }
+        } else {
+            messageAlert("LA DIRECCIÓN DEL CORREO ELECTRÓNICO ES INCORRECTA");
+        }
+    }
     render() {
-        const { changeHora, form, onChange} = this.props
+        const { changeHora, form, onChange, removeCorreo } = this.props
         return (
-            <div className="">
-                hola?
-                {/* <CalendarDay /> */}
-                {/* <TimePicker.RangePicker
-                    format="h:mm"
-                    minuteStep={5}
-                    allowClear={true} 
-                    
-                    placeholder={['Inicio', 'Fin']}
-                    showNow={false}
-                    inputReadOnly
-                    hideDisabledOptions
-                    className="time-picker"
-                    onChange={changeHora}
-                    value={[form.horaInicio, form.horaFin]}
-                /> */}
-                {/* bordered = {false} */}
-                {/* <InputGray
-                    placeholder='NOMBRE DEL LEAD'
-                    withicon={1}
-                    iconclass="far fa-user"
-                    name='name'
-                    value={form.name}
-                    onChange={onChange}
-                /> */}
-
+            <div className="row">
+                <Col md="6">
+                    <CalendarDay />
+                    {/* <TimePicker.RangePicker
+                        format="h:mm"
+                        minuteStep={5}
+                        allowClear={true} 
+                        
+                        placeholder={['Inicio', 'Fin']}
+                        showNow={false}
+                        inputReadOnly
+                        hideDisabledOptions
+                        className="time-picker"
+                        onChange={changeHora}
+                        value={[form.horaInicio, form.horaFin]}
+                    /> */}
+                    <TimeRangePicker
+                        onChange={changeHora}
+                        // autoFocus={true}
+                        format="h:mm a"
+                        disableClock={true}
+                        hourPlaceholder="hh"
+                        minutePlaceholder="mm"
+                        className=""
+                        value={[form.horaInicio, form.horaFin]}
+                    />
+                </Col>
+                <Col md="6">
+                    <div className="form-group row form-group-marginless mt-4">
+                        <div className="col-md-12">
+                            <InputGray
+                                placeholder='Titulo'
+                                withicon={1}
+                                iconclass="fas fa-users"
+                                name='titulo'
+                                value={form.titulo}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="col-md-12">
+                            <InputGray
+                                placeholder='Ubicación'
+                                withicon={1}
+                                iconclass="flaticon2-pin-1"
+                                name='ubicacion'
+                                value={form.ubicacion}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="col-md-12">
+                            <InputGray
+                                placeholder='Link'
+                                withicon={1}
+                                iconclass="fas fa-external-link-alt"
+                                name='link'
+                                value={form.link}
+                                onChange={onChange}
+                            />
+                        </div>
+                    </div>
+                </Col>
+                <Col md="12">
+                    <div className="separator separator-dashed mt-4 mb-2"></div>
+                    <div className="d-flex justify-content-center">
+                        <span className="bg-gray-200 text-dark-50 font-size-h6 p-1 font-weight-bolder">Asistentes</span>
+                    </div>
+                    <div className="d-flex justify-content-center mt-2">
+                        <div className="mr-5 text-center">
+                            <div className="text-dark-75 font-weight-bolder text-hover-primary mb-1 font-size-lg">Carina Jiménez</div>
+                            <span className="text-muted font-weight-bold d-block">Vendedor</span>
+                        </div>
+                        <div className="text-center">
+                            <div className="text-dark-75 font-weight-bolder text-hover-primary mb-1 font-size-lg">Ricardo García</div>
+                            <span className="text-muted font-weight-bold d-block">Cliente</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="form-group row form-group-marginless">
+                            <div className="col-md-10">
+                                <InputGray
+                                    placeholder="CORREO DE CONTACTO"
+                                    withicon={1}
+                                    iconclass='fas fa-envelope'
+                                    name='correo'
+                                    value={form.correo}
+                                    onChange={onChange}
+                                    patterns={EMAIL}
+                                />
+                            </div>
+                            <div className="col-md-2 mt-3 d-flex justify-content-center align-items-center">
+                                <Button icon={faPlus} pulse={"pulse-ring"} className={"btn btn-icon btn-light-gray pulse pulse-dark mr-5"} onClick={(e) => { e.preventDefault(); this.addCorreo() }} />
+                            </div>
+                        </div>
+                        <div className="form-group row form-group-marginless">
+                            <div className="col-md-12 row mx-0">
+                                {
+                                    form.correos.map((correo, key) => {
+                                        return (
+                                            <div className="tagify form-control p-1 col-md-4 px-2 d-flex justify-content-center align-items-center white-space" tabIndex="-1" style={{ borderWidth: "0px" }} key={key}>
+                                                <div className=" image-upload d-flex px-3 align-items-center tagify__tag tagify__tag--primary tagify--noAnim white-space"  >
+                                                    <div
+                                                        title="Borrar archivo"
+                                                        className="tagify__tag__removeBtn"
+                                                        role="button"
+                                                        aria-label="remove tag"
+                                                        onClick={(e) => { e.preventDefault(); removeCorreo(correo) }}
+                                                    >
+                                                    </div>
+                                                    <div><span className="tagify__tag-text p-1 white-space">{correo}</span></div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </Col>
             </div>
         )
     }
