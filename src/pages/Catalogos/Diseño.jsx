6520008ac9e51755dccd2887f75e36b45d1c7e75
CariJG <64931832+CariJG@users.noreply.github.com>
2020-11-02 16:16:15 -0600

    Se cambio el numero de puntos y se agrego el componente DiseñoForm y la grafica en dos columnas

--- src/pages/Catalogos/Diseño.jsx
+++ src/pages/Catalogos/Diseño.jsx
@@ -637,8 +637,8 @@ class Contabilidad extends Component {
             let aux = empresa.variaciones
             let limiteInf = aux[0].inferior
             let limiteSup = aux[aux.length - 1].superior
-            for (let i = 0; i <= 39; i++) {
-                let limite = (limiteInf + (i * (limiteSup - limiteInf) / 40))
+            for (let i = 0; i <= 19; i++) {
+                let limite = (limiteInf + (i * (limiteSup - limiteInf) / 20))
                 limite = parseInt(parseFloat(limite).toFixed(2))
                 labels.push(limite)
                 data.push(this.getPrecioEsquemas(form, limite))
@@ -868,7 +868,7 @@ class Contabilidad extends Component {
                             </div>
                         </Card.Header>
                         <Card.Body className="pt-2">
-                            <Tab.Container defaultActiveKey="planos">
+                            <Tab.Container defaultActiveKey="diseño">
                                 <Nav className="nav nav-tabs nav-tabs-space-lg nav-tabs-line nav-bold nav-tabs-line-3x border-0">
                                     <Nav.Item className="nav-item">
                                         <Nav.Link className="nav-link" eventKey="diseño">
@@ -913,23 +913,28 @@ class Contabilidad extends Component {
                                 </Nav>
                                 <Tab.Content className="py-5">
                                     <Tab.Pane eventKey="diseño">
-                                        <DiseñoForm
-                                            form={form}
-                                            onChange={this.onChange}
-                                            onSubmit={this.onSubmit}
-                                            addRow={this.addRow}
-                                            deleteRow={this.deleteRow}
-                                            onChangeVariaciones={this.onChangeVariaciones}
-                                        />
-                                        {
-                                            grafica !== '' ?
-                                                <div className="row mx-0 justify-content-center p-3">
-                                                    <div className="col-md-8">
-                                                        <Line data={grafica} />
-                                                    </div>
-                                                </div>
-                                                : <></>
-                                        }
+                                    <div className="row">
+                                        <div className="col-md-5">
+                                            <DiseñoForm
+                                                form={form}
+                                                onChange={this.onChange}
+                                                onSubmit={this.onSubmit}
+                                                addRow={this.addRow}
+                                                deleteRow={this.deleteRow}
+                                                onChangeVariaciones={this.onChangeVariaciones}
+                                                grafica={grafica}
+                                            />
+                                        </div>
+                                        <div className="col-lg-7 d-flex justify-content-center align-items-center">
+                                            {
+                                                grafica !== '' ?
+                                                        <div className="col-md-11">
+                                                            <Line data={grafica} />
+                                                        </div>
+                                                    : <></>
+                                            }
+                                        </div>
+                                    </div>
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="obra">
                                         <ObraForm
