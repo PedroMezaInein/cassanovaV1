9838d30d468ac83d7437be601152acebbd13d1d6
CariJG <64931832+CariJG@users.noreply.github.com>
2021-01-12 15:03:15 -0600

    Se agregaro ItemSlider, se agregaron iconos para redes sociales.

--- src/pages/Mercadotecnia/ParrillasContenido/ParrillasContenido.jsx
+++ src/pages/Mercadotecnia/ParrillasContenido/ParrillasContenido.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { URL_DEV } from '../../../constants';
-import moment from 'moment'
 import { doneAlert, errorAlert, forbiddenAccessAlert, waitAlert } from '../../../functions/alert';
 import { connect } from 'react-redux';
 import Layout from '../../../components/layout/layout';
@@ -16,39 +15,50 @@ import { Modal } from '../../../components/singles'
 import ParrillaContenidoForm from '../../../components/forms/mercadotecnia/ParrillaContenidoForm';
 import { setOptions } from '../../../functions/setters';
 const $ = require('jquery');
-
 class Calendario extends Component {
     state = {
-        content:[],
-        formeditado:0,
-        title:'',
+        content: [],
+        formeditado: 0,
+        title: '',
         activeKeyModal: 'form',
-        modal:{
+        modal: {
             form: false
         },
-        form:{
-            socialNetwork:'',
-            typeContent:"contenido",
-            title:'',
-            copy:'',
-            cta:'',
-            comments:'',
+        form: {
+            socialNetwork: '',
+            typeContent: "contenido",
+            title: '',
+            copy: '',
+            cta: '',
+            comments: '',
             empresa: '',
             hora: '09',
             minuto: '00',
-            fecha: ''
+            fecha: '',
+            adjuntos: {
+                adjunto: {
+                    value: '',
+                    placeholder: 'Adjunto',
+                    files: []
+                },
+                adjunto_comentario: {
+                    value: '',
+                    placeholder: 'Adjunto',
+                    files: []
+                },
+            }
         },
-        options:{
+        options: {
             socialNetworks: [],
             typeContents:
-            [
-                {
-                    name: "CONTENIDO", value: "contenido", label: "CONTENIDO"
-                },
-                {
-                    name: "HISTORIA", value: "historia", label: "HISTORIA"
-                }
-            ],
+                [
+                    {
+                        name: "CONTENIDO", value: "contenido", label: "CONTENIDO"
+                    },
+                    {
+                        name: "HISTORIA", value: "historia", label: "HISTORIA"
+                    }
+                ],
             empresas: []
         },
         data: {
@@ -79,24 +89,24 @@ class Calendario extends Component {
                 data.empresas = empresas
 
                 options.empresas = setOptions(empresas, 'name', 'id')
-                options.socialNetworks  = setOptions(redes, 'nombre', 'id')
+                options.socialNetworks = setOptions(redes, 'nombre', 'id')
 
                 let bandera = false
                 let aux = []
                 let aux2 = ''
 
-                if(empresa === ''){
-                    empresas.map( (item) => {
-                        if(item.parrillas.length > 0 && bandera === false){
+                if (empresa === '') {
+                    empresas.map((item) => {
+                        if (item.parrillas.length > 0 && bandera === false) {
                             bandera = item;
                         }
                     })
-                }else{
-                    empresas.map( (item) => {
-                        if(item.id === empresa.id){
+                } else {
+                    empresas.map((item) => {
+                        if (item.id === empresa.id) {
                             bandera = item;
-                            item.parrillas.map((parrilla)=>{
-                                if(parrilla.id === evento.id){
+                            item.parrillas.map((parrilla) => {
+                                if (parrilla.id === evento.id) {
                                     evento = parrilla
                                 }
                             })
@@ -104,7 +114,7 @@ class Calendario extends Component {
                     })
                 }
 
-                if(bandera !== false){
+                if (bandera !== false) {
                     bandera.parrillas.map((parrilla) => {
                         aux.push(
                             {
@@ -117,7 +127,7 @@ class Calendario extends Component {
                     })
                 }
 
-                console.log(evento, 'evento')
+                // console.log(evento, 'evento')
 
                 this.setState({
                     ... this.state,
@@ -130,9 +140,9 @@ class Calendario extends Component {
             },
             (error) => {
                 console.log(error, 'error')
-                if(error.response.status === 401){
+                if (error.response.status === 401) {
                     forbiddenAccessAlert()
-                }else{
+                } else {
                     errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
                 }
             }
@@ -142,14 +152,14 @@ class Calendario extends Component {
         })
     }
 
-    sendParrillaAxios = async() => {
+    sendParrillaAxios = async () => {
         waitAlert()
         const { access_token } = this.props.authUser
         const { form } = this.state
         await axios.post(URL_DEV + 'mercadotecnia/parrilla-contenido', form, { headers: { Authorization: `Bearer ${access_token}` } }).then(
             (response) => {
                 doneAlert('Parrilla guardad con éxito');
-                const { modal} = this.state
+                const { modal } = this.state
                 modal.form = false
                 this.setState({
                     ...this.state,
@@ -159,9 +169,9 @@ class Calendario extends Component {
             },
             (error) => {
                 console.log(error, 'error')
-                if(error.response.status === 401){
+                if (error.response.status === 401) {
                     forbiddenAccessAlert()
-                }else{
+                } else {
                     errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
                 }
             }
@@ -175,7 +185,7 @@ class Calendario extends Component {
         waitAlert()
         const { access_token } = this.props.authUser
         const { form, evento } = this.state
-        await axios.post(URL_DEV + 'mercadotecnia/parrilla-contenido/comentario/' +evento.id, form, { headers: { Authorization: `Bearer ${access_token}` } }).then(
+        await axios.post(URL_DEV + 'mercadotecnia/parrilla-contenido/comentario/' + evento.id, form, { headers: { Authorization: `Bearer ${access_token}` } }).then(
             (response) => {
                 doneAlert('Comentario agregado con éxito');
                 this.setState({
@@ -186,9 +196,9 @@ class Calendario extends Component {
             },
             (error) => {
                 console.log(error, 'error')
-                if(error.response.status === 401){
+                if (error.response.status === 401) {
                     forbiddenAccessAlert()
-                }else{
+                } else {
                     errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
                 }
             }
@@ -199,8 +209,8 @@ class Calendario extends Component {
     }
 
     openModal = () => {
-        const { modal} = this.state
-        modal.form =true
+        const { modal } = this.state
+        modal.form = true
         this.setState({
             ...this.state,
             modal,
@@ -211,8 +221,8 @@ class Calendario extends Component {
     }
 
     handleCloseForm = () => {
-        const { modal} = this.state
-        modal.form =false
+        const { modal } = this.state
+        modal.form = false
         this.setState({
             ...this.state,
             modal,
@@ -236,15 +246,29 @@ class Calendario extends Component {
         const { form } = this.state
         let aux = Object.keys(form)
         aux.map((element) => {
-            switch(element){
+            switch (element) {
                 case 'typeContent':
-                    form[element] =  "contenido";
+                    form[element] = "contenido";
                     break;
                 case 'hora':
                     form[element] = "09";
                     break;
                 case 'minuto':
                     form[element] = "00";
+                    break;
+                case 'adjuntos':
+                    form[element] = {
+                        adjunto: {
+                            value: '',
+                            placeholder: 'Adjunto',
+                            files: []
+                        },
+                        adjunto_comentario: {
+                            value: '',
+                            placeholder: 'Adjunto',
+                            files: []
+                        }
+                    }
                     break;
                 default:
                     form[element] = '';
@@ -279,9 +303,9 @@ class Calendario extends Component {
     clickEvent = (evento) => {
         const { evento: event } = evento.event._def.extendedProps
         const { form, modal } = this.state
-        
+
         modal.form = true
-        
+
         form.copy = event.copy
         form.cta = event.cta
         form.comments = event.imagen
@@ -292,7 +316,7 @@ class Calendario extends Component {
 
         let aux = []
         aux = event.hora.split(":")
-        if(aux.length === 3){
+        if (aux.length === 3) {
             form.hora = aux[0].toString();
             form.minuto = aux[1].toString();
         }
@@ -303,10 +327,11 @@ class Calendario extends Component {
             form,
             modal,
             formeditado: 1,
-            activeKeyModal: "comments",
+            activeKeyModal: "form",
+            title: 'Información de contenido',
             evento: event
         })
-        console.log(evento.event._def.extendedProps.evento, 'evento')
+        // console.log(evento.event._def.extendedProps.evento, 'evento')
     }
 
     onChangeModalTab = key => {
@@ -318,36 +343,89 @@ class Calendario extends Component {
 
     renderEventContent = (eventInfo) => {
         const { evento: event } = eventInfo.event._def.extendedProps
+        // console.log(event)
         let aux = ''
-        if(event.red)
-            if(event.red.nombre)
+
+        if (event.red)
+            if (event.red.nombre)
                 aux = event.red.nombre.toLowerCase()
         let auxHora = ''
-            if(event.hora){
-                auxHora = event.hora.split(':')
-                if(auxHora.length === 3) auxHora = auxHora[0] + ':' + auxHora[1]
-                else auxHora = ''
-            }
-        return(
-            <OverlayTrigger overlay = {
+        if (event.hora) {
+            auxHora = event.hora.split(':')
+            if (auxHora.length === 3) auxHora = auxHora[0] + ':' + auxHora[1]
+            else auxHora = ''
+        }
+
+        return (
+            <OverlayTrigger overlay={
                 <Tooltip>
-                    {eventInfo.event.title}
-                    <br />
-                    {auxHora}
-                </Tooltip> }>
-                <div className = 'evento'
-                    onClick = { (e) => { e.preventDefault(); this.clickEvent(eventInfo) } }>
-                    <i className = { 'fab fa-' + aux +  " mr-3"}></i>
-                    <span>{eventInfo.event.title}</span>
+                    <span>
+                        <span>
+                            {eventInfo.event.title}
+                        </span>
+                        <div className="mt-3 font-weight-bolder">
+                            <div>
+                                {event.tipo_contenido.toUpperCase()}<span> - {auxHora}</span>
+                            </div>
+                        </div>
+                    </span>
+                </Tooltip>}>
+                <div className="d-flex justify-content-center align-items-center" onClick={(e) => { e.preventDefault(); this.clickEvent(eventInfo) }}>
+                    <span className={'btn btn-icon btn-sm ml-2 btn-light-' + aux}>
+                        <i className={'line-height-0 socicon-' + aux}></i>
+                    </span>
                 </div>
             </OverlayTrigger>
         )
     }
-
+    handleChange = (files, item) => {
+        const { form } = this.state
+        let aux = []
+        for (let counter = 0; counter < files.length; counter++) {
+            aux.push(
+                {
+                    name: files[counter].name,
+                    file: files[counter],
+                    url: URL.createObjectURL(files[counter]),
+                    key: counter
+                }
+            )
+        }
+        form['adjuntos'][item].value = files
+        form['adjuntos'][item].files = aux
+        this.setState({
+            ...this.state,
+            form
+        })
+    }
+    deleteContenido = (id) => {
+        this.deleteContenidoAxios(id)
+    }
+    async deleteContenidoAxios(id) {
+        const { access_token } = this.props.authUser
+        await axios.delete(URL_DEV + 'mercadotecnia/parrillas-de-contenido/' + id, { headers: { Authorization: `Bearer ${access_token}`, } }).then(
+            (response) => {
+                this.setState({
+                    ...this.state,
+                })
+            },
+            (error) => {
+                console.log(error, 'error')
+                if (error.response.status === 401) {
+                    forbiddenAccessAlert()
+                } else {
+                    errorAlert(error.response.data.message !== undefined ? error.response.data.message : 'Ocurrió un error desconocido, intenta de nuevo.')
+                }
+            }
+        ).catch((error) => {
+            errorAlert('Ocurrió un error desconocido catch, intenta de nuevo.')
+            console.log(error, 'error')
+        })
+    }
     render() {
 
-        const { modal, title, form, formeditado, options, content, data, empresa,activeKeyModal, evento } = this.state
-        
+        const { modal, title, form, formeditado, options, content, data, empresa, activeKeyModal, evento } = this.state
+
         return (
             <Layout active={"mercadotecnia"} {...this.props}>
                 <Card className="card-custom">
@@ -365,21 +443,21 @@ class Calendario extends Component {
                                 className="btn btn-light-success btn-sm font-weight-bold"
                                 only_icon="flaticon2-writing pr-0 mr-2"
                                 text='AGREGAR CONTENIDO'
-                                onClick={this.openModal} 
+                                onClick={this.openModal}
                             />
                         </div>
                     </Card.Header>
                     <Card.Body>
-                        <div className = 'parrilla'>
-                            <div className = 'd-flex justify-content-end mb-4'>
+                        <div className='parrilla'>
+                            <div className='d-flex justify-content-end mb-4'>
                                 <Nav className="nav-tabs nav-bold nav-tabs-line nav-tabs-line-3x border-0"
-                                    activeKey = { empresa.id } >
+                                    activeKey={empresa.id} >
                                     {
-                                        data.empresas.map( ( item, key ) => {
-                                            return(
-                                                <Nav.Item key = { key } onClick = { (e) => { e.preventDefault(); this.handleClickEmpresa(item) } } >
-                                                    <Nav.Link eventKey = { item.id }> 
-                                                        { item.name }
+                                        data.empresas.map((item, key) => {
+                                            return (
+                                                <Nav.Item key={key} onClick={(e) => { e.preventDefault(); this.handleClickEmpresa(item) }} >
+                                                    <Nav.Link eventKey={item.id}>
+                                                        {item.name}
                                                     </Nav.Link>
                                                 </Nav.Item>
                                             )
@@ -387,17 +465,18 @@ class Calendario extends Component {
                                     }
                                 </Nav>
                             </div>
-                            <FullCalendar locale = { esLocale } plugins = { [dayGridPlugin, interactionPlugin, bootstrapPlugin] } eventContent={this.renderEventContent}
-                                initialView = "dayGridMonth" weekends = { false } firstDay = { 1 } themeSystem = 'bootstrap' events = { content } />
+                            <FullCalendar locale={esLocale} plugins={[dayGridPlugin, interactionPlugin, bootstrapPlugin]} eventContent={this.renderEventContent}
+                                initialView="dayGridMonth" weekends={false} firstDay={1} themeSystem='bootstrap' events={content} />
                         </div>
-                        
+
                     </Card.Body>
                 </Card>
                 <Modal size="xl" title={title} show={modal.form} handleClose={this.handleCloseForm}>
-                    <ParrillaContenidoForm form = { form } formeditado = { formeditado }
-                        options = { options } onChange = { this.onChange } onSubmit = { this.sendParrillaAxios }
-                        onChangeModalTab = { this.onChangeModalTab } activeKey =  { activeKeyModal }
-                        addComentario = { this.addComentarioAxios } evento = { evento }
+                    <ParrillaContenidoForm form={form} formeditado={formeditado}
+                        options={options} onChange={this.onChange} onSubmit={this.sendParrillaAxios}
+                        onChangeModalTab={this.onChangeModalTab} activeKey={activeKeyModal}
+                        addComentario={this.addComentarioAxios} evento={evento} handleChange={this.handleChange} 
+                        deleteContenido={this.deleteContenido}
                     />
                 </Modal>
             </Layout>
