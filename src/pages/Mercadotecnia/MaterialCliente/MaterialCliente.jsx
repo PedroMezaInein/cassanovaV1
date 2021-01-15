b78e6c37ceb4807da7edf4861ef5178a17e51aac
CariJG <64931832+CariJG@users.noreply.github.com>
2021-01-15 13:30:11 -0600

    Correccion de nuevo folder

--- src/pages/Mercadotecnia/MaterialCliente/MaterialCliente.jsx
+++ src/pages/Mercadotecnia/MaterialCliente/MaterialCliente.jsx
@@ -978,13 +978,12 @@ class MaterialCliente extends Component {
                                                             />
                                                             {
                                                                 actualSubMenuCarpeta === "RENDERS" ?
-                                                                    <div className="row mx-0 col-md-12 row-paddingless px-0 d-flex justify-content-center">
-                                                                        <div className="col-md-4">
+                                                                    <div className="row mx-0 px-0 d-flex justify-content-center col-md-12">
                                                                             {
                                                                                 Object.keys(form.adjuntos.renders).map((nombreRenderCarpeta, key) => {
                                                                                     if (nombreRenderCarpeta !== "placeholder")
                                                                                         return (
-                                                                                            <div className='col-md-12' key={key}>
+                                                                                            <div className='col-md-3' key={key}>
                                                                                                 <FolderStatic
                                                                                                     text={form.adjuntos.renders[nombreRenderCarpeta].placeholder}
                                                                                                     onClick={this.onClickFolderRender}
@@ -994,7 +993,6 @@ class MaterialCliente extends Component {
                                                                                         )
                                                                                 })
                                                                             }
-                                                                        </div>
                                                                     </div>
                                                                     :
                                                                     adjuntosSubMenu.length === 0 ?
@@ -1015,13 +1013,12 @@ class MaterialCliente extends Component {
                                                         </div>
                                                         :
                                                         submenuactive ?
-                                                            <div className="row mx-0 col-md-12 row-paddingless px-0 d-flex justify-content-center">
-                                                                <div className="col-md-3">
+                                                            <div className="row mx-0 px-0 d-flex justify-content-center">
                                                                     {
                                                                         Object.keys(form.adjuntos).map((subcarpeta, key) => {
                                                                             if (subcarpeta !== "slider")
                                                                                 return (
-                                                                                    <div className='col-md-12' key={key}>
+                                                                                    <div className='col-md-3' key={key}>
                                                                                         <FolderStatic
                                                                                             text={subcarpeta}
                                                                                             onClick={this.onClickFolderSubMenu}
@@ -1031,7 +1028,6 @@ class MaterialCliente extends Component {
                                                                                 )
                                                                         })
                                                                     }
-                                                                </div>
                                                             </div>
                                                             :
                                                             <div className='col-md-12'>
