4b84df923f1d8ca3397fb12de3603336becd9e68
OmarAbaroa <omarac1295@gmail.com>
2021-01-19 13:11:05 -0600

    Setter y plan de trabajo

--- src/pages/Mercadotecnia/PlanTrabajo/PlanTrabajo.jsx
+++ src/pages/Mercadotecnia/PlanTrabajo/PlanTrabajo.jsx
@@ -6,27 +6,18 @@ import { Card } from 'react-bootstrap'
 import { URL_DEV } from '../../../constants'
 import { Button, SelectSearch } from '../../../components/form-components'
 import { errorAlert, forbiddenAccessAlert } from '../../../functions/alert'
-import Chart from 'react-google-charts'
+import { getAños, getMeses } from '../../../functions/setters'
 
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 class PlanTrabajo extends Component{
 
     state = {
         mes: meses[new Date().getMonth()],
+        año: new Date().getFullYear().toString(),
         form:{
             mes: meses[new Date().getMonth()],
         },
         data: [],
-        options: [
-            { type: 'string', label: 'Task ID' },
-            { type: 'string', label: 'Task Name' },
-            { type: 'string', label: 'Resource' },
-            { type: 'date', label: 'Start Date' },
-            { type: 'date', label: 'Fecha fin' },
-            { type: 'number', label: 'Duración' },
-            { type: 'number', label: 'Percent Complete' },
-            { type: 'string', label: 'Dependencies' },
-        ]
     }
 
     componentDidMount(){
@@ -39,6 +30,50 @@ class PlanTrabajo extends Component{
         this.getContentAxios()
     }
 
+    updateAño = value => {
+        this.setState({...this.state, año: value })
+    }
+
+    updateMes = value => {
+        this.setState({...this.state, mes: value})
+    }
+
+    isActiveNextButton = () => {
+        const { mes, año } = this.state
+        let esteAño = new Date().getFullYear()
+        let esteMes = new Date().getMonth()
+        if(esteMes > 9){
+            if((esteAño + 1).toString() === año )
+                if(mes === 'Diciembre')
+                    return 'disabled'
+        }else{
+            if((esteAño).toString() === año )
+                if(mes === 'Diciembre')
+                    return 'disabled'
+        }
+        return 'enabled'
+    }
+
+    isActiveBackButton = () => {
+        const { mes, año } = this.state
+        let esteAño = new Date().getFullYear()
+        let esteMes = new Date().getMonth()
+        if(esteMes > 9){
+            if((esteAño + 1).toString() === año )
+                if(mes === 'Diciembre')
+                    return 'disabled'
+        }else{
+            if((esteAño).toString() === año )
+                if(mes === 'Diciembre')
+                    return 'disabled'
+        }
+        return 'enabled'
+    }
+
+    nextMonth = () => {
+        const { mes } = this.state
+    }
+
     // ANCHOR ASYNC CALL TO GET CONTENT
     getContentAxios = async() => {
         const { access_token } = this.props.authUser
@@ -46,53 +81,7 @@ class PlanTrabajo extends Component{
         console.log(URL_DEV, "URL_DEV")
         await axios.get(`${URL_DEV}mercadotecnia/plan-trabajo/${mes}`, { headers: {Authorization: `Bearer ${access_token}`} } ).then(
             (response) => {
-                const { options } = this.state
-                let data = [ options, 
-                    [
-                        '1',
-                        'Junta con cliente',
-                        'ventas',
-                        new Date(2021, 0, 1),
-                        null,
-                        1 * 24 * 60 * 60 * 1000,
-                        100,
-                        null
-                    ],
-                    [
-                        '2',
-                        'Presupuesto de obra',
-                        'obra',
-                        new Date(2021, 0, 2),
-                        null,
-                        4 * 24 * 60 * 60 * 1000,
-                        100,
-                        '1'
-                    ],
-                    [
-                        '3',
-                        'Compra de materiales',
-                        'ventas',
-                        new Date(2021, 0, 4),
-                        null,
-                        2 * 24 * 60 * 60 * 1000,
-                        100,
-                        '1'
-                    ],
-                    [
-                        '4',
-                        'Ejecución de obra',
-                        'obra',
-                        new Date(2021, 0, 6),
-                        null,
-                        25 * 24 * 60 * 60 * 1000,
-                        100,
-                        '2,3'
-                    ]
-                ]
-                this.setState({
-                    ...this.state,
-                    data: data
-                })
+                
             },
             (error) => {
                 console.log(error, 'error')
@@ -104,26 +93,9 @@ class PlanTrabajo extends Component{
             console.log(error, 'error')
         }) 
     }
-
-    getMeses = () => {
-        return [
-            { name: 'Enero', value: 'Enero' },
-            { name: 'Febrero', value: 'Febrero' },
-            { name: 'Marzo', value: 'Marzo' },
-            { name: 'Abril', value: 'Abril' },
-            { name: 'Mayo', value: 'Mayo' },
-            { name: 'Junio', value: 'Junio' },
-            { name: 'Julio', value: 'Julio' },
-            { name: 'Agosto', value: 'Agosto' },
-            { name: 'Septiembre', value: 'Septiembre' },
-            { name: 'Octubre', value: 'Octubre' },
-            { name: 'Noviembre', value: 'Noviembre' },
-            { name: 'Diciembre', value: 'Diciembre' }
-        ]
-    }
     
     render(){
-        const { form, mes, options, data } = this.state
+        const { mes, año } = this.state
         return(
             <Layout active = 'mercadotecnia' { ... this.props}>
                 <Card className = 'card-custom'>
@@ -131,58 +103,39 @@ class PlanTrabajo extends Component{
                         <div className="d-flex align-items-center">
                             <h3 className="card-title align-items-start flex-column">
                                 <span className="font-weight-bolder text-dark">
-                                    Plan de trabajo {meses[mes]}
+                                    Plan de trabajo
                                 </span>
                             </h3>
                         </div>
                         <div className="card-toolbar align-items-center">
                             <div className = 'mr-3 d-flex'>
-                                <SelectSearch name = 'mes' options = { this.getMeses() } value = { mes }
+                                <SelectSearch name = 'mes' options = { getMeses() } value = { mes }
                                     onChange = { this.updateMes } iconclass = "fas fa-calendar-day"
                                     messageinc = "Incorrecto. Selecciona el mes." />
                             </div>
-                            <Button icon = '' className = 'btn btn-light-success btn-sm font-weight-bold' 
-                                only_icon = 'flaticon2-writing pr-0 mr-2' text = 'Agendar plan'
-                                onClick = { console.log('Parrilla de contenido') } />
+                            <div className = 'd-flex'>
+                                <SelectSearch name = 'mes' options = { getAños() } value = { año }
+                                    onChange = { this.updateAño } iconclass = "fas fa-calendar-day"
+                                    messageinc = "Incorrecto. Selecciona el año." />
+                            </div>
                         </div>
                     </Card.Header>
                     <Card.Body>
-                        <div className = ''>
-                            <Chart width = '100%' height = '400px' chartType = 'Gantt' loader = { <div>Cargando</div>}
-                                chartLanguage = 'es'
-                                data = { data } options = { 
-                                    {
-                                        gantt: {
-                                            criticalPathEnabled: false,
-                                            defaultStartDateMillis: new Date(2021,0,1),
-                                        }
-                                    }
-                                } 
-                                chartEvents = { [
-                                    {
-                                        eventName: 'select',
-                                        callback: ({ chartWrapper }) => {
-                                            const chart = chartWrapper.getChart()
-                                            const selection = chart.getSelection()
-                                            if (selection.length === 1) {
-                                                const [selectedItem] = selection
-                                                const dataTable = chartWrapper.getDataTable()
-                                                const { row } = selectedItem
-                                                alert(
-                                                    'You selected : ' +
-                                                    JSON.stringify({
-                                                        row,
-                                                        value: dataTable.getValue(row, 0),
-                                                    }),
-                                                    null,
-                                                    2,
-                                                )
-                                            }
-                                            console.log(selection, chart)
-                                        },
-                                    },
-                                ]}
-                                />
+                        <div className = 'd-flex justify-content-between'>
+                            <div>
+                                {`${mes} ${año}`}
+                            </div>
+                            <div>
+                                <div class="btn-group">
+                                    <button class={`fc-prev-button btn btn-primary ${this.isActiveBackButton()}`} type="button" aria-label="prev">
+                                        <span class="fa fa-chevron-left"></span>
+                                    </button>
+                                    <button class={`fc-next-button btn btn-primary ${this.isActiveNextButton()}`} type="button" aria-label="next"
+                                        onClick = { this.nextMonth }>
+                                        <span class="fa fa-chevron-right"></span>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </Card.Body>
                 </Card>
