38fe7695180fd8a289120a8a5cee29bd93a2d169
CariJG <64931832+CariJG@users.noreply.github.com>
2021-01-21 11:04:42 -0600

    Correcciones de plan de trabajo

--- src/components/forms/mercadotecnia/PlanTrabajoForm.jsx
+++ src/components/forms/mercadotecnia/PlanTrabajoForm.jsx
@@ -4,7 +4,7 @@ import { validateAlert } from '../../../functions/alert';
 import { Button, Input, RangeCalendar, TagSelectSearch, CircleColor } from '../../form-components';
 const $ = require('jquery');
 
-const colors = ["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722", "#795548", "#607d8b"];
+const colors = ["#20ACE9", "#EE4C9E", "#62D270 ", "#E63850", "#A962E2", "#E4C127", "#1D69E1", "#8C5E4D", "##737373"];
 
 class PlanTrabajoForm extends Component{
     updateRangeCalendar = range => {
