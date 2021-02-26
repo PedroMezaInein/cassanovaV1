4abf76ad9794fb1b80b52f67ac2f97e3bd02101d
CariJG <64931832+CariJG@users.noreply.github.com>
2021-02-26 09:48:46 -0600

    Cambio de color rojo

--- src/constants.js
+++ src/constants.js
@@ -11,7 +11,7 @@ import {
     faAtlas, faCubes
 } from '@fortawesome/free-solid-svg-icons'
 
-export const COLORES_GRAFICAS_MESES = [ '#EF5350', '#EC407A', '#AB47BC', '#5C6BC0', '#42A5F5', '#26C6DA', 
+export const COLORES_GRAFICAS_MESES = [ '#F64E60', '#EC407A', '#AB47BC', '#5C6BC0', '#42A5F5', '#26C6DA', 
     '#26A69A', '#66BB6A', '#D4E157', '#FFEE58', '#FFA726', '#FF7043', '#8D6E63', '#78909C' ]
 
 export const COLORES_GRAFICAS = [
