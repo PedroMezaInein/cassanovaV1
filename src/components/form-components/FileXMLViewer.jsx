db95839edc9b9d77d661972e470c25a9f95bdff0
CariJG <64931832+CariJG@users.noreply.github.com>
2021-03-05 14:55:07 -0600

    Se elimino el comentario

--- src/components/form-components/FileXMLViewer.jsx
+++ src/components/form-components/FileXMLViewer.jsx
@@ -13,13 +13,6 @@ const customTheme = {
 }
 
 class FileXMLViewer extends Component{
-    // xml(){
-    //     let xml= `<?xml version="1.0" encoding="UTF-8"?>
-    //     <sferp:ComprobanteFiscal xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:sferp='http://www.solucionfactible.com/sferp/conector/cfd33' xsi:schemaLocation='http://www.solucionfactible.com/sferp/conector/cfd33  http://www.sat.gob.mx/Pagos http://www.sat.gob.mx/sitio_internet/cfd/Pagos/Pagos10.xsd' fecha="2017-08-09T00:01:15" folio="23554991" serie="OAS33" subTotal="40.00" total="41.20" descuento="3.00" motivoDescuento="PRONTO PAGO" metodoDePago="PUE" condicionesDePago="" estatus="vigente" numCtaPago="" formaDePago="01">
-            
-    //     </sferp:ComprobanteFiscal>`;
-    //     return xml;
-    // }
     render () {
         const { xml } = this.props
         return (
