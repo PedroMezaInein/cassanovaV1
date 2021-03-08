a445a78489e1d468393bcaf733d6cfda2d78eb70
CariJG <64931832+CariJG@users.noreply.github.com>
2021-03-08 12:07:18 -0600

    Paleta de colores nueva

--- src/constants.js
+++ src/constants.js
@@ -110,7 +110,8 @@ export const ICONS_MODULES = {
     'faAtlas':faAtlas, 'faCubes':faCubes
 }
 //Paleta de colores
-export const COLORS = ["#20ACE9", "#EE4C9E", "#62D270", "#E63850", "#A962E2", "#E4C127", "#1D69E1", "#8C5E4D", "#737373"];
+// export const COLORS = ["#20ACE9", "#EE4C9E", "#62D270", "#E63850", "#A962E2", "#E4C127", "#1D69E1", "#8C5E4D", "#737373"];
+export const COLORS = ["#E53935", "#CB4335", "#AB47BC", "#7D3C98", "#C15994", "#2471A3", "#2E86C1","#42A5F5", "#4DD0E1", "#17A589", "#229954", "#28B463", "#9CCC65", "#FFC42C", "#D4AC0D", "#D68910", "#CA6F1E", "#FF7043", "#9F6A57", "#707B7C"];
 
 //Expresiones Regulares
 
