5ffdf48ae5b696b02e6586d5db707d557808d13d
OmarAbaroa <omarac1295@gmail.com>
2021-03-22 12:43:24 -0600

    One lead

--- src/pages/Leads/CRM/Crm.jsx
+++ src/pages/Leads/CRM/Crm.jsx
@@ -26,6 +26,7 @@ import Pagination from "react-js-pagination"
 import SymbolIcon from '../../../components/singles/SymbolIcon'
 import Moment from 'react-moment'
 import FileItem from '../../../components/singles/FileItem'
+import { OneLead } from '../../../components/modal'
 const $ = require('jquery');
 class Crm extends Component {
 
@@ -1759,14 +1760,6 @@ class Crm extends Component {
         )
     }
 
-    noHayAdjuntos = () => {
-        return(
-            <tr className="text-center text-dark-75">
-                <th className="pl-2" colSpan = "3" >NO HAY ADJUNTOS</th>
-            </tr>
-        )
-    }
-
     onSubmitRRHHPP = e => {
         e.preventDefault()
         const { title } = this.state
@@ -2145,227 +2138,11 @@ class Crm extends Component {
                 {/* ANCHOR MODAL SINGLE ONE LEAD */}
                 <Modal size="xl" title='INFORMACIÓN DEL LEAD' 
                     show = { modal_one_lead } handleClose = { this.handleCloseModalOneLead } >
-                    <div className = "row mx-0">
-                        <div className = "col-md-12">
-                            <div className = "d-flex justify-content-between mt-4">
-                                <p className = "font-size-h5 text-muted font-size-lg mt-0">
-                                    Nombre:&nbsp; <strong className="font-size-h6">{lead.nombre}</strong>
-                                </p>
-                                {
-                                    lead.estatus ? 
-                                        <span className="navi-link">
-                                            <span className="navi-text">
-                                                <span className="label label-xl label-inline w-100 font-weight-bolder" 
-                                                    style = { { backgroundColor: lead.estatus.color_fondo, color: lead.estatus.color_texto, border: 'transparent' } }>
-                                                    { lead.estatus.estatus.toUpperCase() }
-                                                </span>
-                                            </span>
-                                        </span>
-                                    : ''
-                                }
-                            </div>
-                            <div className="separator separator-solid mb-3"></div>
-                        </div>
-                    </div>
-                    <Tab.Container defaultActiveKey = 'info'>
-                        {
-                            lead.prospecto ?
-                                lead.prospecto.contactos ?
-                                    lead.prospecto.contactos.length > 0 ?
-                                        <Nav className="nav-tabs nav-bold nav-tabs-line nav-tabs-line-3x border-0 mb-8 justify-content-end">
-                                            <Nav.Item className="nav-item">
-                                                <Nav.Link eventKey="info">
-                                                    <span className="nav-text font-weight-bold">INFORMACIÓN GENERAL</span>
-                                                </Nav.Link>
-                                            </Nav.Item>
-                                            <Nav.Item className="nav-item">
-                                                <Nav.Link eventKey="contactos">
-                                                    <span className="nav-text font-weight-bold">HISTORIAL DE CONTACTO</span>
-                                                </Nav.Link>
-                                            </Nav.Item>
-                                            {
-                                                lead.presupuesto_diseño ?
-                                                    lead.presupuesto_diseño.pdfs ?
-                                                        lead.presupuesto_diseño.pdfs.length > 0 ?
-                                                            <Nav.Item className="nav-item">
-                                                                <Nav.Link eventKey="presupuesto">
-                                                                    <span className="nav-text font-weight-bold">Presupuesto</span>
-                                                                </Nav.Link>
-                                                            </Nav.Item>
-                                                        : ''
-                                                    : ''
-                                                : ''
-                                            }
-                                        </Nav>
-                                    : ''
-                                : ''
-                            : ''
-                        }
-                        <Tab.Content>
-                            <Tab.Pane eventKey = 'presupuesto'>
-                                <div className="table-responsive mt-4">
-                                    <table className="table table-vertical-center">
-                                        <thead className="thead-light">
-                                            <tr className="text-left text-dark-75">
-                                                <th className="pl-2" style={{ minWidth: "150px" }}>Adjunto</th>
-                                                <th style={{ minWidth: "80px" }} className="text-center">Fecha</th>
-                                                <th style={{ minWidth: "80px" }} className="text-center">Fecha de envio</th>
-                                                {/* <th className="pr-0 text-right" style={{ minWidth: "70px" }}></th> */}
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {
-                                                lead ?
-                                                    lead.presupuesto_diseño ?
-                                                        lead.presupuesto_diseño.pdfs ?
-                                                            lead.presupuesto_diseño.pdfs.length > 0 ?
-                                                                lead.presupuesto_diseño.pdfs.map((pdf, key) => {
-                                                                    return(
-                                                                        <FileItem item = { pdf } key = { key }
-                                                                            anotherDate = { pdf ? pdf.pivot ? pdf.pivot.fecha_envio ? pdf.pivot.fecha_envio : '' : '' : '' } />
-                                                                    )
-                                                                })
-                                                            : this.noHayAdjuntos()
-                                                        : this.noHayAdjuntos()
-                                                    : this.noHayAdjuntos()
-                                                : this.noHayAdjuntos()           
-                                            }
-                                        </tbody>
-                                    </table>
-                                </div >
-                            </Tab.Pane>
-                            <Tab.Pane eventKey = 'contactos'>
-                                <div className = "row mx-0 justify-content-center">
-                                    <div className="col-md-7 pt-4">
-                                        {
-                                            lead ?
-                                                lead.prospecto ?
-                                                    lead.prospecto.contactos.length === 0 ?
-                                                        <div className="text-center text-dark-75 font-weight-bolder font-size-lg">No se ha registrado ningún contacto</div>
-                                                    :
-                                                        lead.prospecto.contactos.map((contacto, key) => {
-                                                            let limiteInferior = (activePage - 1) * itemsPerPage
-                                                            let limiteSuperior = limiteInferior + (itemsPerPage - 1)
-                                                            if(contacto.length < itemsPerPage || ( key >= limiteInferior && key <= limiteSuperior))
-                                                                return this.printTimeLineContact(contacto, key)
-                                                            return false
-                                                        })
-                                                : <div className="text-center text-dark-75 font-weight-bolder font-size-lg">No se ha registrado ningún contacto</div>
-                                            : <div className="text-center text-dark-75 font-weight-bolder font-size-lg">No se ha registrado ningún contacto</div>
-                                        }
-                                        {
-                                            lead ? 
-                                                lead.prospecto ?
-                                                    lead.prospecto.contactos.length > itemsPerPage ?
-                                                        <div className="d-flex justify-content-center mt-4">
-                                                            <Pagination itemClass="page-item"  firstPageText = 'Primero' lastPageText = 'Último'
-                                                                activePage = { activePage } itemsCountPerPage = { itemsPerPage } totalItemsCount = { lead.prospecto.contactos.length }
-                                                                pageRangeDisplayed = { 5 } onChange={this.onChangePage.bind(this)} itemClassLast="d-none" 
-                                                                itemClassFirst="d-none" prevPageText={<i className='ki ki-bold-arrow-back icon-xs'/>}
-                                                                nextPageText={<i className='ki ki-bold-arrow-next icon-xs'/>}
-                                                                linkClassPrev="btn btn-icon btn-sm btn-light-primary mr-2 my-1 pagination"
-                                                                linkClassNext="btn btn-icon btn-sm btn-light-primary mr-2 my-1 pagination"
-                                                                linkClass="btn btn-icon btn-sm border-0 btn-hover-primary mr-2 my-1 pagination"
-                                                                activeLinkClass="btn btn-icon btn-sm border-0 btn-light btn-hover-primary active mr-2 my-1 pagination"
-                                                                />
-                                                        </div>
-                                                    : ''
-                                                : ''
-                                            : ''
-                                        }
-                                    </div>
-                                </div>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey = 'info'>
-                                <div className = "row mx-0 px-lg-2">
-                                    {
-                                        lead.empresa ? 
-                                            <div className="col-md-3 text-truncate">
-                                                <div className="d-flex justify-content-start">
-                                                    <SymbolIcon tipo = 'info' urlIcon = '/images/svg/Building.svg' />
-                                                    <div>
-                                                        <div className="font-size-h6 text-dark-75 font-weight-bolder">{lead.empresa.name}</div>
-                                                        <div className="font-size-sm text-muted font-weight-bold mt-1">Empresa</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        : ''
-                                    }
-                                    <div className="col-md-3 text-truncate">
-                                        <div className="d-flex justify-content-start mr-2">
-                                            <SymbolIcon tipo = 'primary' urlIcon = '/images/svg/iPhone-X.svg' />
-                                            <div>
-                                                <a target="_blank" href={`tel:+${lead.telefono}`} rel="noopener noreferrer"
-                                                    className="font-size-h6 text-dark-75 font-weight-bolder text-hover-primary">
-                                                    { lead.telefono }
-                                                </a>
-                                                <div className="font-size-sm text-muted font-weight-bold mt-1">TELÉFONO</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-3 text-truncate">
-                                        <div className="d-flex justify-content-start mr-2">
-                                            <SymbolIcon tipo = 'info' urlIcon = '/images/svg/Box1.svg' />
-                                            <div>
-                                                <div className="font-size-h6 text-dark-75 font-weight-bolder"><Moment format="DD/MM/YYYY">{lead.created_at}</Moment></div>
-                                                <div className="font-size-sm text-muted font-weight-bold mt-1">FECHA</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-3 text-truncate">
-                                        <div className="d-flex justify-content-start mr-2">
-                                            <SymbolIcon tipo = 'primary' urlIcon = '/images/svg/Mail.svg' />
-                                            <div>
-                                                <a target="_blank" href={`mailto:+${lead.email}`} rel="noopener noreferrer"
-                                                    className="font-size-h6 text-dark-75 font-weight-bolder text-hover-primary">
-                                                    { lead.email }
-                                                </a>
-                                                <div className="font-size-sm text-muted font-weight-bold mt-1">CORREO ELECTRÓNICO</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {
-                                        lead.origen ? 
-                                            <div className="col-md-3 mt-4 text-truncate">
-                                                <div className="d-flex justify-content-start mr-2">
-                                                    <SymbolIcon tipo = 'primary' urlIcon = '/images/svg/Folder-cloud.svg' />
-                                                    <div>
-                                                        <div className="font-size-h6 text-dark-75 font-weight-bolder">{lead.origen.origen}</div>
-                                                        <div className="font-size-sm text-muted font-weight-bold mt-1">Origen</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        : ''
-                                    }
-                                    <div className="col-md-9 mt-4 text-truncate">
-                                        <div className="d-flex justify-content-start mr-2">
-                                            <SymbolIcon tipo = 'info' urlIcon = '/images/svg/Tools.svg' />
-                                            <div>
-                                                <ul className="list-inline mb-0 font-size-h6 text-dark-75 font-weight-bolder">
-                                                    {
-                                                        lead.servicios ? 
-                                                            lead.servicios.map((servicio, key) => {
-                                                                return (
-                                                                    <li className="list-inline-item" key={key}>&#8226; {servicio.servicio}</li>
-                                                                )
-                                                            })
-                                                        :
-                                                            <li className="list-inline-item">No hay servicios registrados</li>
-                                                    }
-                                                </ul>
-                                                <div className="font-size-sm text-muted font-weight-bold mt-1">Servicios</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className = 'col-md-12 mt-4'>
-                                        <div className="bg-gray-100 p-3 font-size-lg font-weight-light text-justify" >
-                                            <strong >Comentario: </strong>{lead.comentario}
-                                        </div>
-                                    </div>
-                                </div>
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Tab.Container>
+                    {
+                        lead ? 
+                            <OneLead lead = { lead } eliminarContacto = { this.eliminarContacto } />
+                        : ''
+                    }
                 </Modal>
                 <Modal size="xl" title={title} show={modal_formRRHHP} handleClose={this.handleCloseFormRRHHP} >
                     <FormProveedoresRRHH
