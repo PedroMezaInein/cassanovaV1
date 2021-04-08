9f58be015e3abf4d77ec0ef7d48a460b41a75fb0
OmarAbaroa <omarac1295@gmail.com>
2021-04-08 13:05:56 -0500

    Single tagify

--- src/pages/Usuarios/Usuarios/Usuarios.jsx
+++ src/pages/Usuarios/Usuarios/Usuarios.jsx
@@ -6,8 +6,8 @@ import { URL_DEV, USUARIOS, CLIENTES } from '../../../constants'
 import { Modal, ModalDelete, SingleTagify } from '../../../components/singles'
 import { RegisterUserForm, PermisosForm } from '../../../components/forms'
 import Swal from 'sweetalert2'
-import { setOptions, setSelectOptions, setTextTableReactDom, setListTableReactDom } from '../../../functions/setters'
-import { printResponseErrorAlert, errorAlert, waitAlert, doneAlert, questionAlertY, customInputAlert} from '../../../functions/alert'
+import { setOptions, setSelectOptions, setTextTableReactDom, setListTableReactDom, setTagLabelReactDom } from '../../../functions/setters'
+import { printResponseErrorAlert, errorAlert, waitAlert, doneAlert, questionAlertY, customInputAlert, questionAlert} from '../../../functions/alert'
 import NewTableServerRender from '../../../components/tables/NewTableServerRender'
 import { save, deleteForm } from '../../../redux/reducers/formulario'
 import FloatButtons from '../../../components/singles/FloatButtons'
@@ -320,33 +320,26 @@ class Usuarios extends Component {
         return form;
     }
 
-    setTagLabelReactDom = (user, arreglo) => {
+    /* setTagLabelReactDom = (user, arreglo, tipo) => {
         return (
             <div className="d-flex flex-wrap tr-hover">
                 {
                     arreglo.map((element, index) => {
                         return(
-                            <SingleTagify />
-                            
+                            <div key = { index } >
+                                <SingleTagify element = { element } color = { index % 2 ? 'success' : 'primary' } 
+                                    onClick = { (e) => { questionAlert(
+                                            '¿ESTÁS SEGURO?', 
+                                            `ELIMINARÁS EL DEPARTAMENTO ${element.nombre} DEL USUARIO ${user.name}`,
+                                            () => this.deleteElementAxios(user, element, tipo)
+                                        ) } }/>
+                            </div>
                         )
-                        {/* <div className="tagify pr-2 pb-2 d-flex justify-content-center align-items-center w-auto" 
-                                tabIndex="-1" style={{ borderWidth: "0px" }} key={index}>
-                                <div className=" image-upload d-flex px-3 align-items-center tagify__tag tagify__tag--success tagify--noAnim"  >
-                                    <div title="Borrar archivo" className="tagify__tag__removeBtn" role="button"
-                                        aria-label="remove tag" >
-                                    </div>
-                                    <div>
-                                        <span className="tagify__tag-text p-1 white-space"  >
-                                            {element.nombre}
-                                        </span>
-                                    </div>
-                                </div>
-                            </div> */}
                     })
                 }
             </div>
         )
-    }
+    } */
 
     setUsers = users => {
         let aux = []
@@ -360,8 +353,7 @@ class Usuarios extends Component {
                             email: setTextTableReactDom(user.email, this.doubleClick, user, 'email', 'text-center'),
                             departamento: user.departamentos.length === 0 ? 
                                 setTextTableCenter("Sin definir") 
-                                : this.setTagLabelReactDom(user, user.departamentos),
-                                    /* setListTableReactDom(user.departamentos, "nombre", '', this.doubleClick, user, 'departamentos', ''), */
+                                : setTagLabelReactDom(user, user.departamentos, 'departamento', this.deleteElementAxios),
                             id: user.id
                         }
                     )
@@ -372,7 +364,9 @@ class Usuarios extends Component {
                             actions: this.setActions(user),
                             name: setTextTableReactDom(user.name, this.doubleClick, user, 'name', 'text-center'),
                             email: setTextTableReactDom(user.email, this.doubleClick, user, 'email', 'text-center'),
-                            proyecto: user.proyectos.length === 0 ?  setTextTableCenter("Sin definir") : setListTableReactDom(user.proyectos, "nombre", '', this.doubleClick, user, 'proyectos', ''),       
+                            proyecto: user.proyectos.length === 0 ?  
+                                setTextTableCenter("Sin definir") 
+                                : setTagLabelReactDom(user, user.proyectos, 'proyecto', this.deleteElementAxios),
                             id: user.id
                         }
                     )
@@ -563,6 +557,30 @@ class Usuarios extends Component {
         })
         return form
     }
+
+    deleteElementAxios = async(user, element, tipo) => {
+        const { access_token } = this.props.authUser
+        const { form } = this.state
+        let value = form[tipo]
+        waitAlert()
+        await axios.delete(`${URL_DEV}v2/usuarios/usuarios/${user.id}/${tipo}/${element.id}`, 
+            { headers: { Authorization: `Bearer ${access_token}` } }).then(
+            (response) => {
+                const { key } = this.state
+                if(key === 'administrador')
+                    this.getAdministradorAxios()
+                if(key === 'empleados')
+                    this.getEmpleadosAxios()
+                if(key === 'clientes')
+                    this.getClientesAxios()
+                doneAlert(response.data.message !== undefined ? response.data.message : 'El rendimiento fue editado con éxito.')
+            }, (error) => { printResponseErrorAlert(error) }
+        ).catch((error) => {
+            errorAlert('Ocurrió un error desconocido catch, intenta de nuevo.')
+            console.log(error, 'error')
+        })
+    }
+
     patchUsuario = async( data,tipo ) => {
         const { access_token } = this.props.authUser
         const { form } = this.state
@@ -572,7 +590,13 @@ class Usuarios extends Component {
             { value: value }, 
             { headers: { Authorization: `Bearer ${access_token}` } }).then(
             (response) => {
-                this.getHerramientasAxios()
+                const { key } = this.state
+                if(key === 'administrador')
+                    this.getAdministradorAxios()
+                if(key === 'empleados')
+                    this.getEmpleadosAxios()
+                if(key === 'clientes')
+                    this.getClientesAxios()
                 doneAlert(response.data.message !== undefined ? response.data.message : 'El rendimiento fue editado con éxito.')
             }, (error) => { printResponseErrorAlert(error) }
         ).catch((error) => {
