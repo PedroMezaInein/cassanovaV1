21bd6c4b678061ebf8cb25a18aaf77d12480ff14
OmarAbaroa <omarac1295@gmail.com>
2021-04-13 12:01:07 -0500

    Actualizar redirect in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,35 +7,35 @@ class Home extends Component{
     componentDidMount(){
         const { user : { permisos } } = this.props.authUser
         const { history } = this.props
+
+        const calendarioTareas = permisos ? permisos.find(function(element, index) {
+            return element.modulo.slug === 'calendario-tareas'
+        }) : null;
+        if(calendarioTareas)
+            history.push('/usuarios/calendario-tareas')
         
         const crm = permisos ? permisos.find(function(element, index) {
             return element.modulo.slug === 'crm'
         }) : null;
+        if(crm)
+            history.push('/leads/crm')
+
         const tareas = permisos ? permisos.find(function(element, index) {
             return element.modulo.slug === 'tareas'
         }) : null;
+        if(tareas)
+            history.push('/usuarios/tareas')
+
         const proyecto = permisos ? permisos.find(function(element, index) {
             return element.modulo.slug === 'mi-proyecto'
         }) : null;
+        if(proyecto)
+            history.push('/mi-proyecto')
         
-        if(crm){
-            history.push('/leads/crm')
-        }
-        else{
-            if(tareas){
-                history.push('/usuarios/tareas')
-            }
-            else{
-                if(proyecto){
-                    history.push('/mi-proyecto')
-                }else{
-                    if(permisos === undefined)
-                        history.push('/login')
-                    else    
-                        history.push(permisos[0].modulo.url)
-                }
-            }
-        }
+        if(permisos === undefined)
+            history.push('/login')
+        else    
+            history.push(permisos[0].modulo.url)
     }
 
     render(){
