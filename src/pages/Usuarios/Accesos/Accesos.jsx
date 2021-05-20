a0368f0126e371f70dbd8ae7856da204f29c4e80
CariJG <64931832+CariJG@users.noreply.github.com>
2021-05-20 10:43:49 -0500

    Se eliminaron imports

--- src/pages/Usuarios/Accesos/Accesos.jsx
+++ src/pages/Usuarios/Accesos/Accesos.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Layout from '../../../components/layout/layout'
 import { connect } from 'react-redux'
-import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { errorAlert, printResponseErrorAlert, waitAlert, doneAlert, customInputAlert } from '../../../functions/alert'
 import axios from 'axios'
 import { URL_DEV, ACCESOS_COLUMNS, TEL } from '../../../constants'
