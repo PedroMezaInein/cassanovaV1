72ed214b78fab6bda1b08d26f378981e69782d14
CariJG <64931832+CariJG@users.noreply.github.com>
2021-06-07 10:16:42 -0500

    Corrección de options en responsables

--- src/components/forms/usuarios/tareas/AddTaskForm.jsx
+++ src/components/forms/usuarios/tareas/AddTaskForm.jsx
@@ -40,7 +40,14 @@ class AddTaskForm extends Component {
         onChange({target: { value: '', name: 'nuevo_tag'}}, true)
         this.setState({...this.state, newTag: false})
     }
-
+    transformarOptions = options => {  
+        options = options ? options : []
+        options.map( (value) => {
+            value.label = value.name 
+            return ''
+        });
+        return options
+    }
     render() {
         const { form, tarea, onChange, formeditado, options, handleCreateOption, handleChangeCreate, onSubmit, sendTag, ...props } = this.props
         const { newTag } = this.state
@@ -72,9 +79,10 @@ class AddTaskForm extends Component {
                                         messageinc = "Incorrecto. Ingresa el título de la tarea." />
                                 </div>
                                 <div className="col-md-12 px-0 mb-3">
-                                    <TagSelectSearchGray placeholder = 'Selecciona los responsables' options = { options.responsables } 
-                                        iconclass = 'las la-user-friends icon-xl' defaultvalue = { form.responsables } onChange = { this.updateResponsable }
-                                        requirevalidation={1} messageinc = "Incorrecto. Selecciona los responsables."  />
+                                    <TagSelectSearchGray placeholder = 'Selecciona los responsables' iconclass = 'las la-user-friends icon-xl'
+                                        onChange = { this.updateResponsable } requirevalidation={1} messageinc = "Incorrecto. Selecciona los responsables."  
+                                        options={this.transformarOptions(options.responsables)} defaultvalue={this.transformarOptions(form.responsables)}
+                                    />
                                 </div>
                                 <div className="col-md-12 px-0 mb-3">
                                     <InputGray withtaglabel = { 1 } withtextlabel = { 1 } withplaceholder = { 1 } withicon = { 0 } requirevalidation = { 0 } 
