50360dff151225ad22de82dd6d1c8501789a48df
OmarAbaroa <omarac1295@gmail.com>
2021-06-29 12:45:40 -0500

    Warning correction

--- src/components/form-components/Gray/TagInputGray.jsx
+++ src/components/form-components/Gray/TagInputGray.jsx
@@ -8,6 +8,7 @@ class TagInputGray extends Component {
     componentDidMount() {
         $('.react-tag-input').attr('id','react-tag-input')
     }
+    
     render() {
         const { placeholder, tags, onChange, iconclass, uppercase } = this.props
         return (
@@ -17,15 +18,8 @@ class TagInputGray extends Component {
                     <span className="input-icon input-icon-right">
                         <i className={iconclass + " m-0 kt-font-boldest text-dark-50"} style={{zIndex:'2'}}></i>
                     </span>
-                    <ReactTagInput
-                        tags = { tags }
-                        placeholder = 'PRESIONA ENTER PARA AGREGAR'
-                        editable = { true }
-                        readOnly = { false }
-                        removeOnBackspace = { true }
-                        onChange = { onChange }
-                        className = { uppercase === false ? 'text-transform-none' : '' }
-                    />
+                    <ReactTagInput tags = { tags } placeholder = 'PRESIONA ENTER PARA AGREGAR' editable = { true } readOnly = { false }
+                        removeOnBackspace = { true } onChange = { onChange } className = { uppercase === false ? 'text-transform-none' : '' } />
                 </div>
             </div>
         )
