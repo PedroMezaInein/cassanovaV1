3df72dbe5e1c8d54d2f20dec8b1208294baeec03
OmarAbaroa <omarac1295@gmail.com>
2021-07-08 17:53:37 -0500

    Calendario de instalacion

--- src/assets/animate/index.js
+++ src/assets/animate/index.js
@@ -1,4 +1,5 @@
 import * as UploadingFile from './uploadingFile.json'
 import * as Meetings from './meetings.json'
+import * as NoData from './no-data-error.json'
 
-export { UploadingFile, Meetings }
+export { UploadingFile, Meetings, NoData }
