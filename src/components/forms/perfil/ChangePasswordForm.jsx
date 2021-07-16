a12d4e0c42397eb3b872985d71f7c9f880b0574a
CariJG <64931832+CariJG@users.noreply.github.com>
2021-07-16 15:16:52 -0500

    Se eliminó bg de fotos de perfil

--- src/components/forms/perfil/ChangePasswordForm.jsx
+++ src/components/forms/perfil/ChangePasswordForm.jsx
@@ -24,11 +24,13 @@ class ChangePasswordFrom extends Component {
 		const { user, onSubmit, form, onChange, sendAvatar, clearAvatar, handleChange, sendCorreo, empresas, onClickEmpresa, activeKey, formeditado, cropped } = this.props
 		
 		return (<>
+		{
+			user&&
 			<Tab.Container defaultActiveKey="1" className="p-5">
 				<div className="card mb-6 mb-xl-9">
 					<div className="card-body pt-9 pb-0">
 						<div className="d-flex flex-wrap flex-sm-nowrap mb-3">
-							<div className="d-flex flex-center flex-shrink-0 bg-light rounded mr-7">
+							<div className="d-flex flex-center flex-shrink-0 rounded mr-7">
 								<img alt='avatar' className="rounded w-100px h-100px w-lg-150px h-lg-150px" src={user.avatar ? user.avatar : "/default.jpg"} />
 							</div>
 							<div className="flex-grow-1 align-self-center">
@@ -369,6 +371,8 @@ class ChangePasswordFrom extends Component {
 						</Col>
 					</Row >
 			</Tab.Container>
+		
+		}
 		</>
 			)
 	}
