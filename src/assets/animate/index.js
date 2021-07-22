465a8acd32a3457eadd29dc631a133377ae61819
OmarAbaroa <omarac1295@gmail.com>
2021-07-22 14:11:06 -0500

    Alertas

--- src/assets/animate/index.js
+++ src/assets/animate/index.js
@@ -2,4 +2,5 @@ import * as UploadingFile from './uploadingFile.json'
 import * as Meetings from './meetings.json'
 import * as NoData from './no-data-error.json'
 import * as QuestionBoy from './question-boy.json'
-export { UploadingFile, Meetings, NoData, QuestionBoy }
+import * as Trash from './trash-clean.json'
+export { UploadingFile, Meetings, NoData, QuestionBoy, Trash }
