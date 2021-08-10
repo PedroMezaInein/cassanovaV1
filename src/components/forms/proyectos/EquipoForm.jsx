d7c3f6bbc9142ade51c71419642d7fbe016ec985
CariJG <64931832+CariJG@users.noreply.github.com>
2021-08-10 11:28:30 -0500

    Cambio de columnas

--- src/components/forms/proyectos/EquipoForm.jsx
+++ src/components/forms/proyectos/EquipoForm.jsx
@@ -15,39 +15,40 @@ class EquipoForm extends Component {
         const { form, formeditado, onChange, options, handleChange, onSubmit } = this.props
         return (
             <Form id = 'form-equipo'>
-                <div className="row form-group-marginless mx-0 my-6 justify-content-center">
-                    <div className="col-md-3">
+                <div className="row form-group-marginless mx-0 my-6">
+                    <div className="col-md-4">
                         <Input requirevalidation = { 1 } formeditado = { formeditado } name = "equipo" value = { form.equipo }
                             onChange = { onChange } type = "text" placeholder="NOMBRE DEL EQUIPO" iconclass = "fas fa-desktop"
                             messageinc="Incorrecto. Ingresa el nombre del equipo." />
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-4">
                         <Input requirevalidation = { 1 } formeditado = { formeditado } name = "marca" value = { form.marca }
                             onChange = { onChange } type = "text" placeholder="MARCA DEL EQUIPO" iconclass = "fab fa-apple"
                             messageinc="Incorrecto. Ingresa la marca del equipo." />
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-4">
                         <Input requirevalidation = { 1 } formeditado = { formeditado } name = "modelo" value = { form.modelo }
                             onChange = { onChange } type = "text" placeholder="MODELO DEL EQUIPO" iconclass = "fas fa-desktop"
                             messageinc="Incorrecto. Ingresa el model del equipo." />
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-12 separator separator-dashed mb-5 mt-8" />
+                    <div className="col-md-6">
                         <SelectSearch options = { options.proveedores } placeholder = "SELECCIONA EL PROVEEDOR" name = "proveedor" 
                             value = { form.proveedor } onChange = { (value) => this.updateSelect(value, 'proveedor') } 
                             iconclass = 'far fa-user' messageinc="Incorrecto. Selecciona el proveedor" />
                     </div>
-                    <div className="col-md-12 separator separator-dashed my-5" />
-                    <div className="col-md-3">
+                    <div className="col-md-6">
                         <SelectSearch options = { options.partidas } placeholder = "SELECCIONA LA PARTIDA" name = "partida" 
                             value = { form.partida } onChange = { (value) => this.updateSelect(value, 'partida') } 
                             iconclass = 'fas fa-book' messageinc="Incorrecto. Selecciona la partida" />
                     </div>
-                    <div className="col-md-9 mb-3">
-                        <Input requirevalidation = { 0 } formeditado = { formeditado } placeholder = "OBSERVACIONES" as = "textarea" rows = "1"
+                    <div className="col-md-12 separator separator-dashed mb-4 mt-8" />
+                    <div className="col-md-12 mb-3">
+                        <Input requirevalidation = { 0 } formeditado = { formeditado } placeholder = "OBSERVACIONES" as = "textarea"
                             name = "observaciones" value = { form.observaciones } onChange = { onChange } messageinc = "Incorrecto. Ingresa las observaciones."
-                            style = { { paddingLeft: "10px" } } />
+                            customclass="px-2 textarea-input text-justify" />
                     </div>
-                    <div className="col-md-6 pt-3">
+                    <div className="col-md-6 mx-auto pt-3">
                         <div className="mt-5">
                             <ItemSlider items = { form.adjuntos.adjunto.files } item = 'adjunto' multiple = { false }
                                 handleChange = { handleChange } deleteFile = { this.deleteFile }  />
