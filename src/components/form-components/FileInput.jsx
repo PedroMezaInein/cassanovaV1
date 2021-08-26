e5807912f285f7d11d6819894baeb485e01b2ee6
CariJG <64931832+CariJG@users.noreply.github.com>
2021-08-26 16:06:55 -0500

    Validación de input

--- src/components/form-components/FileInput.jsx
+++ src/components/form-components/FileInput.jsx
@@ -65,7 +65,7 @@ class FileInput extends Component {
 
     render() {
         const { onChangeAdjunto, placeholder, value, name, id, accept, files, deleteAdjunto, messageinc, deleteAdjuntoAvance, _key, classbtn, iconclass, color_label, classinput, ...props } = this.props
-        const { fileValido } = this.state
+        let newfileValido = files.length > 0 ? true: false;
         return (
             <>
                 {/* <label className="col-form-label ">{placeholder}</label> */}
@@ -102,7 +102,7 @@ class FileInput extends Component {
                         className={classinput}
                     />
                 </span>
-                <span className={fileValido ? "form-text text-danger hidden" : "form-text text-danger is-invalid"}> {messageinc} </span>
+                <span className={newfileValido ? "form-text text-danger hidden" : "form-text text-danger is-invalid font-size-xs"}> {messageinc} </span>
                 <div className="flex-wrap d-flex d-flex justify-content-center align-items-center">
                     {
                         files.map((file, key) => {
