2c53fa998290a84b26366b5ac3048e9afef55c22
OmarAbaroa <omarac1295@gmail.com>
2021-09-23 10:47:22 -0500

    Error chatch correction

--- src/pages/Usuarios/Accesos/AccesosForm.jsx
+++ src/pages/Usuarios/Accesos/AccesosForm.jsx
@@ -43,7 +43,8 @@ class AccesosForm extends Component {
         },
         editables: {
             usuarios: [],
-            empresas: []
+            empresas: [],
+            departamentos: []
         }
     }
     componentDidMount() {
