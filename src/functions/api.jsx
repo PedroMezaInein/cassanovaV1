93b4bc0013627539f489b012ed6356066cdd3fb4
OmarAbaroa <omarac1295@gmail.com>
2021-10-29 13:14:21 -0500

    Actualziacion de catching errors

--- src/functions/api.jsx
+++ src/functions/api.jsx
@@ -2,28 +2,31 @@ import axios from 'axios'
 import { URL_DEV } from '../constants'
 import { setSingleHeader } from './routers'
 import { errorAlert } from './alert'
+import store from '../redux/store/store';
 
-export function apiDelete(url, at){
+export const apiDelete = async(url, at) => {
     return axios.delete(`${URL_DEV}${url}`, { headers: setSingleHeader(at) })
 }
 
-export async function apiOptions(url, at){
+export const apiOptions = async(url, at) => {
     return await axios.options(`${URL_DEV}${url}`, { headers: setSingleHeader(at) })
 }
 
-export async function apiPutForm(url, form, at){
+export const apiPutForm = async(url, form, at) => {
     return await axios.put(`${URL_DEV}${url}`, form, { headers: setSingleHeader(at) })
 }
 
-export async function apiPostForm(url, form, at){
+export const apiPostForm = async(url, form, at) => {
     return await axios.post(`${URL_DEV}${url}`, form, { headers: setSingleHeader(at) })
 }
 
-export async function apiGet(url, at){
+export const apiGet = async(url, at) => {
     return await axios.get(`${URL_DEV}${url}`, { headers: setSingleHeader(at) })
 }
 
-export function catchErrors(error, flag){
+export const catchErrors = (error, flag) => {
+    let storing = store.getState();
+    const { user } = storing.authUser
     if(flag === false){
         errorAlert(`Ocurrió un error desconocido, intenta refrescando la página`)
     }
@@ -32,6 +35,7 @@ export function catchErrors(error, flag){
         axios.post(`https://hooks.zapier.com/hooks/catch/6874908/bhr9ezu/`,
         encodeForm({
             'error': error,
+            'user': user.name,
             'date': new Date(),
             'url': window.location.href
         }), {headers: {'Accept': 'application/json'}}
