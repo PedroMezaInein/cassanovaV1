b64c0eb16de010b6ed846a86b4eca7c0c0f71177
CariJG <64931832+CariJG@users.noreply.github.com>
2021-11-12 14:40:02 -0600

    Estilos de prestaciones

--- src/components/forms/recursoshumanos/PrestacionesRHList.jsx
+++ src/components/forms/recursoshumanos/PrestacionesRHList.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import SVG from 'react-inlinesvg'
 import { Form } from 'react-bootstrap'
+import { Button } from '../../form-components'
 import { dayDMY } from '../../../functions/setters'
 import { toAbsoluteUrl } from '../../../functions/routers'
 import { apiOptions, catchErrors } from '../../../functions/api'
 import { printResponseErrorAlert } from '../../../functions/alert'
-import { Button } from '../../form-components'
-
 class PrestacionesRHList extends Component {
     state = {
         prestaciones: [],
         form: {
             prestaciones: [{
                 id: '',
-                active: false
+                active: false,
+                activeHistorial:false
             }],
         },
     }
@@ -50,28 +50,35 @@ class PrestacionesRHList extends Component {
         this.setState({ ...this.state, form })
     }
     getHistorial(historial, index){
-        if(historial.pivot.fecha_activacion !== null && historial.pivot.fecha_cancelacion){
+        if (historial.pivot.fecha_activacion !== null && historial.pivot.fecha_cancelacion) {
             let activacionDate = dayDMY(historial.pivot.fecha_activacion)
             let cancelacionDate = dayDMY(historial.pivot.fecha_cancelacion)
-            if(activacionDate === cancelacionDate){
-                return(
-                    <div>
-                        <span className="font-size-h6 text-success">
-                            <span className="svg-icon svg-icon-dark svg-icon-lg">
-                                <SVG src={toAbsoluteUrl('/images/svg/Cross-circle.svg')} />
-                            </span>
-                        </span> <span className="font-weight-bolder">{activacionDate}</span> - Se activó y canceló el mismo día 
+            if (activacionDate === cancelacionDate) {
+                return (
+                    <div className="mb-2" key={index}>
+                        <span className="svg-icon svg-icon-dark svg-icon-lg">
+                            <SVG src={toAbsoluteUrl('/images/svg/Cross-circle.svg')} />
+                        </span>
+                        <span className="font-weight-bolder ml-2">{activacionDate}</span> - Se activó y canceló el mismo día
                     </div>
                 )
-            }else{
-                <div>
-                    <span className="font-weight-bolder">Perido: </span> {activacionDate}
-                </div>
+            } else {
+                return (
+                    <div className="mb-2" key={index}>
+                        <span className="svg-icon svg-icon-dark svg-icon-lg">
+                            <SVG src={toAbsoluteUrl('/images/svg/Cross-circle.svg')} />
+                        </span>
+                        <span className="font-weight-bolder ml-2">Perido: </span> {activacionDate} - {cancelacionDate}
+                    </div>
+                )
             }
-        }else{
-            return(
-                <div>
-                    <span className="font-weight-bolder">Perido: </span>{dayDMY(historial.pivot.fecha_activacion)}
+        } else {
+            return (
+                <div className="mb-2" key={index}>
+                    <span className="svg-icon svg-icon-success svg-icon-lg">
+                        <SVG src={toAbsoluteUrl('/images/svg/Done-circle.svg')} />
+                    </span>
+                    <span className="font-weight-bolder ml-2">Activado </span>- {dayDMY(historial.pivot.fecha_activacion)}
                 </div>
             )
         }
@@ -80,71 +87,98 @@ class PrestacionesRHList extends Component {
     onSubmit = (e) => {
 
     }
+    showHistorial = (key) => {
+        const { form } = this.state
+        let box = document.getElementById(`div-historial-${key}`);
+        if (box.classList.contains('d-none')) {
+            box.classList.remove('d-none');
+            setTimeout(function () { box.classList.remove('opacity-1'); }, 5);
+            form.prestaciones[key].activeHistorial = true
+        } else {
+            box.classList.add('opacity-1');
+            box.addEventListener('transitionend', function (e) {
+                box.classList.add('d-none');
+            },{
+                capture: false,
+                once: true,
+                passive: false
+            });
+            form.prestaciones[key].activeHistorial = false
+        }
+        this.setState({
+            ...this.state,
+            form
+        })
+    }
     render() {
         const { prestaciones, form } = this.state
-        console.log(prestaciones, 'prestaciones')
-        console.log(form, 'form')
         return (
             <>
-                <Form id='form-prestaciones' onSubmit={ this.onSubmit }>
-                    <div className="mt-5">
-                        {
-                            prestaciones.map((prestaciones, key) => {
-                                return (
-                                    <div key={key}>
-                                        <div className={`d-flex align-items-center mb-8`}>
-                                            <span className={`bullet bullet-vertical h-40px w-4px bg-${form.prestaciones[key].active ? 'success' : 'gray-300'}`}></span>
-                                            <div className="d-flex align-items-center mx-4">
-                                                <label className={`checkbox checkbox-lg checkbox-single checkbox-${form.prestaciones[key].active ? 'success' : 'light'}`}>
-                                                    <input
-                                                        type="checkbox"
-                                                        onChange={(e) => { this.checkButton(key, e) }}
-                                                        name='active'
-                                                        checked={form.prestaciones[key].active}
-                                                        value={form.prestaciones[key].active}
-                                                    />
-                                                    <span></span>
-                                                </label>
-                                            </div>
-                                            <div className="flex-grow-1">
-                                                <div className="text-gray-800 font-weight-bolder font-size-lg">{prestaciones.nombre}</div>
-                                                <div class="font-size-sm text-muted align-self-center">
+                {
+                    prestaciones ?
+                        <Form id='form-prestaciones' onSubmit={this.onSubmit}>
+                            <div className="mt-5">
+                                {
+                                    prestaciones.map((prestacion, key) => {
+                                        return (
+                                            <div key={key}>
+                                                <div className={`d-flex align-items-center py-5 ${key === prestaciones.length - 1 ? '' : 'border-bottom-dashed border-light-secondary'} `}>
+                                                    <span className={`bullet bullet-vertical h-40px w-4px bg-${form.prestaciones[key].active ? 'success' : 'gray-300'}`}></span>
+                                                    <div className="d-flex align-items-center mx-4">
+                                                        <label className={`checkbox checkbox-lg checkbox-single checkbox-${form.prestaciones[key].active ? 'success' : 'light'}`}>
+                                                            <input
+                                                                type="checkbox"
+                                                                onChange={(e) => { this.checkButton(key, e) }}
+                                                                name='active'
+                                                                checked={form.prestaciones[key].active}
+                                                                value={form.prestaciones[key].active}
+                                                            />
+                                                            <span></span>
+                                                        </label>
+                                                    </div>
+                                                    <div className="flex-grow-1">
+                                                        <div className="text-gray-800 font-weight-bolder font-size-lg">{prestacion.nombre}</div>
+                                                        <div class="font-size-sm text-muted align-self-center">
+                                                            {
+                                                                prestacion.active !== null ?
+                                                                    <>ACTIVADO: <span class="text-success">{dayDMY(prestacion.active.pivot.fecha_activacion)}</span></>
+                                                                    :
+                                                                    <>DESACTIVADO</>
+                                                            }
+                                                        </div>
+                                                    </div>
                                                     {
-                                                        prestaciones.active !== null ?
-                                                            <>ACTIVADO: <span class="text-success">{dayDMY(prestaciones.active.pivot.fecha_activacion)}</span></>
-                                                            :
-                                                            <>DESACTIVADO</>
+                                                        prestacion.historial.length > 0 ?
+                                                            <div class="btn btn-xs btn-light btn-clean px-2 py-2" onClick={() => { this.showHistorial(key) }}>
+                                                                <span class="text-dark-50 font-weight-bolder font-size-sm">{form.prestaciones[key].activeHistorial?'OCULTAR':'VER'} HISTORIAL</span>
+                                                            </div>
+                                                            : <></>
                                                     }
                                                 </div>
-                                            </div>
-                                            {
-                                                prestaciones.historial.length > 0 ?
-                                                    <span className="badge badge-light-success fs-8 fw-bolder">btn</span>
-                                                    : <></>
-                                            }
-                                        </div>
-                                        {
-                                            prestaciones.historial.length > 0 ?
-                                                prestaciones.historial.map((historial, key2) => {
-                                                    console.log(prestaciones, 'prestaciones')
-                                                    return (
-                                                        <div key={key2}>
-                                                            {this.getHistorial(historial, key2)}
+                                                {
+                                                    prestacion.historial.length > 0 ?
+                                                        <div className="bg-light px-4 pt-4 pb-2 transition-prestaciones opacity-1 d-none w-max-content mx-auto rounded" id={`div-historial-${key}`}>
+                                                            {
+                                                                prestacion.historial.map((historial, key2) => {
+                                                                    return (
+                                                                        this.getHistorial(historial, key2)
+                                                                    )
+                                                                })
+                                                            }
                                                         </div>
-                                                    )
-                                                })
-                                                : <></>
-                                        }
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                    
-                    <div className="d-flex justify-content-center border-top mt-3 pt-3 mx-4">
-                        <Button only_icon='las la-save' className="btn btn-light-success btn-sm font-weight-bold" type='submit' text="Guardar" />
-                    </div>
-                </Form>
+                                                        : <></>
+                                                }
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className="d-flex justify-content-center border-top mt-3 pt-3">
+                                <Button only_icon='las la-save' className="btn btn-light-success btn-sm font-weight-bold" type='submit' text="Guardar" />
+                            </div>
+                        </Form>
+                        : <></>
+                }
             </>
         )
     }
