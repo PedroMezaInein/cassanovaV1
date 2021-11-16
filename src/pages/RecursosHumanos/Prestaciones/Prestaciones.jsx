c553ee6e4df54420f2bff20dece60419417d1465
OmarAbaroa <omarac1295@gmail.com>
2021-11-16 15:42:24 -0600

    Corrección de error crítico en tabla de recursos humanos

--- src/pages/RecursosHumanos/Prestaciones/Prestaciones.jsx
+++ src/pages/RecursosHumanos/Prestaciones/Prestaciones.jsx
@@ -27,10 +27,12 @@ class Prestaciones extends Component {
     componentDidMount(){
         this.getOptions()
         const { state } = this.props.location
-        if(state.prestacion){
-            if(state.flag){
-                if(state.flag === 'egreso'){
-                    this.openModalEgreso(state.prestacion)
+        if(state){
+            if(state.prestacion){
+                if(state.flag){
+                    if(state.flag === 'egreso'){
+                        this.openModalEgreso(state.prestacion)
+                    }
                 }
             }
         }
