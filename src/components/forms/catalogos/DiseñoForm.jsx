2371c3ae856805ea3e554beede33aa69c52c82f4
Pedro <pedro@inein.mx>
2022-04-07 16:51:09 -0500

    se modifico tabulador. proyecto de diseno y se quito grafica de tabulador

--- src/components/forms/catalogos/DiseñoForm.jsx
+++ src/components/forms/catalogos/DiseñoForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Form } from 'react-bootstrap'
-import { Button, InputNumberSinText, InputMoneySinText } from '../../form-components'
+import { InputNumberSinText, InputMoneySinText } from '../../form-components'
 import { validateAlert } from '../../../functions/alert'
 import { setMoneyTableForNominas } from '../../../functions/setters'
 class DiseñoForm extends Component {
@@ -125,106 +125,7 @@ class DiseñoForm extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className="row my-4 d-flex justify-content-center">
-                        <div className="col-md-8">
-                            <div className="separator separator-dashed"></div>
-                        </div>
-                    </div>
-                    <div className="d-flex justify-content-end">
-                        <Button
-                            icon=''
-                            onClick={addRow}
-                            className={"btn btn-icon btn-xs p-3 btn-light-success mr-2"}
-                            only_icon={"flaticon2-plus icon-13px"}
-                            tooltip={{ text: 'AGREGAR' }}
-                        />
-                        <Button
-                            icon=''
-                            onClick={deleteRow}
-                            className={"btn btn-icon btn-xs p-3 btn-light-danger"}
-                            only_icon={"far fa-trash-alt icon-15px"}
-                            tooltip={{ text: 'ELIMINAR' }}
-                        />
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <table className="table table-separate table-responsive-sm text-center w-auto">
-                            <thead>
-                                <tr>
-                                    <th className="pb-0 border-bottom-0">Inferior</th>
-                                    <th className="pb-0 border-bottom-0">Superior</th>
-                                    <th className="pb-0 border-bottom-0">Cambio</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    form.variaciones.map((variacion, key) => {
-                                        return (
-                                            <tr key={key}>
-                                                <td className="px-1 pt-0 pb-2 border-0">
-                                                    <div className="d-flex justify-content-center">
-                                                        <InputNumberSinText
-                                                            requirevalidation={1}
-                                                            name="inferior"
-                                                            formeditado={1}
-                                                            value={form['variaciones'][key]['inferior']}
-                                                            onChange={(e) => onChangeVariaciones(key, e, 'inferior')}
-                                                            // customstyle={{ width: "auto", borderRadius: 0, borderTop: 'none', borderRight: 'none', borderLeft: 'none' }}
-                                                            /* identificador="inferior" */
-                                                            customclass="border-top-0 border-left-0 border-right-0 rounded-0 w-100px text-center pl-0"
-                                                        />
-                                                    </div>
-                                                </td>
-                                                <td className="px-1 pt-0 pb-2 border-0">
-                                                    <div className="d-flex justify-content-center">
-                                                        <InputNumberSinText
-                                                            requirevalidation={1}
-                                                            formeditado={1}
-                                                            name="superior"
-                                                            value={form['variaciones'][key]['superior']}
-                                                            onChange={(e) => onChangeVariaciones(key, e, 'superior')}
-                                                            // customstyle={{ width: "auto", borderRadius: 0, borderTop: 'none', borderRight: 'none', borderLeft: 'none' }}
-                                                            /* identificador="superior" */
-                                                            customclass="border-top-0 border-left-0 border-right-0 rounded-0 w-100px text-center pl-0"
-                                                        />
-                                                    </div>
-                                                </td>
-                                                <td className="px-1 pt-0 pb-2 border-0">
-                                                    <div className="d-flex justify-content-center">
-                                                        <InputMoneySinText
-                                                            requirevalidation={1}
-                                                            formeditado={1}
-                                                            name="cambio"
-                                                            value={form['variaciones'][key]['cambio']}
-                                                            onChange={(e) => onChangeVariaciones(key, e, 'cambio')}
-                                                            thousandseparator={true}
-                                                            prefix='$'
-                                                            // customstyle = { { width: "auto", borderRadius: 0, borderTop: 'none', borderRight: 'none', borderLeft: 'none' } }
-                                                            /* identificador="cambio" */
-                                                            customclass="border-top-0 border-left-0 border-right-0 rounded-0 w-100px text-center pl-0"
-                                                        />
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="card-footer py-3 pr-1">
-                        <div className="row mx-0">
-                            <div className="col-lg-12 text-right pr-0 pb-0">
-                                <Button icon='' className="btn btn-primary mr-2"
-                                    onClick={
-                                        (e) => {
-                                            e.preventDefault();
-                                            validateAlert(onSubmit, e, 'form-diseño')
-                                        }
-                                    }
-                                    text="ENVIAR" />
-                            </div>
-                        </div>
-                    </div>
+                    
                 </Form>
             </>
         )
