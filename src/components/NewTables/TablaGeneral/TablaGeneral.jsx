c70321f0e89e75d164447492a3f37f7f8bba7479
Victor Jesus Cervantes Pizaña <jesus@inein.mx>
2022-11-09 10:57:49 -0600

    Plantilla base Tabla General

---
+++ src/components/NewTables/TablaGeneral/TablaGeneral.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useState, useSelector } from 'react-redux';
+import axios from 'axios';
+
+export default function TablaGeneral(props) {
+  const { titulo, subtitulo, columnas, url, filtroSort, numPaginado, acciones } = props;
+
+  const getData = () => {
+    
+  }
+
+  return (
+
+    <>
+
+    </>
+
+  );
+}
