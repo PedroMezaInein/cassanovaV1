a808c2fd5e9756a84061ddc6644e1123d0ec5e5a
Victor Jesus Cervantes Pizaña <jesus@inein.mx>
2022-12-02 10:21:36 -0600

    fases por proyecto y opciones fase 1

--- src/pages/Proyectos/Proyectos/Fases/BotonAcciones.jsx
+++ src/pages/Proyectos/Proyectos/Fases/BotonAcciones.jsx
@@ -11,27 +11,35 @@ export default function BotonAcciones({ modal, setModal }) {
                 <div className="card-toolbar toolbar-dropdown">
                     <DropdownButton menualign="right"
                         title={<span className="d-flex">OPCIONES <i className="las la-angle-down icon-md p-0 ml-2"></i></span>} id='dropdown-proyectos' >
-                        <Dropdown.Item className="text-hover-info dropdown-info" onClick={() => setModal({ ...modal, info: true })}>
+                        
+                        <Dropdown.Item className="text-hover-info dropdown-info" onClick={() => setModal({ ...modal, edit_phase: true })}>
                             {setNaviIcon('las la-clipboard-list icon-xl', 'EDITAR FASE')}
                         </Dropdown.Item>
-                        <Dropdown.Item className="text-hover-success dropdown-success" onClick={() => setModal({ ...modal, edit_proyect: true })}>
+
+                        <Dropdown.Item className="text-hover-success dropdown-success" onClick={() => setModal({ ...modal, add_agreement: true })}>
                             {setNaviIcon('las la-pencil-alt icon-xl', 'AGREGAR MINUTA DE ACUERDO')}
                         </Dropdown.Item>
+
                         <Dropdown.Item className="text-hover-primary dropdown-primary" onClick={() => setModal({ ...modal, hire_phase: true })}>
                             {setNaviIcon('las la-handshake icon-xl', 'CONTRATAR FASES')}
                         </Dropdown.Item>
-                        <Dropdown.Item className="text-hover-primary dropdown-primary" onClick={() => setModal({ ...modal, hire_phase: true })}>
+
+                        <Dropdown.Item className="text-hover-primary dropdown-primary" onClick={() => setModal({ ...modal, invoice: true })}>
                             {setNaviIcon('las la-handshake icon-xl', 'SOLICITAR FACTURA')}
                         </Dropdown.Item>
-                        <Dropdown.Item className="text-hover-primary dropdown-primary" onClick={() => setModal({ ...modal, hire_phase: true })}>
+
+                        <Dropdown.Item className="text-hover-primary dropdown-primary" onClick={() => setModal({ ...modal, sale: true })}>
                             {setNaviIcon('las la-handshake icon-xl', 'VENTA')}
                         </Dropdown.Item>
-                        <Dropdown.Item className="text-hover-primary dropdown-primary" onClick={() => setModal({ ...modal, hire_phase: true })}>
+
+                        <Dropdown.Item className="text-hover-primary dropdown-primary" onClick={() => setModal({ ...modal, purchase: true })}>
                             {setNaviIcon('las la-handshake icon-xl', 'COMPRA')}
                         </Dropdown.Item>
-                        <Dropdown.Item className="text-hover-primary dropdown-primary" onClick={() => setModal({ ...modal, hire_phase: true })}>
+
+                        <Dropdown.Item className="text-hover-primary dropdown-primary" onClick={() => setModal({ ...modal, end_proyect: true })}>
                             {setNaviIcon('las la-handshake icon-xl', 'FIN DE PROYECTO')}
                         </Dropdown.Item>
+
                     </DropdownButton>
                 </div>
             </div>
--- src/pages/Proyectos/Proyectos/Fases/Fase1.jsx
+++ src/pages/Proyectos/Proyectos/Fases/Fase1.jsx
@@ -1,24 +1,67 @@
 import React, { useEffect, useState } from 'react';
 
+
+
 import BotonAcciones from './BotonAcciones';
+import { Modal } from '../../../../components/singles'
 
 import '../../../../styles/_fases.scss'
 
 export default function Fase1() {
     const [modal, setModal] = useState({
-        edit_proyect: false,
+        edit_phase: false,
+        add_agreement: false,
         hire_phase: false,
-        info: false,
+        invoice: false,
+        sale: false,
+        purchase: false,
+        end_proyect: false,
     });
     return (
-        <div className='container-fase'>
-            <div>
-                header
-                <BotonAcciones modal={modal} setModal={setModal} />
+        <>
+            <div className='container-fase'>
+                <div>
+                    <div>
+                        porcentaje de avance
+                    </div>
+                    <BotonAcciones modal={modal} setModal={setModal} />
+                </div>
+                <div>
+                    
+                    <div>
+                        Historial de minutas
+                    </div>
+                    <div>
+                        Presupuesto inicial
+                    </div>
+                    <div>
+                        Presupuestos adicionales
+                    </div>
+                </div>
             </div>
-            <div>
-                body
-            </div>
-        </div>
+
+            <Modal size="lg" show={modal.edit_phase} title='Editar Fase' handleClose={() => setModal({ ...modal, edit_phase: false })}>
+            </Modal>
+
+            <Modal size="lg" show={modal.add_agreement} title='Minuta de Acuerdo' handleClose={() => setModal({ ...modal, add_agreement: false })}>
+            </Modal>
+
+            <Modal size="lg" show={modal.hire_phase} title='Contratar Fase' handleClose={() => setModal({ ...modal, hire_phase: false })}>
+            </Modal>
+
+            <Modal size="lg" show={modal.invoice} title='Facturar Fase' handleClose={() => setModal({ ...modal, invoice: false })}>
+            </Modal>
+
+            <Modal size="lg" show={modal.sale} title='Venta' handleClose={() => setModal({ ...modal, sale: false })}>
+            </Modal>
+
+            <Modal size="lg" show={modal.purchase} title='Compra' handleClose={() => setModal({ ...modal, purchase: false })}>
+            </Modal>
+
+            <Modal size="lg" show={modal.end_proyect} title='Finalizar Proyecto' handleClose={() => setModal({ ...modal, end_proyect: false })}>
+            </Modal>
+
+        </>
+        
     )
 }
