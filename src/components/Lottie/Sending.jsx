677df4cf9535f2865d02d79a5f6e2924e9c6d7e4
Victor Jesus Cervantes Pizaña <jesus@inein.mx>
2023-03-22 12:49:05 -0600

    Abeja

--- src/components/Lottie/Sending.jsx
+++ src/components/Lottie/Sending.jsx
@@ -4,6 +4,7 @@ import * as animationData2 from '../../assets/animate/arbol.json'
 import * as navidad from '../../assets/animate/navidad.json'
 import * as valentine from '../../assets/animate/valentines-day.json'
 import * as Mujer from '../../assets/animate/diaDeLaMujer.json'
+import * as Abeja from '../../assets/animate/abeja.json'
 import Lottie from 'react-lottie';
 
 class Sending extends Component{
@@ -11,7 +12,7 @@ class Sending extends Component{
         const defaultOptions = {
             loop: true,
             autoplay: true, 
-            animationData: new Date().getMonth() === 11 ? animationData2.default : Mujer.default,
+            animationData: new Date().getMonth() === 11 ? animationData2.default : Abeja.default,
             rendererSettings: {
                 preserveAspectRatio: 'xMidYMid slice'
             }
@@ -19,8 +20,8 @@ class Sending extends Component{
         return(
             <div className="mx-auto">
                 <Lottie options={defaultOptions}
-                    height={200}
-                    width={310}
+                    height={150}
+                    width={150}
                     isStopped={false}
                     isPaused={false}/>
             </div>
