5059a80f9402c0ec98850c21aa9f3a39dc4059a1
Victor Jesus Cervantes Pizaña <jesus@inein.mx>
2023-04-03 17:36:50 -0600

    cambio de fechas crear colaborador a formato yyyy-mm-dd

--- src/pages/RecursosHumanos/Empleados/EmpleadosForm.jsx
+++ src/pages/RecursosHumanos/Empleados/EmpleadosForm.jsx
@@ -278,19 +278,23 @@ class EmpleadosForm extends Component {
         const { form } = this.state
         const data = new FormData();
         let aux = Object.keys(form)
+        
         aux.map((element) => {
             switch (element) {
-                case 'fechaInicio':
+                /* case 'fechaInicio':
+                    data.append(form[element])
+                    debugger
                 case 'fecha_nacimiento':
-                    data.append(element, (new Date(form[element])).toDateString())
+                    data.append(form[element])
+                    debugger
                     break;
                 case 'fechaFin':
                 case 'fecha_alta_imss':
                     if (form[element])
-                        data.append(element, (new Date(form[element])).toDateString())
+                        data.append(form[element])
                     else
                         data.append(element, '')
-                    break;
+                    break; */
                 case 'adjuntos':
                     break;
                 case 'departamentos':
@@ -304,6 +308,7 @@ class EmpleadosForm extends Component {
             }
             return false
         })
+        debugger
         aux = Object.keys(form.adjuntos)
         aux.map((element) => {
             if (form.adjuntos[element].value !== '') {
